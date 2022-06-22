acf0c0fd5af06f9b5ee24caf8b4d4411f39b6922
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-06-22 12:03:30 +0700

    Minimal debug print on main thread

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,9 @@
-#include <Gts/HitCounterManager.h>
-
 #include "Config.h"
-#include "Papyrus.h"
 
 #include <stddef.h>
 
 using namespace RE::BSScript;
+using namespace RE::ConsoleLog;
 using namespace Gts;
 using namespace SKSE;
 using namespace SKSE::log;
@@ -46,130 +44,6 @@ namespace {
         spdlog::set_default_logger(std::move(log));
         spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%t] [%s:%#] %v");
     }
-
-    /**
-     * Initialize the SKSE cosave system for our plugin.
-     *
-     * <p>
-     * SKSE comes with a feature called a <em>cosave</em>, an additional save file kept alongside the original Skyrim
-     * save file. SKSE plugins can write their own data to this file, and load it again when the save game is loaded,
-     * allowing them to keep custom data along with a player's save. Each plugin must have a unique ID, which is four
-     * characters long (similar to the record names used by forms in ESP files). Note however this is little-endian, so
-     * technically the 'SMPL' here ends up as 'LPMS' in the save file, unless we use a byte order swap.
-     * </p>
-     *
-     * <p>
-     * There can only be one serialization callback for save, revert (called on new game and before a load), and load
-     * for the entire plugin.
-     * </p>
-     */
-    void InitializeSerialization() {
-        log::trace("Initializing cosave serialization...");
-        auto* serde = GetSerializationInterface();
-        serde->SetUniqueID(_byteswap_ulong('SMPL'));
-        serde->SetSaveCallback(Gts::HitCounterManager::OnGameSaved);
-        serde->SetRevertCallback(Gts::HitCounterManager::OnRevert);
-        serde->SetLoadCallback(Gts::HitCounterManager::OnGameLoaded);
-        log::trace("Cosave serialization initialized.");
-    }
-
-    /**
-     * Initialize our Papyrus extensions.
-     *
-     * <p>
-     * A common use of SKSE is to add new Papyrus functions. You can call a registration callback to do this. This
-     * callback will not necessarily be called immediately, if the Papyrus VM has not been initialized yet (in that case
-     * it's execution is delayed until the VM is available).
-     * </p>
-     *
-     * <p>
-     * You can call the <code>Register</code> function as many times as you want and at any time you want to register
-     * additional functions.
-     * </p>
-     */
-    void InitializePapyrus() {
-        log::trace("Initializing Papyrus binding...");
-        if (GetPapyrusInterface()->Register(Gts::RegisterHitCounter)) {
-            log::debug("Papyrus functions bound.");
-        } else {
-            stl::report_and_fail("Failure to register Papyrus bindings.");
-        }
-    }
-
-    /**
-     * Initialize the trampoline space for function hooks.
-     *
-     * <p>
-     * Function hooks are one of the most powerful features available to SKSE developers, allowing you to replace
-     * functions with your own, or replace a function call with a call to another function. However, to do this, we
-     * need a code snippet that replicates the first instructions of the original code we overwrote, in order to be
-     * able to call back to the original control flow with all the same functionality.
-     * </p>
-     *
-     * <p>
-     * CommonLibSSE comes with functionality to allocate trampoline space, including a common singleton space we can
-     * access from anywhere. While this is not necessarily the most advanced use of trampolines and hooks, this will
-     * suffice for our demo project.
-     * </p>
-     */
-    void InitializeHooking() {
-        log::trace("Initializing trampoline...");
-        auto& trampoline = GetTrampoline();
-        trampoline.create(64);
-        log::trace("Trampoline initialized.");
-
-        Gts::InitializeHook(trampoline);
-    }
-
-    /**
-     * Register to listen for messages.
-     *
-     * <p>
-     * SKSE has a messaging system to allow for loosely coupled messaging. This means you don't need to know about or
-     * link with a message sender to receive their messages. SKSE itself will send messages for common Skyrim lifecycle
-     * events, such as when SKSE plugins are done loading, or when all ESP plugins are loaded.
-     * </p>
-     *
-     * <p>
-     * Here we register a listener for SKSE itself (because we have not specified a message source). Plugins can send
-     * their own messages that other plugins can listen for as well, although that is not demonstrated in this example
-     * and is not common.
-     * </p>
-     *
-     * <p>
-     * The data included in the message is provided as only a void pointer. It's type depends entirely on the type of
-     * message, and some messages have no data (<code>dataLen</code> will be zero).
-     * </p>
-     */
-    void InitializeMessaging() {
-        if (!GetMessagingInterface()->RegisterListener([](MessagingInterface::Message* message) {
-            switch (message->type) {
-                // Skyrim lifecycle events.
-                case MessagingInterface::kPostLoad: // Called after all plugins have finished running SKSEPlugin_Load.
-                    // It is now safe to do multithreaded operations, or operations against other plugins.
-                case MessagingInterface::kPostPostLoad: // Called after all kPostLoad message handlers have run.
-                case MessagingInterface::kInputLoaded: // Called when all game data has been found.
-                    break;
-                case MessagingInterface::kDataLoaded: // All ESM/ESL/ESP plugins have loaded, main menu is now active.
-                    // It is now safe to access form data.
-                    InitializeHooking();
-                    break;
-
-                // Skyrim game events.
-                case MessagingInterface::kNewGame: // Player starts a new game from main menu.
-                case MessagingInterface::kPreLoadGame: // Player selected a game to load, but it hasn't loaded yet.
-                    // Data will be the name of the loaded save.
-                case MessagingInterface::kPostLoadGame: // Player's selected save game has finished loading.
-                    // Data will be a boolean indicating whether the load was successful.
-                case MessagingInterface::kSaveGame: // The player has saved a game.
-                    // Data will be the save name.
-                case MessagingInterface::kDeleteGame: // The player deleted a saved game from within the load menu.
-                    break;
-            }
-        })) {
-            stl::report_and_fail("Unable to register message listener.");
-        }
-    }
 }
 
 /**
@@ -191,9 +65,14 @@ SKSEPluginLoad(const LoadInterface* skse) {
 
 
     Init(skse);
-    InitializeMessaging();
-    InitializeSerialization();
-    InitializePapyrus();
+
+    auto task = SKSE::GetTaskInterface();
+
+  	// using a lambda
+  	task->AddTask([]() {
+  		Print("This is a task implemented with a lambda!");
+      log::info("Task Ran.", plugin->GetName());
+  	});
 
     log::info("{} has finished loading.", plugin->GetName());
     return true;
--- src/Papyrus.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <RE/Skyrim.h>
-
-namespace Gts {
-    bool RegisterHitCounter(RE::BSScript::IVirtualMachine* vm);
-
-    void InitializeHook(SKSE::Trampoline& trampoline);
-}
