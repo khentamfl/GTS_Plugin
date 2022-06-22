ef85f454cb3ef39fef2418252b8f30a7555840c9
Andrew King <sheepchaan@gmail.com>
2022-06-23 01:40:29 +0700

    Step7

--- src/Config.cpp
+++ src/Config.cpp
@@ -8,17 +8,17 @@ using namespace Gts;
 const Config& Config::GetSingleton() noexcept {
     static Config instance;
 
-    // static std::atomic_bool initialized;
-    // static std::latch latch(1);
-    // if (!initialized.exchange(true)) {
-    //     std::ifstream inputFile(R"(Data\SKSE\Plugins\GtsPlugin.yaml)");
-    //     if (inputFile.good()) {
-    //         yaml_source ar(inputFile);
-    //         ar >> instance;
-    //     }
-    //     latch.count_down();
-    // }
-    // latch.wait();
+    static std::atomic_bool initialized;
+    static std::latch latch(1);
+    if (!initialized.exchange(true)) {
+        // std::ifstream inputFile(R"(Data\SKSE\Plugins\GtsPlugin.yaml)");
+        // if (inputFile.good()) {
+        //     yaml_source ar(inputFile);
+        //     ar >> instance;
+        // }
+        latch.count_down();
+    }
+    latch.wait();
 
     return instance;
 }
