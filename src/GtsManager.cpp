22d20bd1cc4f5b50a9d469c84a6f06c6df54f53e
Andrew King <sheepchaan@gmail.com>
2022-06-24 11:15:44 +0700

    Print actor races nearby

--- src/GtsManager.cpp
+++ src/GtsManager.cpp
@@ -13,10 +13,41 @@ GtsManager& GtsManager::GetSingleton() noexcept {
 	return instance;
 }
 
+void GtsManager::find_actors() {
+	log::info("A");
+	auto process_list = ProcessLists::GetSingleton();
+	log::info("B");
+	for (ActorHandle actor_handle: process_list->highActorHandles)
+	{
+		log::info("C");
+		NiPointer<TESObjectREFR> actor_ref = TESObjectREFR::LookupByHandle(actor_handle);
+		log::info("D");
+		Acror* actor = DYNAMIC_CAST(actor_ref, TESObjectREFR, Actor);
+		log::info("E");
+		if (actor && actor->Is3DLoaded())
+		{
+			log::info("F");
+			auto race = actor->GetRace();
+			log::info("G");
+			auto race_name = race.GetFullName();
+			log::info("Actor with race %s found!", race_name);
+		}
+	}
+}
+
 // Poll for updates
 void GtsManager::poll() {
+	auto player_char = RE::PlayerCharacter::GetSingleton();
+	if (!player_char) {
+		return;
+	}
+	if (!player_char.Is3DLoaded()) {
+		return;
+	}
+
 	auto ui = RE::UI::GetSingleton();
 	if (!ui->GameIsPaused()) {
 		log::info("Poll.");
+		$ this->find_actors ();
 	}
 }
