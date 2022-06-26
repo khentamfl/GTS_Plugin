b2bfa08e193f7d28a69a390e66b7f0b60968dd35
Andrew King <sheepchaan@gmail.com>
2022-06-26 11:24:44 +0700

    Hook actors

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -10,6 +10,7 @@ namespace Hooks
 		logger::info("Gts applying hooks...");
 
 		MainUpdateHook::Hook();
+		HookOnPlayerUpdate::Hook();
 
 		logger::info("Gts finished applying hooks...");
 	}
@@ -19,19 +20,48 @@ namespace Hooks
 		logger::info("Gts applying MainUpdateHook");
 
 		auto& trampoline = SKSE::GetTrampoline();
-		trampoline.create(64);
 
-		REL::Relocation<uintptr_t> hook(REL::RelocationID(35551, 36544), REL::Relocate(0x11F, 0x160));          // main loop
-		_Update = trampoline.write_call<5>(hook.address(), Update);
+		REL::Relocation<uintptr_t> hook(REL::RelocationID(35551, 36544));          // main loop
+		_Update = trampoline.write_call<5>(hook.address() + REL::Relocate(0x11F, 0x160), Update);
 	}
 
 	void MainUpdateHook::Update(RE::Main* a_this, float a2)
 	{
 		_Update(a_this, a2);
 
-		Gts::GtsManager::GetSingleton().poll();
+		logger::info("Update");
 	}
 
 
+	void HookOnPlayerUpdate::Hook() {
+		logger::info("Hooking player update");
+		REL::Relocation<std::uintptr_t> PlayerCharacterVtbl{ RE::VTABLE_PlayerCharacter[0] };
 
+		_Update = PlayerCharacterVtbl.write_vfunc(0xAD, Update);
+
+	}
+
+	void HookOnPlayerUpdate::Update(RE::PlayerCharacter* a_this, float a_delta) {
+		_Update(a_this, a_delta);
+
+		auto base_actor = a_this->GetActorBase();
+		auto actor_name = base_actor->GetFullName();
+		logger::info("Update player: {}", actor_name);
+	}
+
+	void HookOnActorUpdate::Hook() {
+		logger::info("Hooking character update");
+		REL::Relocation<std::uintptr_t> ActorVtbl{ RE::VTABLE_Character[0] };
+
+		_Update = ActorVtbl.write_vfunc(0xAD, Update);
+
+	}
+
+	void HookOnActorUpdate::Update(RE::Actor* a_this, float a_delta) {
+		_Update(a_this, a_delta);
+
+		auto base_actor = a_this->GetActorBase();
+		auto actor_name = base_actor->GetFullName();
+		logger::info("Update actor: {}", actor_name);
+	}
 }
--- src/hooks.h
+++ src/hooks.h
@@ -14,4 +14,24 @@ namespace Hooks
 			static inline REL::Relocation<decltype(Update)> _Update;
 	};
 
+	class HookOnPlayerUpdate
+	{
+		public:
+			static void Hook();
+		private:
+
+			static void Update(RE::PlayerCharacter* a_this, float a_delta);
+			static inline REL::Relocation<decltype(Update)> _Update;
+	};
+
+	class HookOnActorUpdate
+	{
+		public:
+			static void Hook();
+		private:
+
+			static void Update(RE::Actor* a_this, float a_delta);
+			static inline REL::Relocation<decltype(Update)> _Update;
+	};
+
 }
