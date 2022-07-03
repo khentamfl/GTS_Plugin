3364bd8ae7a0d37a173818155a544fb1c5c8fe9a
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-07-03 10:28:50 +0700

    Some rigid body searching

--- src/node.cpp
+++ src/node.cpp
@@ -4,7 +4,7 @@ using namespace SKSE;
 using namespace Gts;
 
 namespace Gts {
-  void walk_nodes(Actor* actor) {
+	void walk_nodes(Actor* actor) {
 		if (!actor->Is3DLoaded()) {
 			return;
 		}
@@ -103,7 +103,69 @@ namespace Gts {
 		return nullptr;
 	}
 
-  void clone_bound(Actor* actor) {
+	void query_nodes(Actor* actor) {
+		if (!actor->Is3DLoaded()) {
+			return nullptr;
+		}
+		auto model = actor->Get3D();
+		if (!model) {
+			return nullptr;
+		}
+		auto node_lookup = model->GetObjectByName(node_name);
+		if (node_lookup) {
+			return node_lookup;
+		}
+
+		// Game lookup failed we try and find it manually
+		std::deque<NiAVObject*> queue;
+		queue.push_back(model);
+
+
+		while (!queue.empty()) {
+			auto currentnode = queue.front();
+			queue.pop_front();
+			try {
+				if (currentnode) {
+					auto ninode = currentnode->AsNode();
+					if (ninode) {
+						for (auto child: ninode->GetChildren()) {
+							// Bredth first search
+							queue.push_back(child.get());
+							// Depth first search
+							//queue.push_front(child.get());
+						}
+					}
+					// Do smth
+					auto collision_object = currentnode->GetCollisionObject();
+					if (collision_object) {
+						auto rigid_body = collision_object->GetRigidBody();
+						if (rigid_body) {
+
+						}
+					}
+					if  (currentnode->name.c_str() == node_name) {
+						return currentnode;
+					}
+				}
+			}
+			catch (const std::overflow_error& e) {
+				log::warn("Overflow: {}", e.what());
+			} // this executes if f() throws std::overflow_error (same type rule)
+			catch (const std::runtime_error& e) {
+				log::warn("Underflow: {}", e.what());
+			} // this executes if f() throws std::underflow_error (base class rule)
+			catch (const std::exception& e) {
+				log::warn("Exception: {}", e.what());
+			} // this executes if f() throws std::logic_error (base class rule)
+			catch (...) {
+				log::warn("Exception Other");
+			}
+		}
+
+		return;
+	}
+
+	void clone_bound(Actor* actor) {
 		// This is the bound on the NiExtraNodeData
 		// This data is shared between all skeletons and this hopes to correct this
 		auto model = actor->Get3D();
