b1236e19254d9b0fc382863a016259b4cd8d701b
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-07-07 17:12:13 +0700

    Write formid to save correctly

--- src/persistent.cpp
+++ src/persistent.cpp
@@ -29,38 +29,43 @@ namespace Gts {
 
 		while (serde->GetNextRecordInfo(type, version, size)) {
 			if (type == ActorDataRecord) {
-				std::size_t size;
-				serde->ReadRecordData(&size, sizeof(size));
-				for (; size > 0; --size) {
-					RE::FormID actorFormID;
-					serde->ReadRecordData(&actorFormID, sizeof(actorFormID));
-					RE::FormID newActorFormID;
-					if (!serde->ResolveFormID(actorFormID, newActorFormID)) {
-						log::warn("Actor ID {:X} could not be found after loading the save.", actorFormID);
-						continue;
+				if (version == 1) {
+					std::size_t count;
+					serde->ReadRecordData(&count, sizeof(count));
+					for (; count > 0; --count) {
+						RE::FormID actorFormID;
+						serde->ReadRecordData(&actorFormID, sizeof(actorFormID));
+						RE::FormID newActorFormID;
+						if (!serde->ResolveFormID(actorFormID, newActorFormID)) {
+							log::warn("Actor ID {:X} could not be found after loading the save.", actorFormID);
+							continue;
+						}
+						float native_scale;
+						serde->ReadRecordData(&native_scale, sizeof(native_scale));
+						float visual_scale;
+						serde->ReadRecordData(&visual_scale, sizeof(visual_scale));
+						float visual_scale_v;
+						serde->ReadRecordData(&visual_scale_v, sizeof(visual_scale_v));
+						float target_scale;
+						serde->ReadRecordData(&target_scale, sizeof(target_scale));
+						float max_scale;
+						serde->ReadRecordData(&max_scale, sizeof(max_scale));
+						ActorData data;
+						log::info("Loading Actor {:X} with data, native_scale: {}, visual_scale: {}, visual_scale_v: {}, target_scale: {}, max_scale: {}", newActorFormID, native_scale, visual_scale, visual_scale_v, target_scale, max_scale);
+						data.native_scale = native_scale;
+						data.visual_scale = visual_scale;
+						data.visual_scale_v = visual_scale_v;
+						data.target_scale = target_scale;
+						data.max_scale = max_scale;
+						auto* actor = TESForm::LookupByID<Actor>(newActorFormID);
+						if (actor) {
+							GetSingleton()._actor_data.insert_or_assign(actor, data);
+						} else {
+							log::warn("Actor ID {:X} could not be found after loading the save.", newActorFormID);
+						}
 					}
-					float native_scale;
-					serde->ReadRecordData(&native_scale, sizeof(native_scale));
-					float visual_scale;
-					serde->ReadRecordData(&visual_scale, sizeof(visual_scale));
-					float visual_scale_v;
-					serde->ReadRecordData(&visual_scale_v, sizeof(visual_scale_v));
-					float target_scale;
-					serde->ReadRecordData(&target_scale, sizeof(target_scale));
-					float max_scale;
-					serde->ReadRecordData(&max_scale, sizeof(max_scale));
-					ActorData data;
-					data.native_scale = native_scale;
-					data.visual_scale = visual_scale;
-					data.visual_scale_v = visual_scale_v;
-					data.target_scale = target_scale;
-					data.max_scale = max_scale;
-					auto* actor = TESForm::LookupByID<Actor>(newActorFormID);
-					if (actor) {
-						GetSingleton()._actor_data.insert_or_assign(actor, data);
-					} else {
-						log::warn("Actor ID {:X} could not be found after loading the save.", newActorFormID);
-					}
+				} else {
+					log::info("Disregarding version 0 cosave info.")
 				}
 			} else {
 				log::warn("Unknown record type in cosave.");
@@ -74,21 +79,27 @@ namespace Gts {
 		log::info("+ Saving cosave");
 		std::unique_lock lock(GetSingleton()._lock);
 
-		if (!serde->OpenRecord(ActorDataRecord, 0)) {
+		if (!serde->OpenRecord(ActorDataRecord, 1)) {
 			log::error("Unable to open record to write cosave data.");
 			return;
 		}
 
-		auto size = GetSingleton()._actor_data.size();
-		serde->WriteRecordData(&size, sizeof(size));
+		auto count = GetSingleton()._actor_data.size();
+		serde->WriteRecordData(&count, sizeof(count));
 		for (auto const& [actor, data] : GetSingleton()._actor_data) {
-			auto form_id = &actor->formID;
-			serde->WriteRecordData(form_id, sizeof(form_id));
-			serde->WriteRecordData(&data.native_scale, sizeof(data.native_scale));
-			serde->WriteRecordData(&data.visual_scale, sizeof(data.visual_scale));
-			serde->WriteRecordData(&data.visual_scale_v, sizeof(data.visual_scale_v));
-			serde->WriteRecordData(&data.target_scale, sizeof(data.target_scale));
-			serde->WriteRecordData(&data.max_scale, sizeof(data.max_scale));
+			FormID form_id = actor->formID;
+			float native_scale = data.native_scale;
+			float visual_scale = data.visual_scale;
+			float visual_scale_v = data.visual_scale_v;
+			float target_scale = data.target_scale;
+			float max_scale = data.max_scale;
+			log::info("Saving Actor {:X} with data, native_scale: {}, visual_scale: {}, visual_scale_v: {}, target_scale: {}, max_scale: {}", form_id, native_scale, visual_scale, visual_scale_v, target_scale, max_scale);
+			serde->WriteRecordData(&form_id, sizeof(form_id));
+			serde->WriteRecordData(&native_scale, sizeof(native_scale));
+			serde->WriteRecordData(&visual_scale, sizeof(visual_scale));
+			serde->WriteRecordData(&visual_scale_v, sizeof(visual_scale_v));
+			serde->WriteRecordData(&target_scale, sizeof(target_scale));
+			serde->WriteRecordData(&max_scale, sizeof(max_scale));
 		}
 		log::info("- Saving cosave");
 	}
