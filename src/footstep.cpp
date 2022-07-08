b448df511a8ec8f470750434e99aa7d4b249e7c5
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-07-08 19:40:33 +0700

    Retry skyrim event based footsteps

--- src/footstep.cpp
+++ src/footstep.cpp
@@ -11,13 +11,35 @@ namespace Gts {
 	}
 
 	void FootStepManager::HookProcessEvent(const BGSFootstepEvent* a_event, BSTEventSource<BGSFootstepEvent>* a_eventSource) {
-		log::info("+ Foot step event");
+		log::info("+ Foot step event Hook");
 		auto actor = a_event->actor.get().get();
 		if (actor) {
 			auto base_actor = actor->GetActorBase();
 			log::info("  - Event for: {}", base_actor->GetFullName());
 		}
 		log::info("  - Tag: {}", a_event->tag);
-		log::info("- Foot step event");
+		log::info("- Foot step event Hook");
+	}
+
+	bool FootStepManager::RegisterSink() {
+		auto manager = BGSFootstepManager::GetSingleton();
+		if (!manager) {
+			return false;
+		}
+		manager->AddEventSink(this);
+		return true;
+	}
+
+	BSEventNotifyControl FootStepManager::ProcessEvent(const BGSFootstepEvent* a_event, BSTEventSource<BGSFootstepEvent>* a_eventSource) {
+		log::info("+ Foot step event Skyrim");
+		auto actor = a_event->actor.get().get();
+		if (actor) {
+			auto base_actor = actor->GetActorBase();
+			log::info("  - Event for: {}", base_actor->GetFullName());
+		}
+		log::info("  - Tag: {}", a_event->tag);
+		log::info("- Foot step event Skyrim");
+
+		return BSEventNotifyControl::kContinue;
 	}
 }
--- src/footstep.h
+++ src/footstep.h
@@ -7,10 +7,14 @@ using namespace SKSE;
 using namespace RE;
 
 namespace Gts {
-	class FootStepManager {
+	class FootStepManager : public BSTEventSink<BGSFootstepEvent>  {
 		public:
 			[[nodiscard]] static FootStepManager& GetSingleton() noexcept;
 
 			void HookProcessEvent(const BGSFootstepEvent* a_event, BSTEventSource<BGSFootstepEvent>* a_eventSource);
+
+			BSEventNotifyControl ProcessEvent(const BGSFootstepEvent* a_event, BSTEventSource<BGSFootstepEvent>* a_eventSource) override;
+
+			bool RegisterSink();
 	};
 }
