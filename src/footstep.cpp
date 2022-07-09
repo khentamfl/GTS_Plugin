97031e15a3bf3d679f7cf9f94d23c83468ff3109
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-07-09 12:54:00 +0700

    Clean up logs
    Try to fix mcm

--- src/footstep.cpp
+++ src/footstep.cpp
@@ -12,22 +12,14 @@ namespace Gts {
 	}
 
 	void FootStepManager::HookProcessEvent(const BGSFootstepEvent* a_event, BSTEventSource<BGSFootstepEvent>* a_eventSource) {
-		log::info("+ Foot step event Hook");
 		if (a_event) {
 			auto actor = a_event->actor.get().get();
 			if (actor) {
 				auto base_actor = actor->GetActorBase();
-				log::info("  - Event for: {}", base_actor->GetFullName());
 			}
 			std::string tag = a_event->tag.c_str();
-			log::info("  - Tag: {}", tag);
 			auto event_manager = ModEventManager::GetSingleton();
-			log::info("Queueing event");
 			event_manager.m_onfootstep.SendEvent(actor,tag);
-			log::info("Queued event");
-		} else {
-			log::info("  - No event data supplied");
 		}
-		log::info("- Foot step event Hook");
 	}
 }
