8190cda7f386635a763f4dc2e34102c609faec5b
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-07-11 18:14:55 +0700

    Account for that scale

--- src/highheel.cpp
+++ src/highheel.cpp
@@ -20,8 +20,23 @@ namespace {
 		if (!npc_node) {
 			return 0.0;
 		}
-        log::info("NPC Scale {}", npc_node->local.scale);
+		log::info("NPC Scale {}", npc_node->local.scale);
 		return npc_node->local.translate.z;
+	}
+
+	float npc_scale(Actor* actor) {
+		if (!actor) {
+			return 1.0;
+		}
+		std::string node_name = "NPC";
+		auto npc_node = find_node(actor, node_name, false);
+		if (!npc_node) {
+			npc_node = find_node(actor, node_name, true);
+		}
+		if (!npc_node) {
+			return 1.0;
+		}
+		return npc_node->local.scale;
 	}
 }
 
@@ -41,6 +56,11 @@ namespace Gts {
 		// therefore we just grab the effects of the npc node
 		// scale
 		float scale = get_npcnode_scale(actor);
+		// We also have to account for and
+		// undo the scale of the node between
+		// [NPC..NPC Root Node)
+		// ...Which is just NPC
+		scale *= (1.0 / npc_scale(actor));
 		float new_hh = scale * base_heel;
 		log::info("New HH (Unadjusted): {}", new_hh);
 		// We are going to translate NPC Root [Root]
