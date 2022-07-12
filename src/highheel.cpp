a3f3318fdd700bcd079749a08f98e63eef1d6e60
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-07-12 08:40:06 +0700

    HH direct set experiment

--- src/highheel.cpp
+++ src/highheel.cpp
@@ -22,16 +22,16 @@ namespace {
 		}
 		log::info("NPC Scale {}", npc_node->local.scale);
 		float parent_hh = npc_node->local.translate.z;
-        // we are going to apply this to the child of this node
-        // which is NPCRoot Node. we need to put this height
-        // in child space
+		// we are going to apply this to the child of this node
+		// which is NPCRoot Node. we need to put this height
+		// in child space
 		float child_hh = parent_hh / npc_node->local.scale;
 		// Trying also refscale
 		// This is maybe applied indirectly (maybe via animation) to the NPC node
 		// float ref_scale = get_ref_scale(actor);
-        // log::info("refscale: {}", ref_scale);
+		// log::info("refscale: {}", ref_scale);
 		// child_hh /= ref_scale;
-        return child_hh;
+		return child_hh;
 	}
 }
 
@@ -43,44 +43,53 @@ namespace Gts {
 		if (!Persistent::GetSingleton().highheel_correction) {
 			return;
 		}
-		float base_heel = base_highheel(actor);
-		log::info("Base HH: {}", base_heel);
-		// If there is any scale on the ModelScale
-		// or refscale then it already corrects the
-		// high heels
-		// therefore we just grab the effects of the npc node
-		// scale
-		float scale = get_npcnode_scale(actor);
-		float new_hh = scale * base_heel;
-		log::info("New HH (Unadjusted): {}", new_hh);
-		// We are going to translate NPC Root [Root]
-		// rather than NPC node
-		// This will  leave the NPC node as having a
-		// the base high heel height
-		// But so we don't double it up we subtract base_heel
-		// from the new_hh height that we will apply to
-		// NPC Root [Root] only a correction
-		new_hh -= base_heel;
-		log::info("New HH (Adjusted): {}", new_hh);
-		// The true foot position should lie between
-		// base_heel and (base_heel -1) because most meshes put it slighly under the floor
-		// We apply maximal error approximations to remove the scaled error
-		float maximal_error = -1.0;
-		float scaled_error = maximal_error*scale;
-		float scaled_error_delta = scaled_error - maximal_error;
-		log::info("Scaled maximal error delta: {}", scaled_error_delta);
-		new_hh -= scaled_error_delta;
-		log::info("New HH (Error corrected): {}", new_hh);
-		// Now to set it for third person
+		float new_hh = 0.0;
+		bool experiment = true;
 		std::string node_name = "NPC Root [Root]";
-		auto npc_root_node = find_node(actor, node_name, false);
-		if (npc_root_node) {
-			npc_root_node->local.translate.z = new_hh;
+		if (experiment) {
+			float base_heel = 8.0;
+			float scale = get_npcnode_scale(actor);
+			new_hh = scale * base_heel;
+			node_name = "NPC";
+		} else {
+			float base_heel = base_highheel(actor);
+			log::info("Base HH: {}", base_heel);
+			// If there is any scale on the ModelScale
+			// or refscale then it already corrects the
+			// high heels
+			// therefore we just grab the effects of the npc node
+			// scale
+			float scale = get_npcnode_scale(actor);
+			new_hh = scale * base_heel;
+			log::info("New HH (Unadjusted): {}", new_hh);
+			// We are going to translate NPC Root [Root]
+			// rather than NPC node
+			// This will  leave the NPC node as having a
+			// the base high heel height
+			// But so we don't double it up we subtract base_heel
+			// from the new_hh height that we will apply to
+			// NPC Root [Root] only a correction
+			new_hh -= base_heel;
+			log::info("New HH (Adjusted): {}", new_hh);
+			// The true foot position should lie between
+			// base_heel and (base_heel -1) because most meshes put it slighly under the floor
+			// We apply maximal error approximations to remove the scaled error
+			float maximal_error = -1.0;
+			float scaled_error = maximal_error*scale;
+			float scaled_error_delta = scaled_error - maximal_error;
+			log::info("Scaled maximal error delta: {}", scaled_error_delta);
+			new_hh -= scaled_error_delta;
+			log::info("New HH (Error corrected): {}", new_hh);
+			// Now to set it for third person
 		}
-		// Now to set it for first person
-		auto npc_root_node_fp = find_node(actor, node_name, true);
-		if (npc_root_node_fp) {
-			npc_root_node_fp->local.translate.z = new_hh;
+
+		for (bool person: {false, true}) {
+			auto npc_root_node = find_node(actor, node_name, person);
+			if (npc_root_node) {
+				npc_root_node->local.translate.z = new_hh;
+				NiUpdateData ctx;
+				npc_root_node->UpdateWorldData(&ctx);
+			}
 		}
 	}
 }
