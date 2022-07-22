ea0e851965980307682d2ca59fde5fb7c74e74e4
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-07-22 10:06:30 +0700

    Just use any material

--- src/managers/footstep.cpp
+++ src/managers/footstep.cpp
@@ -30,7 +30,7 @@ namespace {
 				return Runtime::GetSingleton().FootstepR;
 				break;
 		}
-    return nullptr;
+		return nullptr;
 	}
 }
 namespace Gts {
@@ -49,58 +49,60 @@ namespace Gts {
 			auto event_manager = ModEventManager::GetSingleton();
 			event_manager.m_onfootstep.SendEvent(actor,tag);
 
-            log::info("{} for {}", tag, actor_name(actor));
+			log::info("{} for {}", tag, actor_name(actor));
 			// Foot step time
 			float scale = get_scale(actor);
 			float k = 4.0;
 			float n = 5.6;
 			float a = 1.1;
 			if (scale >= a) {
-                log::info("Actor is big enough");
+				log::info("Actor is big enough");
 				float volume = pow(k*(scale-a), n);
 
 				NiAVObject* foot = nullptr;
 				Foot foot_kind = Foot::Unknown;
 				if (starts_with(tag, "FootLeft")) {
-                    log::info("Trying to find FootLeft");
+					log::info("Trying to find FootLeft");
 					foot = find_node_regex_any(actor, ".*(L.*Foot|L.*Leg.*Tip).*");
 					foot_kind = Foot::Left;
 				} else if (starts_with(tag, "FootRight")) {
 					log::info("Trying to find FootRight");
-                    foot = find_node_regex_any(actor, ".*(R.*Foot|R.*Leg.*Tip).*");
+					foot = find_node_regex_any(actor, ".*(R.*Foot|R.*Leg.*Tip).*");
 					foot_kind = Foot::Right;
 				} else if (starts_with(tag, "FootFont")) {
 					log::info("Trying to find FootFont");
-                    foot = find_node_regex_any(actor, ".*((R|L).*Hand|(R|L)b.*Arm.*Tip).*");
+					foot = find_node_regex_any(actor, ".*((R|L).*Hand|(R|L)b.*Arm.*Tip).*");
 					foot_kind = Foot::Front;
 				} else if (starts_with(tag, "FootBack")) {
 					log::info("Trying to find FootBack");
-                    foot = find_node_regex_any(actor, ".*((R|L).*Foot|(R|L)b.*Leg.*Tip).*");
+					foot = find_node_regex_any(actor, ".*((R|L).*Foot|(R|L)b.*Leg.*Tip).*");
 					foot_kind = Foot::Back;
 				}
-                if (!foot) {
-                    log::info("Couldnt find the foot node");
-                }
+				if (!foot) {
+					log::info("Couldnt find the foot node");
+				}
 				if (foot && impact) {
 					auto audio_manager = BSAudioManager::GetSingleton();
 					if (!audio_manager) return;
-                    log::info("Getting TES");
-					auto tes = TES::GetSingleton();
-					if (!tes) return;
-                    log::info("Getting Land");
-					NiPoint3 pos = actor->GetPosition() + foot->world.translate;
-					auto land = tes->GetLandTexture(pos);
-					if (!land) return;
-                    log::info("Getting Material");
-					auto material = land->materialType;
+					// log::info("Getting TES");
+					// auto tes = TES::GetSingleton();
+					// if (!tes) return;
+					// log::info("Getting Land");
+					// NiPoint3 pos = actor->GetPosition() + foot->world.translate;
+					// auto land = tes->GetLandTexture(pos);
+					// if (!land) return;
+					log::info("Getting Material");
+					// TODO get better way of finding material
+					auto material_id = MATERIAL_ID::kGrass;
+					auto material = BGSMaterialType::GetMaterialType(material_id);
 					if (!material) return;
 					log::info("Getting impact data set");
-                    auto imapact_data_set = get_foot_impactdata(foot_kind);
+					auto imapact_data_set = get_foot_impactdata(foot_kind);
 					if (!imapact_data_set) return;
-                    log::info("Getting impact data for material");
+					log::info("Getting impact data for material");
 					auto imapact_data = imapact_data_set->impactMap.find(material)->second;
 					if (!imapact_data) return;
-                    log::info("Creating sound info");
+					log::info("Creating sound info");
 					NiPoint3 sound_position;
 					sound_position.x = 0.0;
 					sound_position.y = 0.0;
@@ -122,7 +124,7 @@ namespace Gts {
 					sound.playSound2      = false;
 					sound.unk2A           = false;
 					sound.unk30           = nullptr;
-                    log::info("Playing sound");
+					log::info("Playing sound");
 					impact->PlayImpactDataSounds(sound);
 				}
 			}
