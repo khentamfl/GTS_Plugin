e8506ad1f330f83ffd2ad7527d370ac97c814155
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-07-27 13:33:17 +0700

    CTD Inducing Impact sounds

--- src/hooks/impact.h
+++ src/hooks/impact.h
@@ -12,5 +12,11 @@ namespace Hooks
 
 			static BSEventNotifyControl ProcessEvent(BGSImpactManager* a_this, const BGSFootstepEvent* a_event, BSTEventSource<BGSFootstepEvent>* a_eventSource);
 			static inline REL::Relocation<decltype(ProcessEvent)> _ProcessEvent;
+
+			static BSEventNotifyControl ProcessEventCombat(BGSImpactManager* a_this, const BGSCombatImpactEvent* a_event, BSTEventSource<BGSCombatImpactEvent>* a_eventSource);
+			static inline REL::Relocation<decltype(ProcessEventCombat)> _ProcessEventCombat;
+
+			static BSEventNotifyControl ProcessEventCollision(BGSImpactManager* a_this, const BGSCollisionSoundEvent* a_event, BSTEventSource<BGSCollisionSoundEvent>* a_eventSource);
+			static inline REL::Relocation<decltype(ProcessEventCollision)> _ProcessEventCollision;
 	};
 }
