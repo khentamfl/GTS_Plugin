8aa534994073ee4ecebbfc89c44bdc16b617af61
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-08-07 18:54:57 +0700

    Trying new tremor values

--- src/managers/tremor.cpp
+++ src/managers/tremor.cpp
@@ -91,12 +91,12 @@ namespace Gts {
 
 				// Camera shakes
 
-				float falloff = falloff_calc(distance, 1.539368969);
+				float falloff = falloff_calc(distance, 0.575399209);
 
-				float min_shake_scale = 1.2; // Before this no shaking
+				float min_shake_scale = 0.0; // Before this no shaking
 				float max_shake_scale = 30.0; // After this we have full power shaking
-				float power_at_min = 0.37; // Power at minimum scale and zero distance will be much lower than this at 2m due to falloff
-				float power_at_max = 46.5327215; // Power at maximum scale
+				float power_at_min = 0.0; // Power at minimum scale and zero distance will be much lower than this at 2m due to falloff
+				float power_at_max = 7.59; // Power at maximum scale
 
 				if (scale < min_shake_scale) return;
 				float power = 0.0;
@@ -105,7 +105,7 @@ namespace Gts {
 				// The equation to use
 				//
 				// FullTesting graph: https://www.desmos.com/calculator/qazgd0awcx
-				Formula formula = Formula::Power;
+				Formula formula = Formula::Linear;
 				switch (formula) {
 					case Formula::Power:
 					{
