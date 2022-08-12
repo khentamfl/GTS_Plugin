515e5ce8422bd9531c19a0f5f5e9ce3b6e55edf4
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-12 17:38:13 +0300

    Update explosive_growth.h

--- src/magic/explosive_growth.h
+++ src/magic/explosive_growth.h
@@ -7,5 +7,7 @@ using namespace SKSE;
 using namespace RE;
 
 namespace Gts {
-  void explosive_growth(Actor* caster);
+  void explosive_growth1(Actor* caster);
+  void explosive_growth2(Actor* caster);
+  void explosive_growth3(Actor* caster);
 }
