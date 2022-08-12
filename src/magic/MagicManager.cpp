b3e6bfff60a5a7aedd661b9b1eb6c96cf92db496
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-12 19:09:33 +0300

    Update MagicManager.cpp

--- src/magic/MagicManager.cpp
+++ src/magic/MagicManager.cpp
@@ -8,31 +8,56 @@ namespace Gts {
   {
     TESGlobal* SizeLimit = find_form<TESGlobal>("GTS.esp|2028B4");
     TESGlobal* progression_multiplier_global = find_form<TESGlobal>("GTS.esp|37E46E");
-    float TargetScale = GetVisualScale(Target); float CasterScale = GetVisualScale(Caster);
-    SetTargetScale(OurTarget, TargetScale * 0.99935)
+    float TargetScale = Get_Visual_Scale(Target); 
+    float CasterScale = Get_Visual_Scale(Caster);
+    SetTargetScale(target, TargetScale * 0.99935)
+	    
       if (CasterScale < SizeLimit)
-    {SetTargetScale(Caster, casterScale + 0.000015)}
+   	 {
+	      SetTargetScale(Caster, casterScale + 0.000015)
+	 }
   }
   
   void ShrinkFoeAoe(Actor* caster, Actor* target)
   {
     TESGlobal* SizeLimit = find_form<TESGlobal>("GTS.esp|2028B4");
     TESGlobal* progression_multiplier_global = find_form<TESGlobal>("GTS.esp|37E46E");
-    float TargetScale = GetVisualScale(Target); float CasterScale = GetVisualScale(Caster);
-    SetTargetScale(OurTarget, TargetScale * 0.99905) //- ((ShrinkRateTotal * SMTRate) * 1 * Efficiency))
+    float TargetScale = Get_Visual_Scale(Target); 
+    float CasterScale = Get_Visual_Scale(Caster);
+    SetTargetScale(target, TargetScale * 0.99905) //- ((ShrinkRateTotal * SMTRate) * 1 * Efficiency))
       
     if (CasterScale < SizeLimit)
-    {SetTargetScale(Caster, casterScale + 0.000020)}
+   	 {
+	    SetTargetScale(Caster, casterScale + 0.000020)
+    	 }
   }
   
   void ShrinkFoeAoeMast(Actor* caster, Actor* target)
   {
     TESGlobal* SizeLimit = find_form<TESGlobal>("GTS.esp|2028B4");
     TESGlobal* progression_multiplier_global = find_form<TESGlobal>("GTS.esp|37E46E");
-    float TargetScale = GetVisualScale(Target); float CasterScale = GetVisualScale(Caster);
-    SetTargetScale(OurTarget, TargetScale * 0.99705)
+    float TargetScale = Get_Visual_Scale(Target); 
+    float CasterScale = Get_Visual_Scale(Caster);
+    SetTargetScale(target, TargetScale * 0.99705)
       
       if (CasterScale < SizeLimit)
-    {SetTargetScale(Caster, casterScale + 0.000025}
+   	 {
+	      SetTargetScale(Caster, casterScale + 0.000025)
+	 }
+  }
+			     
+ void SwordOfSize(Actor* caster, Actor* target)
+  {
+    TESGlobal* SizeLimit = find_form<TESGlobal>("GTS.esp|2028B4");
+    TESGlobal* progression_multiplier_global = find_form<TESGlobal>("GTS.esp|37E46E");
+    float TargetScale = Get_Visual_Scale(Target); 
+    float CasterScale = Get_Visual_Scale(Caster);
+	  if (TargetScale > 0.10)
+	  {SetTargetScale(Target, TargetScale * 1 - 0.00280)}
+      
+      if (CasterScale < SizeLimit)
+   	 {
+	      SetTargetScale(Caster, casterScale + 0.00096)
+	 }
   }
 }
