6c13c0718e4c32b5f378d49963f4db7bd0d39437
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-13 09:14:02 +0300

    Another try

--- src/magic/MagicManager.cpp
+++ src/magic/MagicManager.cpp
@@ -104,7 +104,7 @@ namespace Gts {
     {AdditionalShrinkValue = 2.0;}
 
     if (TargetScale < 0.25 && target->HasMagicEffect(runtime.ShrinkToNothing) == false)
-    {CastSpell(ShrinkToNothingSpell, caster, target)}
+    {ShrinkToNothing->CastSpell(caster, target)}
 
     float AlterationLevel = (caster->GetActorValue(ActorValue::kAlteration) * 0.00166 / 50) * AdditionalShrinkValue;
 
