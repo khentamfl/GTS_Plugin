e31c779a8438ca1cd909c83b2c24f1824ab88156
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-15 23:45:07 +0300

    Another Typo

--- src/magic/effects/absorb_effect.cpp
+++ src/magic/effects/absorb_effect.cpp
@@ -7,10 +7,10 @@
 namespace Gts {
 	bool Absorb::StartEffect(EffectSetting* effect) {
 		auto& runtime = Runtime::GetSingleton();
-		return (effect == runtime.AbsorbMGEF);
+		return (effect == runtime.AbsorbMGEF || effect == runtime.TrueAbsorb);
 	}
 
-	void SlowGrow::OnUpdate() {
+	void Absorb::OnUpdate() {
 		auto caster = GetCaster();
 		if (!caster) {
 			return;
@@ -25,7 +25,17 @@ namespace Gts {
 		float casterScale = get_visual_scale(caster);
         float targetScale = get_visual_scale(target);
         float SizeDifference = casterScale/targetScale;
+        if (effect == runtime.AbsorbMGEF) {
+		mod_target_scale(target, -0.0825 * ProgressionMultiplier * SizeDifference);
+        mod_target_scale(caster, 0.0025 * ProgressionMultiplier * SizeDifference);
+        if (targetScale <= 0.25)
+        {caster->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant)->CastSpellImmediate(runtime.FakeCrushSpell, false, target, 1.00f, false, 0.0f, caster);} 
+        // ^ Emulate absorption
+        }
+	    
 
-		mod_target_scale(caster, -0.0025 * ProgressionMultiplier * SizeDifference);
-	}
-}
+       else if (effect == runtime.AbsorbMGEF) {
+		mod_target_scale(target, -0.0025 * ProgressionMultiplier * SizeDifference);
+        mod_target_scale(caster, 0.0005 * ProgressionMultiplier * SizeDifference);}
+	
+}}
