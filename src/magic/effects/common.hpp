ef9375076e0337746d95418ad8a8df5173d8c346
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-15 16:31:22 +0300

    Typo

--- src/magic/effects/common.hpp
+++ src/magic/effects/common.hpp
@@ -47,7 +47,7 @@ namespace Gts {
 		float AlterationLevel = (caster->GetActorValue(ActorValue::kAlteration) * 0.00166 / 50) * AdditionalShrinkValue * DualCast;
 
 		if (TargetScale <= 0.25 && target->HasSpell(runtime.ShrinkToNothingSpell) == false)
-		{caster->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant->Cast(runtime.ShrinkToNothingSpell, false, target, 1.00f, false, 0.0f, caster));}
+		{caster->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant)->Cast(runtime.ShrinkToNothingSpell, false, target, 1.00f, false, 0.0f, caster);}
 
 		float stolen_amount = (TargetScale * 0.0015 + AlterationLevel * SMTRate * Efficiency) * power;
 		mod_target_scale(target, -stolen_amount);
