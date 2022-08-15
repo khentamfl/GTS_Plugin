53218f29dd29d4fe39a4b4c94436335c0c1e80c0
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-15 23:25:02 +0300

    .hpp

--- src/magic/effects/SizeRelatedDamage.cpp
+++ src/magic/effects/SizeRelatedDamage.cpp
@@ -1,10 +1,11 @@
-#include "magic/effects/shrink.hpp"
+#include "magic/effects/SizeRelatedDamage.hpp"
 #include "magic/effects/common.hpp"
 #include "magic/magic.hpp"
 #include "scale/scale.hpp"
 #include "data/runtime.hpp"
 
 namespace Gts {
+
 	bool SizeDamage::StartEffect(EffectSetting* effect) {
         auto& runtime = Runtime::GetSingleton();
 		return (effect == runtime.SizeRelatedDamage0 || effect == runtime.SizeRelatedDamage1 || effect == runtime.SizeRelatedDamage2);
@@ -19,7 +20,6 @@ namespace Gts {
 		if (!target) {
 			return;
 		}
-        CallCrush = caster->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant)->CastSpellImmediate(runtime.FakeCrushSpell, false, target, 1.00f, false, 0.0f, caster);
 		auto& runtime = Runtime::GetSingleton();
 		float size_limit = runtime.sizeLimit->value;
 		float ProgressionMultiplier = runtime.ProgressionMultiplier->value;
@@ -29,9 +29,14 @@ namespace Gts {
         float SizeDifference = casterScale/targetScale;
         if (target->IsPlayerTeammate() == true && runtime.GtsNPCEffectImmunityToggle->value == 1.0)
         {return;} // Do not apply if those are true
-        if (SizeDifference >= 4.0 && target->IsDead() && target->IsPlayerTeammate() == false) // We don't want to crush allies
-        {CallCrush();} // < Crush only if size difference is > than 4.0
-        else if (SizeDifference >= 24.0)
-        {CallCrush();} // < Crush anyway, no conditions needed since size difference is too massive
+
+         if (SizeDifference >= 24.0)
+        {caster->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant)->CastSpellImmediate(runtime.FakeCrushSpell, false, target, 1.00f, false, 0.0f, caster);} 
+        // ^ Crush anyway, no conditions needed since size difference is too massive
+
+        else if (SizeDifference >= 4.0 && target->IsDead() && target->IsPlayerTeammate() == false) // We don't want to crush allies
+        {caster->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant)->CastSpellImmediate(runtime.FakeCrushSpell, false, target, 1.00f, false, 0.0f, caster);} 
+        // ^ Crush only if size difference is > than 4.0
+       
 	}
 }
