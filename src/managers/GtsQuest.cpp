ade0b750e14c023d5ae8bdc87020a7e71bf1dde8
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-27 00:20:57 +0300

    Fixes

--- src/managers/GtsQuest.cpp
+++ src/managers/GtsQuest.cpp
@@ -24,18 +24,18 @@ namespace Gts {
     if (QuestStage > 100.0 || QuestStage < 20.0)
     {return;} //Do not calculate if true
 
-    float SizeLimit = runtime.SizeLimit->value;
+    float SizeLimit = runtime.sizeLimit->value;
     float SelectedFormula = runtime.SelectedSizeFormula->value;
 
-    float PlayerSize = get_visual_scale(Player);
+    float PCScale = get_visual_scale(Player);
     log::info("Quest Stage is: {}", GtsQuest->GetCurrentStageID());
     if (SelectedFormula >= 2.0)
-    {SizeLimit = runtime.AdjustMaxSize_MassBased->value;}
+    {SizeLimit = runtime.MassBasedSizeLimit->value;}
 
     if (QuestStage >= 40 && QuestStage < 60 && Player->IsInCombat() == false && PCScale > 1.00)
-        {mod_target_scale(Player, -0.00040)} // Shrink Stage 1 (Normal)
+        {mod_target_scale(Player, -0.00040);} // Shrink Stage 1 (Normal)
 
     if (QuestStage >= 60 && QuestStage < 70 && Player->IsInCombat() == false && PCScale > 1.00)
-        {mod_target_scale(Player, -0.00020)} // Shrink Stage 2 (Less)
+        {mod_target_scale(Player, -0.00020);} // Shrink Stage 2 (Less)
     }
 }
