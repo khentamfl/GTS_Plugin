06234419d5a4eb00901c90e63b8f709a2ff79abf
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-28 09:48:14 +0300

    Test

--- src/magic/effects/growth.cpp
+++ src/magic/effects/growth.cpp
@@ -11,10 +11,30 @@ namespace Gts {
 
 	bool Growth::StartEffect(EffectSetting* effect) { // NOLINT
 		auto& runtime = Runtime::GetSingleton();
+		const float BASE_POWER = 0.00125;
+		const float DUAL_CAST_BONUS = 2.0;
+
+		auto caster = GetCaster();
+		if (!caster) {
+			return;
+		}
+		auto target = GetTarget();
+		if (!target) {
+			return;
+		}
+
+		float power = BASE_POWER;
+		if (IsDualCasting()) {
+			power *= DUAL_CAST_BONUS;
+		}
+
+		Grow(caster, 0.0, power);
 		return effect == runtime.GrowthSpell;
+
 	}
 
 	void Growth::OnUpdate() {
+		return;
 		const float BASE_POWER = 0.00125;
 		const float DUAL_CAST_BONUS = 2.0;
 
