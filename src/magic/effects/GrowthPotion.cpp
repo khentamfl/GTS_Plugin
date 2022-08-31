35b1a6ece80c2e48b90525b4b1aae84e7da4c272
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-08-31 13:55:42 +0300

    Fix Conditions

--- src/magic/effects/GrowthPotion.cpp
+++ src/magic/effects/GrowthPotion.cpp
@@ -22,10 +22,10 @@ namespace Gts {
     }
 
 	void GrowthPotion::OnUpdate() {
-		const float BASE_POWER = 0.00150;
-		const float DUAL_CAST_BONUS = 2.0;
+		const float BASE_POWER = 0.00025;
 		auto& runtime = Runtime::GetSingleton();
         float delta_time = *g_delta_time;
+        float GrowthTickIs = this->SoundTick;
 
 		auto caster = GetCaster();
 		if (!caster) {
@@ -33,19 +33,22 @@ namespace Gts {
 		}
 
         if (caster == PlayerCharacter::GetSingleton()) {
-            GrowthTremorManager::GetSingleton().CallRumble(caster, caster, 0.5);
+            GrowthTremorManager::GetSingleton().CallRumble(caster, caster, 0.4);
         
         }
-        if (this->SoundTick <= 0.0)
+        if (GrowthTickIs <= 0.0)
 		{
 		    auto GrowthSound = runtime.growthSound;
-		    float Volume = clamp(0.25, 2.0, get_visual_scale(caster));
+		    float Volume = clamp(0.25, 2.0, get_visual_scale(caster)/2);
 		    PlaySound(GrowthSound, caster, Volume, 0.0);
 		}
-		this->SoundTick +=delta_time;
+		
 
-		if (this->SoundTick >= 160.0)
-		{this->SoundTick = 0.0;}
+		if (GrowthTickIs >= 160.0)
+		{this->SoundTick <= 0.0;}
+
+        this->SoundTick +=delta_time;
+
         float Power = BASE_POWER;
 
 		Grow(caster, 0.0, Power);
