eee99d32d54fc9c72bc25955b29bc6593bc3997b
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-09-01 10:14:34 +0300

    Remove reapply from GameMode

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -107,7 +107,7 @@ namespace {
 		SoftPotential& MS_adjustment = Persistent::GetSingleton().MS_adjustment;
 		float speed_mult = soft_core(scale, speed_adjustment);
 		if (GtsManager::GetSingleton().GetFrameNum() % 30 == 0) {
-		{persi_actor_data->anim_speed = speed_mult;}
+		persi_actor_data->anim_speed = speed_mult;
 
 		float MS_mult = soft_core(scale, MS_adjustment);
 		float Bonus = AttributeManager::GetSingleton().Augmentation();
@@ -116,14 +116,18 @@ namespace {
 		{
 		if (actor->IsWalking() == true) {
 			actor->SetActorValue(ActorValue::kSpeedMult, (trans_actor_data->base_walkspeedmult * (Bonus/3 + 1.0)) * 0.44 / MS_mult);
+			log::info("Adjusting PC MS of {}, {}", actor->GetDisplayFullName(), trans_actor_data->base_walkspeedmult);
 		} else if (actor->IsSprinting() == true) {
 			actor->SetActorValue(ActorValue::kSpeedMult, (trans_actor_data->base_walkspeedmult * (Bonus/3 + 1.0)) * 1.25 / MS_mult);
+			log::info("Adjusting PC MS of {}, {}", actor->GetDisplayFullName(), trans_actor_data->base_walkspeedmult);
 		}
 		else {
 			actor->SetActorValue(ActorValue::kSpeedMult, (trans_actor_data->base_walkspeedmult + (Bonus/3 + 1.0))/ MS_mult);
-		}}
+			log::info("Adjusting PC MS of {}, {}", actor->GetDisplayFullName(), trans_actor_data->base_walkspeedmult);
+			}
+		}
 
-		if (!actor->formID != 0x14)
+		if (!actor->formID == 0x14)
 		{
 		if (actor->IsWalking() == true) {
 			actor->SetActorValue(ActorValue::kSpeedMult, trans_actor_data->base_walkspeedmult * 0.60 / MS_mult);
@@ -275,7 +279,6 @@ namespace {
 		auto& runtime = Runtime::GetSingleton();
 		float size_limit = runtime.sizeLimit->value;
 		auto& Persist = Persistent::GetSingleton();
-		apply_speed(actor, saved_data, temp_data, force);
 
 		if (size_limit < 1.0)
 		{size_limit = 1.0;} // Avoid bugs
