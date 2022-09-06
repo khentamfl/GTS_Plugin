0d7a96bd133e7f5d7b9951045126f4d7ed6e8381
QEAndy <sheepchaan@gmail.com>
2022-09-06 18:41:46 +0700

    3 times slower instead of 10

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -52,7 +52,7 @@ namespace {
 					persi_actor_data->target_scale,
 					persi_actor_data->target_scale_v,
 					max_scale,
-					persi_actor_data->half_life*10,
+					persi_actor_data->half_life*3,
 					Time::WorldTimeDelta()
 					);
 			}
