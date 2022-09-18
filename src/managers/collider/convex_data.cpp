c9f6aa0a28628727a55f3dc0bf4599856101866e
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-09-18 11:23:58 +0700

    Disable conex verts: radius, aabb and center changes

--- src/managers/collider/convex_data.cpp
+++ src/managers/collider/convex_data.cpp
@@ -37,12 +37,12 @@ namespace Gts {
 		log::info("- Scaling ConvexVerts: {}", reinterpret_cast<std::uintptr_t>(this->convex));
 		log::info("  - Scale: {}", scale);
 		auto shape = this->convex;
-		shape->radius = this->radius * scale;
-		log::info("  - Radius: {} -> {}", this->radius, shape->radius);
-		shape->aabbHalfExtents = this->aabbHalfExtents * vecScale;
-		log::info("  - aabbHalfExtents: {} -> {}", Vector2Str(this->aabbHalfExtents), Vector2Str(shape->aabbHalfExtents));
-		shape->aabbCenter = this->aabbCenter * vecScale;
-		log::info("  - aabbCenter: {} -> {}", Vector2Str(this->aabbCenter), Vector2Str(shape->aabbCenter));
+		// shape->radius = this->radius * scale;
+		// log::info("  - Radius: {} -> {}", this->radius, shape->radius);
+		// shape->aabbHalfExtents = this->aabbHalfExtents * vecScale;
+		// log::info("  - aabbHalfExtents: {} -> {}", Vector2Str(this->aabbHalfExtents), Vector2Str(shape->aabbHalfExtents));
+		// shape->aabbCenter = this->aabbCenter * vecScale;
+		// log::info("  - aabbCenter: {} -> {}", Vector2Str(this->aabbCenter), Vector2Str(shape->aabbCenter));
 
 		std::size_t numRotatedVerticies = this->rotated_verts.size();
 		for (std::size_t i = 0; i < numRotatedVerticies; i++) {
