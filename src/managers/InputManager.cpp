cb14eda46e416f262eb98041a920b75abc3a15f9
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-09-19 15:10:29 +0300

    Correct logging

--- src/managers/InputManager.cpp
+++ src/managers/InputManager.cpp
@@ -28,24 +28,31 @@ namespace Gts {
 		}
 		for (auto event = *a_event; event; event = event->next) {
 			if (event->GetEventType() != INPUT_EVENT_TYPE::kButton) {
+				log::info("ButtonEvent Type != Button");
 				continue;
 			}
 			ButtonEvent* buttonEvent = event->AsButtonEvent();
 			if (!buttonEvent || (!buttonEvent->IsPressed() && !buttonEvent->IsUp())) {
+				log::info("ButtonEvent aren't pressed");
 				continue;
+				
 			}
 			if (buttonEvent->device.get() != INPUT_DEVICE::kKeyboard) {
+				log::info("ButtonEvent != Keyboard");
 				continue;
 			}
 			auto key = buttonEvent->GetIDCode();
 			if (key == 0x45) {
 				// Do attack
+				ConsoleLog::GetSingleton()->Print("Pressed E");
 				log::info("0x45 pressed");
 			} else if (key == 0x1) {
 				// Do attack left
+				ConsoleLog::GetSingleton()->Print("Pressed LMB");
 				log::info("0x01 pressed");
 			} else if (key == 0x2) {
 				// Do attack right
+				ConsoleLog::GetSingleton()->Print("Pressed RMB");
 				log::info("0x02 pressed");
 			}
 		}
