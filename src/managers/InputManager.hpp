7f81df3cbdb147f25be26c824fc9802027eeb51c
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-09-19 05:12:23 +0300

    Not Virtual

--- src/managers/InputManager.hpp
+++ src/managers/InputManager.hpp
@@ -15,7 +15,7 @@ namespace Gts
 		public:
 			[[nodiscard]] static InputManager& GetSingleton() noexcept;
 
-			virtual EventResult ProcessEvent(RE::InputEvent* const* a_event, RE::BSTEventSource<RE::InputEvent*>) override;
+			EventResult ProcessEvent(RE::InputEvent* const* a_event, RE::BSTEventSource<RE::InputEvent*>) override;
 			void Start() override;
 	};
 }
