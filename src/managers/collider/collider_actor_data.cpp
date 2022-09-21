0039f5bd886ccbc9cf233f8b31ceda4b050e2dbc
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-09-21 12:54:47 +0700

    Enable lists

--- src/managers/collider/collider_actor_data.cpp
+++ src/managers/collider/collider_actor_data.cpp
@@ -39,9 +39,9 @@ namespace Gts {
 		// for (auto &[key, data]: this->convex_data) {
 		// 	data.ApplyScale(new_scale, vec_scale);
 		// }
-		// for (auto &[key, data]: this->list_data) {
-		// 	data.ApplyScale(new_scale, vec_scale);
-		// }
+		for (auto &[key, data]: this->list_data) {
+			data.ApplyScale(new_scale, vec_scale);
+		}
 	}
 
 	void ColliderActorData::ApplyPose(Actor* actor, const float& new_scale) {
