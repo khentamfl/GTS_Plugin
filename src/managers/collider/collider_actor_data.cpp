53b54e4d06d3c215c6355b78a6f40fad8e074159
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-09-21 11:36:42 +0700

    Only capsule scales

--- src/managers/collider/collider_actor_data.cpp
+++ src/managers/collider/collider_actor_data.cpp
@@ -33,9 +33,9 @@ namespace Gts {
 	}
 
 	void ColliderActorData::ApplyScale(const float& new_scale, const hkVector4& vec_scale) {
-		// for (auto &[key, data]: this->capsule_data) {
-		// 	data.ApplyScale(new_scale, vec_scale);
-		// }
+		for (auto &[key, data]: this->capsule_data) {
+			data.ApplyScale(new_scale, vec_scale);
+		}
 		// for (auto &[key, data]: this->convex_data) {
 		// 	data.ApplyScale(new_scale, vec_scale);
 		// }
