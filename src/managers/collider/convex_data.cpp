3e44f97b81fc58ce32f1e2ba3f4f4eed762c2d78
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-09-23 11:12:28 +0700

    Scale convex shapes relative to lowest vertex position

--- src/managers/collider/convex_data.cpp
+++ src/managers/collider/convex_data.cpp
@@ -31,6 +31,8 @@ namespace Gts {
 				.z = shape->rotatedVertices[i].vertices[2]}
 				);
 		}
+
+		this->SetOriginMinZ();
 	}
 
 	ConvexData::~ConvexData () {
@@ -43,18 +45,57 @@ namespace Gts {
 			log::error("Vertext count mismatch");
 		}
 		// shape->radius = this->radius * scale;
+		hkVector4 origin = hkVector4(this->origin.x, this->origin.y, this->origin.z, 0.0);
+
 		shape->aabbHalfExtents = this->aabbHalfExtents * vecScale;
-		shape->aabbCenter = this->aabbCenter * vecScale;
+		shape->aabbCenter = (this->aabbCenter - origin) * vecScale + origin;
 
 		std::size_t numVertices = shape->numVertices;
 		std::size_t numRotatedVerticies = numVertices / 4;
 		if (numVertices % 4 != 0) {
 			numRotatedVerticies += 1;
 		}
+		hkVector4 x_origin = hkVector4(this->origin.x);
+		hkVector4 y_origin = hkVector4(this->origin.y);
+		hkVector4 z_origin = hkVector4(this->origin.z);
 		for (std::size_t i = 0; i< numRotatedVerticies; i++) {
-			shape->rotatedVertices[i].vertices[0] = this->rotated_verts[i].x * vecScale;
-			shape->rotatedVertices[i].vertices[1] = this->rotated_verts[i].y * vecScale;
-			shape->rotatedVertices[i].vertices[2] = this->rotated_verts[i].z * vecScale;
+			shape->rotatedVertices[i].vertices[0] = (this->rotated_verts[i].x - x_origin) * vecScale + x_origin;
+			shape->rotatedVertices[i].vertices[1] = (this->rotated_verts[i].y - y_origin) * vecScale + y_origin;
+			shape->rotatedVertices[i].vertices[2] = (this->rotated_verts[i].z - z_origin) * vecScale + z_origin;
 		}
+	}
+
+	void ConvexData::SetOrigin(const float& x, const float& y, const float& z) {
+		this->origin = NiPoint3(x, y, z);
+	}
+
+	void ConvexData::SetOrigin(const NiPoint3& point) {
+		this->origin = point;
+	}
+
+	void ConvexData::SetOriginMinZ() {
+		if (this->rotated_verts.size() == 0) {
+			return;
+		}
+		float lowest_z = this->rotated_verts[0].z.quad.m128_f32[0];
+		NiPoint3 lowest = NiPoint3(
+			this->rotated_verts[0].x.quad.m128_f32[0],
+			this->rotated_verts[0].y.quad.m128_f32[0],
+			this->rotated_verts[0].z.quad.m128_f32[0]
+			);
+		for (auto& rotated_vert: this->rotated_verts) {
+			for (std::size_t j = 0; j<4; j++) {
+				if (rotated_vert.z.quad.m128_f32[j] < lowest_z) {
+					lowest_z = rotated_vert.z.quad.m128_f32[j];
+					NiPoint3 lowest = NiPoint3(
+						rotated_vert.x.quad.m128_f32[j],
+						rotated_vert.y.quad.m128_f32[j],
+						rotated_vert.z.quad.m128_f32[j]
+						);
+				}
+			}
+		}
+
+		this->SetOrigin(lowest);
 	}
 }
--- src/managers/collider/convex_data.hpp
+++ src/managers/collider/convex_data.hpp
@@ -20,11 +20,16 @@ namespace Gts {
 		hkVector4 aabbCenter;
 		std::uint32_t numVertices;
 		std::vector<RotatedVerts> rotated_verts;
+		NiPoint3 origin = NiPoint3(0.0, 0.0, 0.0);
+
 		ConvexData(hkpConvexVerticesShape* orig_capsule);
 		ConvexData(ConvexData&& old) : convex(std::move(old.convex)), radius(std::move(old.radius)), aabbHalfExtents(std::move(old.aabbHalfExtents)), aabbCenter(std::move(old.aabbCenter)), rotated_verts(std::move(old.rotated_verts)) {
 		};
 		~ConvexData();
 
 		void ApplyScale(const float& scale, const hkVector4& vecScale);
+		void SetOrigin(const float& x, const float& y, const float& z);
+		void SetOrigin(const NiPoint3& point);
+		void SetOriginMinZ();
 	};
 }
