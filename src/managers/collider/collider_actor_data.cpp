5fecbaf7c9708b58a5e13ddcb6d710003152a6b7
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-09-30 17:49:05 +0700

    Spam a whole bunch of stuff

--- src/managers/collider/collider_actor_data.cpp
+++ src/managers/collider/collider_actor_data.cpp
@@ -101,7 +101,15 @@ namespace Gts {
 		}
 
 		if (actor->formID == 0x14) {
-			log::info("Center: {}", charController->center);
+			log::info("== Frame ==");
+			log::info("  Center: {}", charController->center);
+			log::info("  pushDelta: {}", Vector2Str(charController->pushDelta));
+			log::info("  pitchAngle: {}", Vector2Str(charController->pitchAngle));
+			log::info("  rollAngle: {}", Vector2Str(charController->rollAngle));
+			log::info("  pitchMult: {}", Vector2Str(charController->pitchMult));
+			log::info("  actorHeight: {}", Vector2Str(charController->actorHeight));
+			log::info("  speedPct: {}", Vector2Str(charController->speedPct));
+			log::info("");
 		}
 
 		const float EPSILON = 1e-3;
