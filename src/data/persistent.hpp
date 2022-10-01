cc33bc4102bb4446a5ca96bec4b72eb0b013cf04
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-10-01 04:04:30 +0300

    Playing with values

--- src/data/persistent.hpp
+++ src/data/persistent.hpp
@@ -59,15 +59,15 @@ namespace Gts {
 			float tremor_scale = 1.0;
 			float npc_tremor_scale = 1.0;
 			SoftPotential speed_adjustment {
-				.k = 0.125, // 0.125
-				.n = 0.20, // 0.86
+				.k = 0.07, // 0.125
+				.n = 0.86, // 0.86
 				.s = 1.12,
 				.o = 1.0,
 				.a = -0.0,  //Default is 0
 			};
 			SoftPotential MS_adjustment {
 				.k = 0.132, // 0.132
-				.n = 0.20, // 0.86
+				.n = 0.86, // 0.86
 				.s = 1.12,
 				.o = 1.0,
 				.a = 0.0, //Default is 0
