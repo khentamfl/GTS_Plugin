33bd68045878047e82c323e7ef0ef45c5a908c5f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-10-03 09:06:58 +0300

    Typo

--- src/managers/hitmanager.hpp
+++ src/managers/hitmanager.hpp
@@ -18,6 +18,6 @@ namespace Gts
 			private:
 			bool CanGrow = false;
 			inline static float BonusPower = 1.0;
-			inline static float GrowthTick = 1.0;
+			inline static float GrowthTick = 0.0;
 	};
 }
