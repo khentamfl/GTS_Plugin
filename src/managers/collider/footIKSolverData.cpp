317ed3cbbed80aced0a4579016bf4e6630917ea2
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-10-05 13:29:49 +0700

    Don't do edits

--- src/managers/collider/footIKSolverData.cpp
+++ src/managers/collider/footIKSolverData.cpp
@@ -8,6 +8,11 @@ namespace Gts {
 
 	FootIkSolverData::FootIkSolverData(hkaFootPlacementIkSolver* solver) {
 		this->solver = solver;
+		if (!this->solver) {
+			log::error("It is an error to add an nullptr for the solver data");
+			throw std::runtime_error("It is an error to create the solver data using nullptr");
+			return;
+		}
 		this->solver->AddReference();
 		log::info("solver memSizeAndFlags: {:X}", this->solver->memSizeAndFlags);
 		this->m_footEndLS = solver->m_setup.m_footEndLS;
@@ -23,13 +28,12 @@ namespace Gts {
 	}
 
 	void FootIkSolverData::ApplyScale(const float& new_scale, const hkVector4& vecScale) {
-		log::info("Checking: {}, RawName: {}", reinterpret_cast<std::uintptr_t>(this->solver), GetRawName(this->solver));
-		this->solver->m_setup.m_footEndLS = this->m_footEndLS * vecScale;
-		this->solver->m_setup.m_footPlantedAnkleHeightMS = this->m_footPlantedAnkleHeightMS * new_scale;
-		this->solver->m_setup.m_footRaisedAnkleHeightMS = this->m_footRaisedAnkleHeightMS * new_scale;
-		this->solver->m_setup.m_maxAnkleHeightMS = this->m_maxAnkleHeightMS  * new_scale;
-		this->solver->m_setup.m_minAnkleHeightMS = this->m_minAnkleHeightMS * new_scale;
-		this->solver->m_setup.m_raycastDistanceUp = this->m_raycastDistanceUp * new_scale;
-		this->solver->m_setup.m_raycastDistanceDown = this->m_raycastDistanceDown * new_scale;
+		// this->solver->m_setup.m_footEndLS = this->m_footEndLS * vecScale;
+		// this->solver->m_setup.m_footPlantedAnkleHeightMS = this->m_footPlantedAnkleHeightMS * new_scale;
+		// this->solver->m_setup.m_footRaisedAnkleHeightMS = this->m_footRaisedAnkleHeightMS * new_scale;
+		// this->solver->m_setup.m_maxAnkleHeightMS = this->m_maxAnkleHeightMS  * new_scale;
+		// this->solver->m_setup.m_minAnkleHeightMS = this->m_minAnkleHeightMS * new_scale;
+		// this->solver->m_setup.m_raycastDistanceUp = this->m_raycastDistanceUp * new_scale;
+		// this->solver->m_setup.m_raycastDistanceDown = this->m_raycastDistanceDown * new_scale;
 	}
 }
