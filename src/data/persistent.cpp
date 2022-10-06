133af4a3d0f3727ed5e2cc86a95b5e7505de81f9
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-10-06 10:25:16 +0300

    Typo

--- src/data/persistent.cpp
+++ src/data/persistent.cpp
@@ -38,7 +38,7 @@ namespace Gts {
 		std::uint32_t size;
 		std::uint32_t version;
 
-		SizeManager::GetSingleton().Start();
+		SizeManager::GetSingleton().Reset();
 
 		while (serde->GetNextRecordInfo(type, version, size)) {
 			if (type == ActorDataRecord) {
