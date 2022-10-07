828caa5b1e0e9c0dd717949fc1eb7c153084080f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-10-07 13:05:36 +0300

    Edited wrong line

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -142,9 +142,9 @@ namespace {
 		float MS_mult = soft_core(scale, MS_adjustment);
 		float Bonus = Persistent::GetSingleton().GetActorData(actor)->smt_run_speed;
 		float MS_mult_sprint_limit = clamp(0.65, 1.0, MS_mult); // For sprint
-		float MS_mult_limit = clamp(0.10, 1.0, MS_mult); // For Walk speed
+		float MS_mult_limit = clamp(0.750, 1.0, MS_mult); // For Walk speed
 		float Multy = clamp(0.75, 1.0, MS_mult); // Additional 25% ms
-		float WalkSpeedLimit = clamp(0.33, 1.0, MS_mult);
+		float WalkSpeedLimit = clamp(0.10, 1.0, MS_mult);
 		float PerkSpeed = 1.0;
 
 		static Timer timer = Timer(0.10); // Run every 0.10s or as soon as we can
