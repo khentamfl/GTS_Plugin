4cae9c7be6bb6e0d9a22f63b9e812ce5aac85d5b
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-10-07 12:47:08 +0300

    True disable bloodgush

--- src/magic/effects/common.hpp
+++ src/magic/effects/common.hpp
@@ -201,13 +201,13 @@ namespace Gts {
 		}
 		if (target_scale <= SHRINK_TO_NOTHING_SCALE && !target->HasMagicEffect(runtime.ShrinkToNothing) && !target->IsPlayerTeammate()) {
 			caster->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant)->CastSpellImmediate(runtime.ShrinkToNothingSpell, false, target, 1.00f, false, 0.0f, caster);
-			if (runtime.BloodGushSound) {
-				PlaySound(runtime.BloodGushSound, target, 1.0, 1.0);
-			}
-			else
-			{
-				log::info("SOUND NOT FOUND!");
-			}
+			//if (runtime.BloodGushSound) {
+				//PlaySound(runtime.BloodGushSound, target, 1.0, 1.0);
+			//}
+			//else
+			//{
+				//log::info("SOUND NOT FOUND!");
+			//}
 			AdjustSizeLimit(0.0117);
 			auto Cache = runtime.ManualGrowthStorage;
 			if (caster->formID == 0x14 && caster->HasPerk(runtime.SizeReserve)) {
