7f63cd269df8f6271bd4092337424ceda04de1d0
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-10-16 07:40:42 +0700

    Fully apply scale to Foot IK rays

--- src/managers/collider/footIKSolverData.cpp
+++ src/managers/collider/footIKSolverData.cpp
@@ -17,24 +17,24 @@ namespace Gts {
 			return;
 		}
 		// log::info("solver memSizeAndFlags: {:X}", this->solver->memSizeAndFlags);
-		// this->m_footEndLS = solver->m_setup.m_footEndLS;
-		// this->m_footPlantedAnkleHeightMS = solver->m_setup.m_footPlantedAnkleHeightMS;
-		// this->m_footRaisedAnkleHeightMS = solver->m_setup.m_footRaisedAnkleHeightMS;
-		// this->m_maxAnkleHeightMS = solver->m_setup.m_maxAnkleHeightMS;
-		// this->m_minAnkleHeightMS = solver->m_setup.m_minAnkleHeightMS;
-		// this->m_raycastDistanceUp = solver->m_setup.m_raycastDistanceUp;
-		// this->m_raycastDistanceDown = solver->m_setup.m_raycastDistanceDown;
+		this->m_footEndLS = solver->m_setup.m_footEndLS;
+		this->m_footPlantedAnkleHeightMS = solver->m_setup.m_footPlantedAnkleHeightMS;
+		this->m_footRaisedAnkleHeightMS = solver->m_setup.m_footRaisedAnkleHeightMS;
+		this->m_maxAnkleHeightMS = solver->m_setup.m_maxAnkleHeightMS;
+		this->m_minAnkleHeightMS = solver->m_setup.m_minAnkleHeightMS;
+		this->m_raycastDistanceUp = solver->m_setup.m_raycastDistanceUp;
+		this->m_raycastDistanceDown = solver->m_setup.m_raycastDistanceDown;
 	}
 	FootIkSolverData::~FootIkSolverData() {
 	}
 
 	void FootIkSolverData::ApplyScale(const float& new_scale, const hkVector4& vecScale) {
-		// this->solver->m_setup.m_footEndLS = this->m_footEndLS * vecScale;
-		// this->solver->m_setup.m_footPlantedAnkleHeightMS = this->m_footPlantedAnkleHeightMS * new_scale;
-		// this->solver->m_setup.m_footRaisedAnkleHeightMS = this->m_footRaisedAnkleHeightMS * new_scale;
-		// this->solver->m_setup.m_maxAnkleHeightMS = this->m_maxAnkleHeightMS  * new_scale;
-		// this->solver->m_setup.m_minAnkleHeightMS = this->m_minAnkleHeightMS * new_scale;
-		// this->solver->m_setup.m_raycastDistanceUp = this->m_raycastDistanceUp * new_scale;
-		// this->solver->m_setup.m_raycastDistanceDown = this->m_raycastDistanceDown * new_scale;
+		this->solver->m_setup.m_footEndLS = this->m_footEndLS * vecScale;
+		this->solver->m_setup.m_footPlantedAnkleHeightMS = this->m_footPlantedAnkleHeightMS * new_scale;
+		this->solver->m_setup.m_footRaisedAnkleHeightMS = this->m_footRaisedAnkleHeightMS * new_scale;
+		this->solver->m_setup.m_maxAnkleHeightMS = this->m_maxAnkleHeightMS  * new_scale;
+		this->solver->m_setup.m_minAnkleHeightMS = this->m_minAnkleHeightMS * new_scale;
+		this->solver->m_setup.m_raycastDistanceUp = this->m_raycastDistanceUp * new_scale;
+		this->solver->m_setup.m_raycastDistanceDown = this->m_raycastDistanceDown * new_scale;
 	}
 }
