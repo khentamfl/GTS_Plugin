e2082ae2164bfae87b99a3dd616471d2b468e9e0
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-10-17 21:07:25 +0700

    Check the world from model transform

--- src/managers/collider/collider_actor_data.cpp
+++ src/managers/collider/collider_actor_data.cpp
@@ -47,6 +47,12 @@ namespace {
 			for (auto& graph : animGraphManager->graphs) {
 				if (graph) {
 					auto& character = graph->characterInstance;
+
+					auto& worldFromModel = character.worldFromModel.get();
+					if (worldFromModel) {
+						log::info("worldFromModel: {}", GetRawName(worldFromModel));
+					}
+
 					auto ragdollDriver = character.ragdollDriver.get();
 					if (ragdollDriver) {
 						auto ragdoll = ragdollDriver->ragdoll;
