42da4201d317caa6fc1c1c232024dd2b326fd99a
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-11-07 11:33:03 +0300

    Return if no perk found

--- src/managers/InputManager.cpp
+++ src/managers/InputManager.cpp
@@ -105,9 +105,13 @@ namespace Gts {
 					log::info("l.Shift + E is True");
 					// Currently >>>DISABLED<<<. It reports values < 300.0 when someone is far away for some reason.
 					auto player = PlayerCharacter::GetSingleton();
+
+					if (!player->HasPerk(Runtime::GetSingleton().VorePerk)) {
+						return;
+					}
+
 					auto voreMan = Vore::GetSingleton();
 					auto prey = voreMan.GetPlayerVoreTarget();
-
 
 					if (prey) {
 						log::info("Distance between PC and {} is {}", prey->GetDisplayFullName(), get_distance_to_actor(player, prey));
