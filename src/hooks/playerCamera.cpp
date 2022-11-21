add3fbf35326d08297d79df65ecaa1a13255dc06
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-11-21 09:13:21 +0700

    Debug print the hook

--- src/hooks/playerCamera.cpp
+++ src/hooks/playerCamera.cpp
@@ -16,21 +16,27 @@ namespace Hooks
 	void Hook_PlayerCamera::SetCameraRoot(PlayerCamera* a_this, NiPointer<NiNode> a_root) {
 		log::info("Player camera SetCameraRoot");
 		if (Gts::Plugin::InGame()) {
+			log::info("In Game");
 			auto player = PlayerCharacter::GetSingleton();
 			if (player) {
+				log::Info("Player exists");
 				if (a_root) {
+					log::info("Getting name");
 					log::trace("  - Node {}", a_root->name);
 				}
+				log::info("Getting model");
 				auto model = player->GetCurrent3D();
 				if (model) {
 					auto node = model->AsNode();
 					if (node) {
+						log::info("Alternative Set");
 						_SetCameraRoot(a_this, NiPointer<NiNode>(node));
+						return;
 					}
-					return;
 				}
 			}
 		}
+		log::info("Nomral Set");
 		_SetCameraRoot(a_this, a_root);
 	}
 
