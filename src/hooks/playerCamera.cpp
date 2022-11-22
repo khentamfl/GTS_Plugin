3760bad2c4f7d347805dbf2630984f9c4e227d6c
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-11-22 11:18:38 +0700

    As an pointer of NiNode

--- src/hooks/playerCamera.cpp
+++ src/hooks/playerCamera.cpp
@@ -14,11 +14,11 @@ namespace Hooks
 		_Update = Vtbl.write_vfunc(0x02, Update);
 	}
 
-	void Hook_PlayerCamera::SetCameraRoot(TESCamera* a_this, NiNode a_root) {
+	void Hook_PlayerCamera::SetCameraRoot(TESCamera* a_this, NiNode* a_root) {
 		log::info("Player camera SetCameraRoot");
 		log::info("Grabbing RAW NAME");
 		log::info("a_this: {}", Gts::GetRawName(a_this));
-		log::info("a_root: {}", Gts::GetRawName(&a_root));
+		log::info("a_root: {}", Gts::GetRawName(a_root));
 		if (Gts::Plugin::InGame()) {
 			log::info("In Game");
 			auto player = PlayerCharacter::GetSingleton();
@@ -26,7 +26,7 @@ namespace Hooks
 				log::info("Player exists");
 				if (a_root) {
 					log::info("Getting name");
-					log::trace("  - Node {}", a_root.name);
+					log::trace("  - Node {}", a_root->name);
 				}
 				log::info("Getting model");
 				auto model = player->GetCurrent3D();
