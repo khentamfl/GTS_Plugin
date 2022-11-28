978fb1a30c5af010a571ceddc5340c809ba597f4
QuantumEntangledAndy <sheepchaan@gmail.com>
2022-11-28 15:30:47 +0700

    Player camera update hooks

--- src/hooks/playerCamera.cpp
+++ src/hooks/playerCamera.cpp
@@ -11,48 +11,11 @@ namespace Hooks
 	void Hook_PlayerCamera::Hook() {
 		logger::info("Hooking PlayerCamera");
 		REL::Relocation<std::uintptr_t> Vtbl{ RE::VTABLE_PlayerCamera[0] };
-		// _SetCameraRoot = Vtbl.write_vfunc(0x01, SetCameraRoot);
 		_Update = Vtbl.write_vfunc(0x02, Update);
 	}
 
-	void Hook_PlayerCamera::SetCameraRoot(TESCamera* a_this, NiNode* a_root) {
-		log::info("Player camera SetCameraRoot");
-		log::info("Grabbing RAW NAME");
-		log::info("a_this: {}", Gts::GetRawName(a_this));
-		log::info("a_root: {}", Gts::GetRawName(a_root));
-		if (Gts::Plugin::InGame()) {
-			log::info("In Game");
-			auto player = PlayerCharacter::GetSingleton();
-			if (player) {
-				log::info("Player exists");
-				if (a_root) {
-					log::info("Getting name");
-					log::trace("  - Node {}", a_root->name);
-				}
-				log::info("Getting model");
-				auto model = player->GetCurrent3D();
-				if (model) {
-					auto node = model->AsNode();
-					if (node) {
-						log::info("Alternative Set");
-						_SetCameraRoot(a_this, node);
-						return;
-					}
-				}
-			}
-		}
-		log::info("Normal Set");
-		_SetCameraRoot(a_this, a_root);
-	}
-
-	void Hook_PlayerCamera::Update(TESCamera* a_this) {
-		auto camera = PlayerCamera::GetSingleton();
-		auto third = skyrim_cast<ThirdPersonState*>(camera->cameraStates[CameraState::kThirdPerson].get());
-		log::info("Pre PlayerCamera Update");
-		log::info("cam node pos: {}::{}", Vector2Str(third->thirdPersonCameraObj->world.translate), Vector2Str(third->thirdPersonCameraObj->local.translate));
+	void Hook_PlayerCamera::Update(PlayerCamera* a_this) {
+		log::info("Hook_PlayerCamera::Update");
 		_Update(a_this);
-		log::info("Post PlayerCamera Update");
-		log::info("cam node pos: {}::{}", Vector2Str(third->thirdPersonCameraObj->world.translate), Vector2Str(third->thirdPersonCameraObj->local.translate));
-
 	}
 }
--- src/hooks/playerCamera.hpp
+++ src/hooks/playerCamera.hpp
@@ -11,11 +11,7 @@ namespace Hooks
 		public:
 			static void Hook();
 		private:
-
-			static void SetCameraRoot(TESCamera* a_this, NiNode* a_root);
-			static inline REL::Relocation<decltype(SetCameraRoot)> _SetCameraRoot;
-
-			static void Update(TESCamera* a_this);
+			static void Update(PlayerCamera* a_this);
 			static inline REL::Relocation<decltype(Update)> _Update;
 	};
 }
