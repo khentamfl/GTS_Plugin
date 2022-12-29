53219d59c6708970fa6bdf42b49c36ab5deba75e
Sermitse <108320865+Sermitse@users.noreply.github.com>
2022-12-29 18:32:08 +0300

    Fixes

--- src/managers/RipClothManager.cpp
+++ src/managers/RipClothManager.cpp
@@ -30,6 +30,7 @@ namespace Gts {
         float scale = get_target_scale(player);
         int AllSlots[] = {30, 32, 33, 34, 37}; // Should strip only these slots
         int RandomSlot = rand() % 5; // Randomly choose slot to strip
+        auto Armor = player->GetWornArmor(AllSlots[RandomSlot]);
         static Timer timer = Timer(4.5);
         if (timer.ShouldRunFrame()) {
 		    if (!player || scale <= 2.5) {
@@ -42,11 +43,11 @@ namespace Gts {
                 this->clothtearcount = 0.0;
                 this->clothtearthreshold = 2.5; // reset stuff
              }
-             if (player->GetWornArmor(AllSlots[RandomSlot]) != nullptr && this->clothtearthreshold !< Scale)
+             if (Armor != nullptr && scale >= this->clothtearthreshold)
             {
                 this->clothtearthreshold += rand() % 75; 
                 this->clothtearcount +=1.0;
-                player->unequipItem(true, player->GetWornArmor(AllSlots[RandomSlot]));
+                Armor->unequipItem(true, Armor);
                 Runtime::PlaySound("ClothTearSound", player, 1.0, 1.0);
                 Runtime::PlaySound("MoanSound", player, 1.0, 1.0);
                 GrowthTremorManager::GetSingleton().CallRumble(player, player, 2 * scale);
