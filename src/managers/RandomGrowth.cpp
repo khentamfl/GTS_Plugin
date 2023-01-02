64b432a5aa0c6da7bbf725d92503dc27df9b4146
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-01-03 01:37:25 +0300

    ?

--- src/managers/RandomGrowth.cpp
+++ src/managers/RandomGrowth.cpp
@@ -21,7 +21,7 @@ namespace {
 			return false;
 		}
 		int RNG = rand()% 50;
-		RandomizeGrowth(RNG);
+		RandomGrowth::RandomizeGrowth(RNG);
 
 		if (SizeManager::GetSingleton().BalancedMode() == 2.0) {
 			MultiplySlider = 1.0; // Disable effect in Balance Mode, so slider is always 1.0
