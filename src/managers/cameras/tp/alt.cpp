ccd92f0a4bc1f327f16fed6a30f8afedb7086ad1
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-01-10 05:06:43 +0300

    Test it, prepare example

--- src/managers/cameras/tp/alt.cpp
+++ src/managers/cameras/tp/alt.cpp
@@ -37,9 +37,45 @@ namespace Gts {
 	}
 
 	std::vector<std::string> Alt::GetBoneTargets() {
+		//if (Function.GetValue() == 0.0) {
+		//	return {};
+		//};
+		//else if (Function.GetValue() == 1.0) { // Spine0
+			//return {
+		//	 "NPC Spine [Spn0]",	
+		//	};
+		//};
+	//	else if (Function.GetValue() == 2.0) { // Spine1
+			//return {
+			// "NPC Spine1 [Spn1]",	
+			//};
+		//};
+		//else if (Function.GetValue() == 3.0) { // Breasts, non 3BA
+			//return {
+			// "NPC L Breast",	
+			// "NPC R Breast",
+			//};
+		//};
+	//	else if (Function.GetValue() == 4.0) { // 3BA Breasts
+			//return {
+			// "L Breast02",	
+			// "R Breast02",
+			//};
+		//};
+		//else if (Function.GetValue() == 5.0) { // Neck
+			//return {
+			// "NPC Neck [Neck]",	
+			//};
+		//};
+		//else if (Function.GetValue() == 6.0) { // Butt
+			//return {
+			// "NPC L Butt",	
+			// "NPC R Butt",	
+			//};
+		//};
 		return {
-		        "BONEA",
-		        "BONEB",
+		    "L Breast02",	
+			"R Breast02",
 		};
 	}
 }
