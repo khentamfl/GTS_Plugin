5dd8cbcca5d686879b7239f3af64a4fee20f7813
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-01-10 08:25:32 +0700

    Fixes

--- src/managers/cameras/fpState.hpp
+++ src/managers/cameras/fpState.hpp
@@ -6,7 +6,6 @@ using namespace RE;
 namespace Gts {
 	class FirstPersonCameraState : public CameraState {
 		public:
-			virtual NiPoint3 GetPlayerLocalOffset(const NiPoint3& cameraPos) override;
 			virtual NiPoint3 GetPlayerLocalOffsetProne(const NiPoint3& cameraPos) override;
 			virtual bool PermitManualEdit() override;
 
