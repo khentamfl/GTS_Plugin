a5960fba6bfb270aedb1ea5886aa67f1d2bbf5d5
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-01-10 16:46:37 +0700

    Test for (node)

--- src/managers/highheel.cpp
+++ src/managers/highheel.cpp
@@ -146,38 +146,40 @@ namespace Gts {
 					auto model = actor->Get3D(first);
 					if (model) {
 						auto node = model->GetObjectByName(addonString);
-						{
-							NiExtraData* extraData = node->GetExtraData("HH_OFFSET");
-							if (extraData) {
-								log::info("Extra");
-								NiFloatExtraData* floatData = netimmerse_cast<NiFloatExtraData*>(extraData);
-								if (floatData) {
-									log::info("ExtraFloat");
-									return fabs(floatData->value) > 1e-4;
+						if (node) {
+							{
+								NiExtraData* extraData = node->GetExtraData("HH_OFFSET");
+								if (extraData) {
+									log::info("Extra");
+									NiFloatExtraData* floatData = netimmerse_cast<NiFloatExtraData*>(extraData);
+									if (floatData) {
+										log::info("ExtraFloat");
+										return fabs(floatData->value) > 1e-4;
+									}
 								}
 							}
-						}
-						{
-							NiExtraData* extraData = node->GetExtraData("SDTA");
-							if (extraData) {
-								log::info("Extra2");
-								NiStringExtraData* stringData = netimmerse_cast<NiStringExtraData*>(extraData);
-								if (stringData) {
-									log::info("ExtraString");
-									std::string jsonData = stringData->value;
-									yaml_source ar(jsonData);
-									for (auto node: ar) {
-										RaceMenuSDTA instance;
-										node >> instance;
-										if (instance.name == "NPC") {
-											log::info("NPC Extracted");
-											if (instance.pos.size() > 2) {
-												return fabs(instance.pos[2]) > 1e-4;
+							{
+								NiExtraData* extraData = node->GetExtraData("SDTA");
+								if (extraData) {
+									log::info("Extra2");
+									NiStringExtraData* stringData = netimmerse_cast<NiStringExtraData*>(extraData);
+									if (stringData) {
+										log::info("ExtraString");
+										std::string jsonData = stringData->value;
+										yaml_source ar(jsonData);
+										for (auto node: ar) {
+											RaceMenuSDTA instance;
+											node >> instance;
+											if (instance.name == "NPC") {
+												log::info("NPC Extracted");
+												if (instance.pos.size() > 2) {
+													return fabs(instance.pos[2]) > 1e-4;
+												}
 											}
 										}
-									}
 
-									return fabs(floatData.value) > 1e-4;
+										return fabs(floatData.value) > 1e-4;
+									}
 								}
 							}
 						}
