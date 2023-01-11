b3d8304518f270a06b1f21ba7d414571b43c177c
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-01-11 11:23:36 +0300

    Order fix

--- src/managers/highheel.cpp
+++ src/managers/highheel.cpp
@@ -54,12 +54,14 @@ namespace Gts {
 		bool adjusted = false;
 		NiPoint3 new_hh = this->GetHHOffset(actor);
 		float hh_length = new_hh.Length();
-		if (Runtime::GetBool("ProneEnabled") && actor->formID == 0x14 && actor->IsSneaking()) {
-			npc_root_node->local.translate = 0;
-			return;
-		}
+
 		for (bool person: {false, true}) {
 			auto npc_root_node = find_node(actor, "NPC", person);
+
+			if (Runtime::GetBool("ProneEnabled") && actor->formID == 0x14 && actor->IsSneaking()) {
+				npc_root_node->local.translate = 0;
+				return;
+			}
 			if (npc_root_node) {
 				NiPoint3 current_value = npc_root_node->local.translate;
 				NiPoint3 delta = current_value - new_hh;
