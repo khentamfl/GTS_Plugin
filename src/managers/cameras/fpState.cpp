3e71a23fb6aec8b8f925d0311ca60d5e5eaee760
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-01-12 14:07:35 +0700

    Fix

--- src/managers/cameras/fpState.cpp
+++ src/managers/cameras/fpState.cpp
@@ -10,16 +10,6 @@ namespace  Gts {
 		return clamp(0.25, 20.0, 3.0 * Runtime::GetFloat("ProneOffsetFP"));
 	}
 
-	NiPoint3 FirstPersonCameraState::GetPlayerLocalOffsetProne(const NiPoint3& cameraPos) {
-		NiPoint3 pos = this->GetPlayerLocalOffset(cameraPos);
-		auto player = PlayerCharacter::GetSingleton();
-		if (player) {
-			auto scale = get_visual_scale(player);
-			pos += this->ProneAdjustment(cameraPos)*scale;
-		}
-		return pos;
-	}
-
 	bool FirstPersonCameraState::PermitManualEdit() {
 		return false;
 	}
