73b15256c8703a591228244b9687f94db73edc7b
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-01-12 16:59:34 +0700

    Acually enable scale

--- src/scale/modscale.cpp
+++ src/scale/modscale.cpp
@@ -37,8 +37,8 @@ namespace Gts {
 		auto first_model = actor->Get3D(true);
 		if (first_model) {
 			result = true;
-			//first_model->local.scale = target_scale;
-			//update_node(first_model);
+			first_model->local.scale = target_scale;
+			update_node(first_model);
 		}
 		return result;
 	}
@@ -58,8 +58,8 @@ namespace Gts {
 		auto first_node = find_node(actor, node_name, true);
 		if (first_node) {
 			result = true;
-			//first_node->local.scale = target_scale;
-			//update_node(first_node);
+			first_node->local.scale = target_scale;
+			update_node(first_node);
 		}
 		return result;
 	}
