b88f8d3ceaeed13eb858705b914ac8985fb64f4c
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-01-24 16:56:31 +0300

    Adjustments

--- src/managers/damage/AccurateDamage.cpp
+++ src/managers/damage/AccurateDamage.cpp
@@ -143,8 +143,8 @@ namespace Gts {
 		// Make a list of points to check
 		std::vector<NiPoint3> points = {
 			NiPoint3(0.0, 0.0, -(hh * 0.3)), // The standard at the foot position
-			NiPoint3(0.0, 7.7 + (hh/60), -0.6 + -(hh * 0.7)), // Offset it forward 
-			NiPoint3(0.0, (hh/60), -hh * 0.7), // Offset for HH
+			NiPoint3(-2.0, 7.7 + (hh/60), -0.6 + -(hh * 0.9)), // Offset it forward 
+			NiPoint3(-2.0, (hh/60), -hh * 0.9), // Offset for HH
 		};
 		std::tuple<NiAVObject*, NiAVObject*, NiAVObject*> left(leftFoot, leftToe, leftCalf);
 		std::tuple<NiAVObject*, NiAVObject*, NiAVObject*> right(rightFoot, rightToe, rightCalf);
