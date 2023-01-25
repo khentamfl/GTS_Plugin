4d6c712ba2ab0e6d94c18b4ad54e67b1ca91fde3
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-01-26 00:55:54 +0300

    Fix clamp

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -353,7 +353,7 @@ namespace {
 					//log::info("Curse Of Growth, GameMode PC: {}, NPC: {}", Runtime::GetInt("ChosenGameMode"), Runtime::GetInt("ChosenGameModeNPC"));
 					float CalcAv = actor->GetActorValue(ActorValue::kAlteration);
 					float MaxSize = Runtime::GetFloat("CurseOfGrowthMaxSize");                                       // Slider that determines max size cap.
-					float sizelimit = clamp((1.0, MaxSize, 1.00 * (CalcAv/100 * MaxSize)));                          // Size limit between 1 and [Slider]], based on Alteration. Cap is Slider value.
+					float sizelimit = clamp(1.0, MaxSize, 1.00 * (CalcAv/100 * MaxSize));                          // Size limit between 1 and [Slider]], based on Alteration. Cap is Slider value.
 					int Random = rand() % 20;                                                                        // Randomize power
 					int GrowthTimer = rand() % 7;                                                                    // Randomize 're-trigger' delay, kinda
 					int StrongGrowthChance = rand() % 20;                                                            // Self-explanatory
