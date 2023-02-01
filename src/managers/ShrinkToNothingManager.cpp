8463506bfde6f4b914c8450f9482ad3e3c9ed4a2
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-02-01 04:15:26 +0300

    Bigger random

--- src/managers/ShrinkToNothingManager.cpp
+++ src/managers/ShrinkToNothingManager.cpp
@@ -131,7 +131,7 @@ namespace Gts {
 			return;
 		}
 
-		int random = (100 + (rand()% 24 + 1)) / 100;
+		int random = (100 + (rand()% 85 + 1)) / 100;
 
 		if (GtsSkillLevel->value >= 100) {
 			GtsSkillLevel->value = 100.0;
