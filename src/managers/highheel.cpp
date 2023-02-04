9e580af8107a277c524b1f167ee8a86ff04ba3a6
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-02-04 12:56:36 +0300

    Base Voluime prints

--- src/managers/highheel.cpp
+++ src/managers/highheel.cpp
@@ -48,8 +48,12 @@ namespace Gts {
 		if (!actor->Is3DLoaded()) {
 			return;
 		}
-
+		auto transient = Transient::GetSingleton().GetActorData(actor);
+		if (!transient) {
+			return;
+		}
 		NiPoint3 new_hh;
+		log::info("Actor: {}, BaseHeight: {}, BaseVolume: {}", transient->base_height, transient->base_volume);
 		if (IsProne(actor) || !Persistent::GetSingleton().highheel_correction) {
 			new_hh = NiPoint3();
 		} else if (Persistent::GetSingleton().size_method != SizeMethod::ModelScale) {
