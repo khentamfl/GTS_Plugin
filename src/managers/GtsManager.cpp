dec81bdd809192d702a16c630fbc8d9818f621e0
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-02-05 11:35:26 +0300

    Swim = 36

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -156,7 +156,7 @@ namespace {
 		if (charCont) {
 			if (actor->formID == 0x14) {
 				float defaultjump = 1.0 * scale;
-				float swim = 1.6 * scale;
+				float swim = 36.0;
 				charCont->jumpHeight = defaultjump;
 				charCont->swimFloatHeight = swim;
 				charCont->scale = scale;
