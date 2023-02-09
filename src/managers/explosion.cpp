b952cd117f3171cdf018e7c3e7ccb85d152f7cee
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-02-09 06:22:31 +0300

    ->

--- src/managers/explosion.cpp
+++ src/managers/explosion.cpp
@@ -46,11 +46,11 @@ namespace {
 				return;
 			}
 			log::info("Base Explosion True, scale {}, radius: {}, scale: {}", explosion->radius * scale, explosion->imodRadius * scale, scale);
-			explosion.EXPLOSION_RUNTIME_DATA_CONTENT().SetPosition(position);
-			explosion.EXPLOSION_RUNTIME_DATA_CONTENT().radius *= scale;
-			explosion.EXPLOSION_RUNTIME_DATA_CONTENT().imodRadius *= scale;
-			explosion.EXPLOSION_RUNTIME_DATA_CONTENT().unkB8 = nullptr;
-			explosion.EXPLOSION_RUNTIME_DATA_CONTENT().negativeVelocity *= 0.0;
+			explosion->EXPLOSION_RUNTIME_DATA_CONTENT().SetPosition(position);
+			explosion->EXPLOSION_RUNTIME_DATA_CONTENT().radius *= scale;
+			explosion->EXPLOSION_RUNTIME_DATA_CONTENT().imodRadius *= scale;
+			explosion->EXPLOSION_RUNTIME_DATA_CONTENT().unkB8 = nullptr;
+			explosion->EXPLOSION_RUNTIME_DATA_CONTENT().negativeVelocity *= 0.0;
 		}
 	}
 }
