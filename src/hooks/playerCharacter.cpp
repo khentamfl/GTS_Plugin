2f83fab153328ff6a6b9cd345a5b078e3421067a
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-02-09 11:52:36 +0300

    Fixes

--- src/hooks/playerCharacter.cpp
+++ src/hooks/playerCharacter.cpp
@@ -44,7 +44,7 @@ namespace Hooks
 	void Hook_PlayerCharacter::ProcessTracking(PlayerCharacter* a_this, float a_delta, NiAVObject* a_obj3D)
 	{
 		if (a_obj3D) {
-			log::info("Object Name: {}, Location L: {}, Location W: {}", a_obj3D->GetObjectByName(), Vector2str(a_obj3D->local), Vector2str(a_obj3D->world));
+			log::info("Location L: {}, Location W: {}", Vector2Str(a_obj3D->local), Vector2Str(a_obj3D->world));
 		}
 		_ProcessTracking(a_this, a_delta, a_obj3D);
 	}
