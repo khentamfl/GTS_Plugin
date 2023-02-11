a7183d035a528a808293d3404bf91a2370173e90
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-02-11 10:10:33 +0300

    +, not *

--- src/managers/Attributes.cpp
+++ src/managers/Attributes.cpp
@@ -253,7 +253,7 @@ namespace Gts {
 			}
 			case ActorValue::kAttackDamageMult: {
 				if (Runtime::HasMagicEffect(giant, "SmallMassiveThreat")) {
-					scale *= 3.0;
+					scale += 3.0;
 				}
 				float bonusDamageMultiplier = Runtime::GetFloatOr("bonusDamageMultiplier", 1.0);
 				float damage_storage = 1.0 + ((bonusDamageMultiplier) * scale - 1.0);
