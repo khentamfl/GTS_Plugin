0405d989626862a882593759c2c6e79e5d28a8e9
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-02-12 13:06:05 +0700

    Dummy free

--- src/utils/actorUtils.cpp
+++ src/utils/actorUtils.cpp
@@ -28,14 +28,6 @@ namespace {
 }
 
 RE::ExtraDataList::~ExtraDataList() {
-	size_t a_size;
-	if (SKYRIM_REL_CONSTEXPR (REL::Module::IsAE()) && (REL::Module::get().version() >= SKSE::RUNTIME_SSE_1_6_629)) {
-		a_size = 0x20;
-	} else {
-		a_size = 0x18;
-	}
-	std::memset(this, 0, a_size);
-	std::free(this);
 }
 
 namespace Gts {
