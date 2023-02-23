51820d8b2022ceb4131961746bf520f9e11e7663
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-02-23 23:26:46 +0300

    Try to disable HH when Furniture is not nullptr

--- src/managers/highheel.cpp
+++ src/managers/highheel.cpp
@@ -51,7 +51,7 @@ namespace Gts {
 		}
 		NiPoint3 new_hh;
 		//log::info("Actor: {}, BaseHeight: {}, BaseVolume: {}", actor->GetDisplayFullName(), transient->base_height, transient->base_volume);
-		if (IsProne(actor) || !Persistent::GetSingleton().highheel_correction) {
+		if (IsProne(actor) || !Persistent::GetSingleton().highheel_correction || actor->GetOccupiedFurniture() != nullptr) {
 			new_hh = NiPoint3();
 		} else if (Persistent::GetSingleton().size_method != SizeMethod::ModelScale) {
 			new_hh = this->GetHHOffset(actor);
