7b7f7815a95b9d8c7fe1efc1b12c4dfa8d4f69e4
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-03-03 16:21:50 +0300

    Attempt to do AnimationManager

---
+++ src/managers/AnimationManager.cpp
@@ -0,0 +1,40 @@
+
+#include "managers/AnimationManager.hpp"
+#include "managers/GtsSizeManager.hpp"
+#include "managers/GrowthTremorManager.hpp"
+#include "managers/ShrinkToNothingManager.hpp"
+#include "managers/CrushManager.hpp"
+#include "magic/effects/common.hpp"
+#include "managers/GtsManager.hpp"
+#include "utils/actorUtils.hpp"
+#include "data/persistent.hpp"
+#include "data/runtime.hpp"
+#include "scale/scale.hpp"
+#include "data/time.hpp"
+#include "timer.hpp"
+#include "node.hpp"
+
+using namespace RE;
+using namespace Gts;
+
+
+
+namespace Gts {
+	AnimationManager& Animation::GetSingleton() noexcept {
+		static Animation instance;
+		return instance;
+	}
+
+	std::string AnimationManager::DebugName() {
+		return "AnimationManager";
+	}
+
+	void AnimationManager::ActorAnimEvent(const Actor& actor, const std::string_view& tag, const std::string_view& payload) {
+        if (!actor) {
+            return;
+        }
+        if (actor->formID == 0x14) {
+            log::info("Tag: {}", tag);
+             }
+        }
+	}
---
+++ src/managers/AnimationManager.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "events.hpp"
+
+using namespace std;
+using namespace SKSE;
+using namespace RE;
+
+namespace Gts
+{
+
+	class AnimationManager : public EventListener
+	{
+		public:
+			[[nodiscard]] static AnimationManager& GetSingleton() noexcept;
+
+			virtual std::string DebugName() override;
+			void ActorAnimEvent(const Actor& actor, const std::string_view& tag, const std::string_view& payload) override;
+	};
+}
