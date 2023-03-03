f553eb2df6873883ede19797a4e15ba54622f0ff
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-03-03 16:32:15 +0300

    Actor*

--- src/managers/AnimationManager.cpp
+++ src/managers/AnimationManager.cpp
@@ -29,7 +29,7 @@ namespace Gts {
 		return "AnimationManager";
 	}
 
-	void AnimationManager::ActorAnimEvent(const Actor& actor, const std::string_view& tag, const std::string_view& payload) {
+	void AnimationManager::ActorAnimEvent(Actor* actor, const std::string_view& tag, const std::string_view& payload) {
         if (actor) {
             if (actor.formID == 0x14) {
                 log::info("Tag: {}", tag);
