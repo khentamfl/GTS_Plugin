f1713814bfe56ca1a98587fb2393ef8fac13147a
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-03-04 21:31:55 +0300

    A bit of sounds

--- src/managers/AnimationManager.cpp
+++ src/managers/AnimationManager.cpp
@@ -19,6 +19,21 @@
 using namespace RE;
 using namespace Gts;
 
+namespace {
+	float volume_function(float scale, const VolumeParams& params) {
+		float k = params.k;
+		float a = params.a;
+		float n = params.n;
+		float s = params.s;
+		// https://www.desmos.com/calculator/ygoxbe7hjg
+		return k*pow(s*(scale-a), n);
+	}
+
+		float frequency_function(float scale, const VolumeParams& params) {
+		float a = params.a;
+		return soft_core(scale, 0.01, 1.0, 1.0, a, 0.0)*0.5+0.5;
+	}
+}
 
 
 namespace Gts {
@@ -34,8 +49,14 @@ namespace Gts {
 	void AnimationManager::ActorAnimEvent(Actor* actor, const std::string_view& tag, const std::string_view& payload) {
         if (actor->formID == 0x14) {
             log::info("Tag: {}, payload: {}", tag, payload);
+			auto scale = get_visual_scale(actor);
+			float volume = volume_function(scale, params);
             if (tag == "GTSstompimpactR" || tag == "GTSstompimpactL") {
-                log::info("Stomp is true");
+				//Call UnderFoot event here somehow with x scale bonus
+				Runtime::PlaySound("lFootstepL", actor, volume, 1.0);
+            } 
+			if (tag == "GTSstomplandL" || tag == "GTSstomplandR") {
+				Runtime::PlaySound("lFootstepL", actor, volume * 0.5, 1.0);
             }
         }
     }
