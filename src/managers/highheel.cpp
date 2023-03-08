f414bcf3464e5f92f9a8657d1b62c21d24b6b99b
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-03-08 16:51:47 +0300

    Reset the cache

--- src/managers/highheel.cpp
+++ src/managers/highheel.cpp
@@ -66,11 +66,11 @@ namespace Gts {
 	}
 
 	void HighHeelManager::PapyrusUpdate() {
-		const bool FORCE_APPLY = false;
-		auto actors = find_actors();
-		for (auto actor: actors) {
+		//const bool FORCE_APPLY = false;
+		//auto actors = find_actors();
+		//for (auto actor: actors) {
 			//ApplyHH(actor, FORCE_APPLY);
-		}
+		//}
 	}
 
 	void HighHeelManager::ActorEquip(Actor* actor) {
