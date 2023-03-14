16c9f772f1af902519cbd0f368c4a8447bbf6fd7
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-03-14 12:28:45 +0700

    Ok overload like this

--- src/data/runtime.cpp
+++ src/data/runtime.cpp
@@ -107,14 +107,14 @@ namespace Gts {
 		}
 	}
   void Runtime::PlaySoundAtNode(const std::string_view& tag, Actor* actor, const float& volume, const float& frequency, const std::string_view& node) {
-    Runtime::PlaySoundAtNode(tag, actor, volume, frequency, find_node(node));
+    Runtime::PlaySoundAtNode(tag, actor, volume, frequency, find_node(actor, node));
   }
-  void Runtime::PlaySoundAtNode(const std::string_view& tag, Actor* actor, const float& volume, const float& frequency, const NiAVObject* node) {
+  void Runtime::PlaySoundAtNode(const std::string_view& tag, Actor* actor, const float& volume, const float& frequency, NiAVObject* node) {
     if (node) {
       Runtime::PlaySoundAtNode(tag, actor, volume, frequency, *node);
     }
   }
-  void Runtime::PlaySoundAtNode(const std::string_view& tag, Actor* actor, const float& volume, const float& frequency, const NiAVObject& node) {
+  void Runtime::PlaySoundAtNode(const std::string_view& tag, Actor* actor, const float& volume, const float& frequency, NiAVObject& node) {
 		auto soundDescriptor = Runtime::GetSound(tag);
 		if (!soundDescriptor) {
 			log::error("Sound invalid");
