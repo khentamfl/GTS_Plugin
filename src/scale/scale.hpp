91822e6a15fcee1bfc9e1fca6280419e3b6d61b7
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-03-15 15:06:15 +0700

    BY ref before by ptr

--- src/scale/scale.hpp
+++ src/scale/scale.hpp
@@ -7,24 +7,24 @@ using namespace RE;
 using namespace SKSE;
 
 namespace Gts {
-	void set_target_scale(Actor* actor, float height);
-  void set_target_scale(Actor& actor, float height);
-	float get_target_scale(Actor* actor);
-  float get_target_scale(Actor& actor);
-	void mod_target_scale(Actor* actor, float amt);
-  void mod_target_scale(Actor& actor, float amt);
+	void set_target_scale(Actor& actor, float height);
+  void set_target_scale(Actor* actor, float height);
+	float get_target_scale(Actor& actor);
+  float get_target_scale(Actor* actor);
+	void mod_target_scale(Actor& actor, float amt);
+  void mod_target_scale(Actor* actor, float amt);
 
-	void set_max_scale(Actor* actor, float height);
-  void set_max_scale(Actor& actor, float height);
-	float get_max_scale(Actor* actor);
-  float get_max_scale(Actor& actor);
-	void mod_max_scale(Actor* actor, float amt);
-  void mod_max_scale(Actor& actor, float amt);
+	void set_max_scale(Actor& actor, float height);
+  void set_max_scale(Actor* actor, float height);
+	float get_max_scale(Actor& actor);
+  float get_max_scale(Actor* actor);
+	void mod_max_scale(Actor& actor, float amt);
+  void mod_max_scale(Actor* actor, float amt);
 
-	float get_visual_scale(Actor* actor);
-  float get_visual_scale(Actor& actor);
-	float get_natural_scale(Actor* actor);
-  float get_natural_scale(Actor& actor);
-	float get_effective_scale(Actor* actor);
-  float get_effective_scale(Actor& actor);
+	float get_visual_scale(Actor& actor);
+  float get_visual_scale(Actor* actor);
+	float get_natural_scale(Actor& actor);
+  float get_natural_scale(Actor* actor);
+	float get_effective_scale(Actor& actor);
+  float get_effective_scale(Actor* actor);
 }
