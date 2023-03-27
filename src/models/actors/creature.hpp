573a4cd9f1e2482e7065bb0037236d771c80026d
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-03-27 14:17:43 +0700

    Start adding the creature variant

---
+++ src/models/actors/creature.hpp
@@ -0,0 +1,10 @@
+// Creature variant of GActor
+//
+// Obvious differences lie in the name of the nodes
+// and lack of things like Hh and animations
+
+#include "models/actors/gactor.hpp"
+
+class Creature: public GActor {
+
+}
