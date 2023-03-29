fb82859f20789a1338b422c5c272ae5e571b3480
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-03-29 09:20:59 +0700

    Only teleport when far away

--- src/managers/vore.cpp
+++ src/managers/vore.cpp
@@ -246,7 +246,7 @@ namespace Gts {
 					}
 
 
-          if (deltaLength >= 0.0) {
+          if (deltaLength >= 70.0) {
             // WARP if > 1m
             auto ragDoll = GetRagdoll(tiny_is_actor);
             hkVector4 delta = hkVector4(deltaLocation.x/70.0, deltaLocation.y/70.0, deltaLocation.z/70, 1.0);
