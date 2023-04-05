3fb564a689ecc9bd77c144b916e7c0e10bfcc315
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-05 23:20:08 +0300

    They should be 0

--- src/managers/emotions/EmotionManager.hpp
+++ src/managers/emotions/EmotionManager.hpp
@@ -21,14 +21,14 @@ namespace Gts {
 
 			private:
             std::vector<Spring> Phenomes = {
-                Spring(1.0, 0.2), //0 - 0.0
-                Spring(0.5, 0.2), //0 - 0.5
-                Spring(0.0, 0.2), //0 - 1.0
-                Spring(0.8, 0.2), //0 - 0.8
+                Spring(0.0, 0.25), //0 - 0.0
+                Spring(0.0, 0.25), //0 - 0.5
+                Spring(0.0, 0.25), //0 - 1.0
+                Spring(0.0, 0.25), //0 - 0.8
             };
             std::vector<Spring> Modifiers = {
-                Spring(0.8, 0.2), // 0 - 0.8
-                Spring(0.8, 0.2), // 0 - 0.8
+                Spring(0.0, 0.25), // 0 - 0.8
+                Spring(0.0, 0.25), // 0 - 0.8
             };
 	};
 	class EmotionManager : public EventListener {
