8acfedef14e6be2d715e47ec99f2a153a40819dc
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-05 05:52:12 +0300

    void

--- src/managers/emotions/EmotionManager.hpp
+++ src/managers/emotions/EmotionManager.hpp
@@ -10,7 +10,7 @@ using namespace RE;
 namespace Gts {
     class EmotionData {
 		public:
-            UpdateEmotions(Actor* giant);
+            void UpdateEmotions(Actor* giant);
 			EmotionData(Actor* giant);
 			void Update();
 			Actor* giant;
