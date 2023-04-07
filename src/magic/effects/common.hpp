df74b56b2df459ea7e23beda801f5a11644c28a2
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-08 01:07:17 +0300

    Fixes

--- src/magic/effects/common.hpp
+++ src/magic/effects/common.hpp
@@ -206,8 +206,8 @@ namespace Gts {
 		effeciency = clamp(0.0, 1.0, effeciency);
 		float amount = CalcPower(from, scale_factor, bonus);
 		float target_scale = get_visual_scale(from);
-		AdjustSizeLimit(0.0016 * scale_factor * target_scale, to);
-		AdjustGtsSkill(0.90 * scale_factor * target_scale, to);
+		AdjustSizeLimit(0.0012 * scale_factor * target_scale, to);
+		AdjustGtsSkill(0.60 * scale_factor * target_scale, to);
 		mod_target_scale(from, -amount);
 		mod_target_scale(to, amount*effeciency/10); // < 10 times weaker size steal towards caster. Absorb exclusive.
 	}
--- src/managers/damage/SizeHitEffects.cpp
+++ src/managers/damage/SizeHitEffects.cpp
@@ -123,9 +123,9 @@ namespace Gts {
 		float SizeDifference = get_visual_scale(receiver)/get_visual_scale(attacker);
 
 		if (receiver->formID == 0x14 && Runtime::HasPerk(receiver, "GrowthOnHitPerk") && sizemanager.GetHitGrowth(receiver) >= 1.0) {
-			float GrowthValue = std::clamp((damage/1500) * SizeHunger * Gigantism, 0.0f, 1.15f);
+			float GrowthValue = std::clamp((-damage/1500) * SizeHunger * Gigantism, 0.0f, 1.15f);
 			log::info("GrowthValue of : {} is {} {}, OG damage: {}", receiver->GetDisplayFullName(), GrowthValue, -GrowthValue, damage);
-			mod_target_scale(receiver, -GrowthValue);
+			mod_target_scale(receiver, GrowthValue);
 			
 			Runtime::PlaySoundAtNode("growthSound", receiver, GrowthValue * 600, 1.0, "NPC COM [COM ]");
 			if (ShrinkChance >= 11) {
@@ -140,9 +140,9 @@ namespace Gts {
 		} 
 		else if (BalanceMode >= 2.0 && receiver->formID == 0x14 && !Runtime::HasPerk(receiver, "GrowthOnHitPerk")) {
 			if (get_visual_scale(receiver) > 1.0) {
-				float ShrinkValue = std::clamp((damage/500)/SizeHunger/Gigantism, 0.0f, -0.35f);
+				float ShrinkValue = std::clamp((damage/500)/SizeHunger/Gigantism, 0.0f, 0.35f);
 				log::info("ShrinkValue of : {} is {} {}", receiver->GetDisplayFullName(), ShrinkValue, -ShrinkValue);
-				mod_target_scale(receiver, ShrinkValue);
+				mod_target_scale(receiver, -ShrinkValue);
 			}
 		}
 	}
