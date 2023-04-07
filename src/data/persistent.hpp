9f770f9086fabcc864175346d0f23253b4750ba6
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-07 05:23:03 +0300

    Feet Tracking is True by default

--- src/data/persistent.hpp
+++ src/data/persistent.hpp
@@ -74,7 +74,7 @@ namespace Gts {
 			bool vore_allowplayervore = false;
 			bool vore_combatonly = true;
 			bool devourment_compatibility = false;
-			bool allow_feetracking = false;
+			bool allow_feetracking = true;
 			float tremor_scale = 1.0;
 			float npc_tremor_scale = 1.0;
 			SoftPotential speed_adjustment {
