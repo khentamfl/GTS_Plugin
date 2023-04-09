94e230ebf1dac875f4fc84e817861c42b3e7f83d
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-09 07:16:07 +0300

    ,

--- src/managers/cameras/tp/alt.cpp
+++ src/managers/cameras/tp/alt.cpp
@@ -162,7 +162,7 @@ namespace Gts {
 			}
 			case 13: {
 				return BoneTarget { // R feet + L Feet
-				        .boneNames = {,
+				        .boneNames = {
 						"NPC R PreRearCalf",
 						"NPC R Foot [Rft ]",
 						"NPC L PreRearCalf",
