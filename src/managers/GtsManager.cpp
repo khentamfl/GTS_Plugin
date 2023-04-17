3cd6cb186a6651ca83ad961da4d9bb839bf56170
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-17 22:52:51 +0300

    Just compile already

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -65,9 +65,9 @@ namespace {
 			}
 		}*/
 		bhkCharacterController* CharController = actor->GetCharController();
-		const auto bhkCharacterController& = CharController; 
+		//const auto bhkCharacterController = CharController&; 
 		if (CharController) {
-			actor->UpdateCharacterControllerSimulationSettings(Controller);
+			//actor->UpdateCharacterControllerSimulationSettings(Controller);
 			actor->UpdateFadeSettings(CharController);
 			log::info("Normal Height of {} : {}", actor->GetDisplayFullName(), CharController->actorHeight);
 			CharController->scale = get_visual_scale(actor);
