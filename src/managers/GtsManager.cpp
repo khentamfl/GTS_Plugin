1e093b697b7f757e13e6e85f8684dd2c6c29f091
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-17 22:39:59 +0300

    ffs

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -65,7 +65,7 @@ namespace {
 			}
 		}*/
 		bhkCharacterController* CharController = actor->GetCharController();
-		const bhkCharacterController& Controller = CharController; 
+		const& bhkCharacterController Controller = CharController; 
 		if (CharController) {
 			actor->UpdateCharacterControllerSimulationSettings(Controller);
 			actor->UpdateFadeSettings(CharController);
