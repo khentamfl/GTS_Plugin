76502eb0f6db9ab29adc4d1581340bbfa32115e3
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-17 22:49:08 +0300

    Another try

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -65,7 +65,7 @@ namespace {
 			}
 		}*/
 		bhkCharacterController* CharController = actor->GetCharController();
-		const auto Controller = &CharController; 
+		const auto bhkCharacterController& = CharController; 
 		if (CharController) {
 			actor->UpdateCharacterControllerSimulationSettings(Controller);
 			actor->UpdateFadeSettings(CharController);
