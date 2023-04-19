1d6e1c371772dd0ca1efc07f2062b1ac00a247ab
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-19 23:15:54 +0300

    .?

--- src/managers/highheel.cpp
+++ src/managers/highheel.cpp
@@ -66,7 +66,11 @@ namespace Gts {
 		if (!actor->Is3DLoaded()) { 
 			return;
 		}
-		if (Persistent::GetSingleton().highheel_furniture == false && actor->AsActorState()->sitSleepState->SIT_SLEEP_STATE::kIsSitting) {
+
+		//To-do: try this: attempt to emulate blood on the feet and stuff [BSTempEffectParticle.h]
+		//static BSTempEffectParticle* Spawn(TESObjectCELL* a_cell, float a_lifetime, const char* a_modelName, const NiPoint3& a_rotation, const NiPoint3& a_position, float a_scale, std::uint32_t a_flags, NiAVObject* a_target)
+
+		if (Persistent::GetSingleton().highheel_furniture == false && actor->AsActorState().sitSleepState->SIT_SLEEP_STATE::kIsSitting) {
 			return;
 		}
 		this->data.try_emplace(actor);
