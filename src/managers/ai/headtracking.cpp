30d0931489f6c3eed8f3567375504ec132e6c4f2
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-04-19 13:20:02 +0700

    Let's see what it is like current;y

--- src/managers/ai/headtracking.cpp
+++ src/managers/ai/headtracking.cpp
@@ -126,7 +126,7 @@ namespace Gts {
 				NiPoint3 fakeLookAt = myOneTimeHead + directionToLook;
 				fakeLookAt.z -= height * (scale - 1.0);
 
-				ai->SetHeadtrackTarget(me, fakeLookAt/70.0);
+				ai->SetHeadtrackTarget(me, fakeLookAt);
 				Profilers::Stop("Headtracking: Headtracking Fix");
 				return;
 			}
