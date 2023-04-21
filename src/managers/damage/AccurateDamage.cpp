83898cba071f43cc115e57280aa1dadd30180c3d
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-22 00:42:26 +0300

    &

--- src/managers/damage/AccurateDamage.cpp
+++ src/managers/damage/AccurateDamage.cpp
@@ -52,7 +52,7 @@ namespace {
 			for (auto otherActor: find_actors()) {
 				auto Ref = skyrim_cast<TESObjectREFR*>(otherActor);
 				if (Ref) {
-					if (otherActor->HasLineOfSight(Ref, *true)) {
+					if (otherActor->HasLineOfSight(Ref, &true)) {
 						if (otherActor != tiny && tiny->formID != 0x14) {
 							auto Faction = tiny->GetCrimeFaction();
 							tiny->ModCrimeGoldValue(Faction, true, 5);
