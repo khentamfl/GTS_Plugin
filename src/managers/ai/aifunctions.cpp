64b0b86c560fadd9c5b6a975e0b0aab61c6338db
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-27 17:49:57 +0300

    Try find_actors() instead

--- src/managers/ai/aifunctions.cpp
+++ src/managers/ai/aifunctions.cpp
@@ -44,7 +44,7 @@ namespace Gts {
 
 	void ScareActors(Actor* giant) {
 		Profilers::Start("ActorUtils: ScareActors");
-		for (auto tiny: FindSomeActors("AiActors", 1)) {
+		for (auto tiny: find_actors()) {
 			if (tiny != giant && tiny->formID != 0x14 && !IsTeammate(tiny)) {
 				if (tiny->IsDead()) {
 					return;
