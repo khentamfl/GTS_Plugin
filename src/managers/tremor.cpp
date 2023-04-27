66ab4410cba4dc88ecbeee160961181fc72d2623
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-04-27 23:35:15 +0300

    Try 1:1 scale

--- src/managers/tremor.cpp
+++ src/managers/tremor.cpp
@@ -50,10 +50,10 @@ namespace Gts {
 		float tremor_scale;
 
 		if (actor->formID != 0x14) {
-			float sizedifference = ((get_visual_scale(actor)/get_visual_scale(player)) * 0.20);
-			tremor_scale = persist.npc_tremor_scale * (sizedifference + 0.40);
+			float sizedifference = ((get_visual_scale(actor)/get_visual_scale(player)));
+			tremor_scale = persist.npc_tremor_scale * sizedifference;
 			if (IsTeammate(actor)) {
-				log::info("Is Walking, SizeDifference {}, TremorScale: {}", sizedifference, tremor_scale);
+				log::info("{} Is Walking, SizeDifference {}, TremorScale: {}", actor->GetDisplayFullName(), sizedifference, tremor_scale);
 			}
 		}
 		if (actor->formID == 0x14) {
