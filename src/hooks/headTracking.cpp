ed42c2a615c704e815eeac9aac43124632419dd8
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-05-01 09:09:02 +0700

    Once more

--- src/hooks/headTracking.cpp
+++ src/hooks/headTracking.cpp
@@ -5,10 +5,15 @@ using namespace SKSE;
 
 namespace Hooks
 {
-  static FunctionHook<void(AIProcess* a_this, Actor* a_owner, NiPoint3& a_targetPosition)> SetHeadtrackTarget(RELOCATION_ID(38850, 39887)),
-    [](auto* self, auto* owner, auto& target) {
+
+  void Hook_HeadTracking::Hook(Trampoline& trampoline) {
+    // Hook Test
+    static FunctionHook<void(AIProcess* a_this, Actor* a_owner, NiPoint3& a_targetPosition)> SetHeadtrackTarget(RELOCATION_ID(38850, 39887),
+      [](auto* a_this, auto* a_owner, auto& a_targetPosition) {
         log::info("Function HOOKED: {}, {}", owner? owner->GetDisplayFullName() : "", Vector2Str(taget));
-        SetHeadtrackTarget(self, owner, target);
+        SetHeadtrackTarget(a_this, a_owner, a_targetPosition);
         return;
-    });
+      }
+    );
+  }
 }
