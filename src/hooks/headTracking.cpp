0b4e463d676bca3402748fd14a275aeeed44e1dc
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-05-01 05:30:28 +0300

    a_targetPosition

--- src/hooks/headTracking.cpp
+++ src/hooks/headTracking.cpp
@@ -10,7 +10,7 @@ namespace Hooks
     // Hook Test
     static FunctionHook<void(AIProcess* a_this, Actor* a_owner, NiPoint3& a_targetPosition)> SetHeadtrackTarget(RELOCATION_ID(38850, 39887),
       [](auto* a_this, auto* a_owner, auto& a_targetPosition) {
-        log::info("Function HOOKED: {}, {}", a_owner? a_owner->GetDisplayFullName() : "", Vector2Str(taget));
+        log::info("Function HOOKED: {}, {}", a_owner? a_owner->GetDisplayFullName() : "", Vector2Str(a_targetPosition));
         SetHeadtrackTarget(a_this, a_owner, a_targetPosition);
         return;
       }
