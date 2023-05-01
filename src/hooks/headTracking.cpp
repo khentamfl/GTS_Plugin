2a31032b334656c9267a9cb1fff42b0bb9ba2358
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-05-01 20:23:29 +0700

    Some logs

--- src/hooks/headTracking.cpp
+++ src/hooks/headTracking.cpp
@@ -5,16 +5,19 @@ using namespace RE;
 using namespace SKSE;
 
 namespace {
-  void SetHeadtrackTargetImpl(Actor* actor, NiPoint3& target) {
+  NiPoint3 void SetHeadtrackTargetImpl(Actor* actor, const NiPoint3& target) {
     if (!actor) {
       return;
     }
     auto unscaledHeadPos = actor->GetLookingAtLocation();
+    log::info("unscaledHeadPos: {}", Vector2Str(unscaledHeadPos));
     auto scale = get_visual_scale(actor);
     auto location = actor->GetPosition();
     auto headPos = (unscaledHeadPos - location) * (scale) + location;
+    log::info("headPos: {}", Vector2Str(headPos));
     auto direction = target - unscaledHeadPos;
-    target = headPos + direction;
+    log::info("direction: {}", Vector2Str(direction));
+    return headPos + direction;
   }
 }
 
@@ -24,8 +27,7 @@ namespace Hooks
   void Hook_HeadTracking::Hook(Trampoline& trampoline) {
     static FunctionHook<void(AIProcess* a_this, Actor* a_owner, NiPoint3& a_targetPosition)> SetHeadtrackTarget(RELOCATION_ID(38850, 39887),
       [](auto* a_this, auto* a_owner, auto& a_targetPosition) {
-        NiPoint3 newPoint = a_targetPosition;
-        SetHeadtrackTargetImpl(a_owner, newPoint);
+        NiPoint3 newPoint = SetHeadtrackTargetImpl(a_owner, a_targetPosition);
         SetHeadtrackTarget(a_this, a_owner, newPoint);
         return;
       }
