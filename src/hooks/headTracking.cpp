fbd31d883def8bb16f63bddf993797dce3b5f03c
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-05-02 09:14:54 +0700

    Matrix mechanics

--- src/hooks/headTracking.cpp
+++ src/hooks/headTracking.cpp
@@ -5,22 +5,28 @@ using namespace RE;
 using namespace SKSE;
 
 namespace {
-  NiPoint3 SetHeadtrackTargetImpl(Actor* actor, const NiPoint3& target) {
+  void SetHeadtrackTargetImpl(Actor* actor, NiPoint3& target) {
     if (!actor) {
-      return NiPoint3();
+      return;
     }
     log::info("Actor: {}", actor->GetDisplayFullName());
-    auto unscaledHeadPos = actor->GetLookingAtLocation();
-    log::info("unscaledHeadPos: {}", Vector2Str(unscaledHeadPos));
-    auto scale = get_visual_scale(actor);
-    auto location = actor->GetPosition();
-    log::info("unscaledHeadPos - location: {}", Vector2Str(unscaledHeadPos - location));
-    auto headPos = (unscaledHeadPos - location) * (scale) + location;
+    auto headPos = actor->GetLookingAtLocation();
     log::info("headPos: {}", Vector2Str(headPos));
-    log::info("headPos - location: {}", Vector2Str(headPos - location));
+    auto model = actor->Get3D();
+    if (!model) {
+      return;
+    }
+    auto trans = model->world;
+    auto transInv = trans.Invert();
+    auto scale = get_visual_scale(actor);
+
+    log::info("headPos (local): {}", Vector2Str(transInv*headPos));
+    auto unscaledHeadPos = trans * (transInv*headPos * (1.0/scale));
+    log::info("unscaledHeadPos: {}", Vector2Str(unscaledHeadPos));
+    log::info("unscaledHeadPos (local): {}", Vector2Str(transInv*headPos));
     auto direction = target - headPos;
     log::info("direction: {}", Vector2Str(direction));
-    return unscaledHeadPos + direction;
+    target = unscaledHeadPos + direction;
   }
 }
 
@@ -30,12 +36,10 @@ namespace Hooks
   void Hook_HeadTracking::Hook(Trampoline& trampoline) {
     static FunctionHook<void(AIProcess* a_this, Actor* a_owner, NiPoint3& a_targetPosition)> SetHeadtrackTarget(RELOCATION_ID(38850, 39887),
       [](auto* a_this, auto* a_owner, auto& a_targetPosition) {
-        NiPoint3 newPoint = SetHeadtrackTargetImpl(a_owner, a_targetPosition);
-        SetHeadtrackTarget(a_this, a_owner, newPoint);
+        SetHeadtrackTargetImpl(a_owner, a_targetPosition);
+        SetHeadtrackTarget(a_this, a_owner, a_targetPosition);
         return;
       }
     );
-
-    // static REL::Relocation<NiPoint3(TESObjectREFR*)> GetLookingAtLocationPlayer = ;
   }
 }
