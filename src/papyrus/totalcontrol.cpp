a061f3f32d37b8237b70576688d4189a8274c3d5
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-05-26 14:24:20 +0700

    `;`

--- src/papyrus/totalcontrol.cpp
+++ src/papyrus/totalcontrol.cpp
@@ -58,7 +58,7 @@ namespace {
           bonus = target_scale * 0.25 + 0.75;
         }
 
-        log::info("  - Growing Teammate")
+        log::info("  - Growing Teammate");
         DamageAV(caster, ActorValue::kMagicka, 0.45 * (target_scale * 0.25 + 0.75) * magicka * bonus * timeDelta * power);
         mod_target_scale(target, 0.0030 * magicka * bonus * timeDelta * power);
         Rumble::Once("GrowOtherButton", target, 1.0, 0.05);
