ba1b194a7622cf32573278a4ddaa0e2e3b3e0533
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-09 09:00:59 +0700

    Does this even compile?

---
+++ src/managers/animation/AttachPoint.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace Gts {
+
+  void GetActor(Actor& actor) -> Actor& {
+    return actor;
+  }
+  void GetActor(Actor* actor) -> Actor& {
+    if (!actor) {
+      throw std::runtime_error("Pointer to actor is invalid");
+    }
+    return GetActor(*actor);
+  }
+  void GetActor(ActorHandle& actor) -> Actor& {
+    if (!actor) {
+      throw std::runtime_error("ActorHandle is invalid");
+    }
+    return GetActor(*actor.get().get());
+  }
+  void GetActor(FormID formId) -> Actor& {
+    Actor* actor = TESForm::LookupByID<Actor>(formId);
+    if (!actor) {
+      throw std::runtime_error("ActorHandle is invalid");
+    }
+    return GetActor(*actor.get().get());
+  }
+
+  template<T, U>
+  void AttachToObjectA(T maybeGiant, U maybeTiny) {
+      Actor& giant;
+      Actor& tiny;
+      try {
+         giant = GetActor(maybeGiant);
+         tiny = GetActor(maybeTiny);
+      } catch (const std::runtime_error& e) {
+        return;
+      }
+      // Ready
+  }
