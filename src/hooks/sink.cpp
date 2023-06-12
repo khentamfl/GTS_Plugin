0354b23dddaa0556cbd776dbac5ee337b645c4ba
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-12 09:04:18 +0700

    Which one

--- src/hooks/sink.cpp
+++ src/hooks/sink.cpp
@@ -9,7 +9,8 @@ namespace Hooks {
 	void Hook_Sinking::Hook(Trampoline& trampoline) {
 
     static FunctionHook<float(Actor* a_this, TESObjectCELL* a_cell, float z_pos)> GetSubmergeLevel(
-      RELOCATION_ID(38850, 39887), Offset(0x30),
+      RELOCATION_ID(38850, 39887),
+      Offset(0x30),
         [](auto* a_this, auto* cell, float z_pos){
           float result = GetSubmergeLevel(a_this, cell, z_pos);
           log::info("GetSubmergeLevel");
