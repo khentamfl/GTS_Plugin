8480383e5649fb18fe6099eec220fef44daad3f1
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-12 09:03:51 +0700

    More fix attempts

--- src/hooks/sink.cpp
+++ src/hooks/sink.cpp
@@ -9,9 +9,9 @@ namespace Hooks {
 	void Hook_Sinking::Hook(Trampoline& trampoline) {
 
     static FunctionHook<float(Actor* a_this, TESObjectCELL* a_cell, float z_pos)> GetSubmergeLevel(
-      REL::RELOCATION_ID(38850, 39887), REL::Offset(0x30),
+      RELOCATION_ID(38850, 39887), Offset(0x30),
         [](auto* a_this, auto* cell, float z_pos){
-          float result = return GetSubmergeLevel(a_this, cell, z_pos);
+          float result = GetSubmergeLevel(a_this, cell, z_pos);
           log::info("GetSubmergeLevel");
           if (a_this) {
             log::info("  - Actor: {}", a_this->GetDisplayFullName());
