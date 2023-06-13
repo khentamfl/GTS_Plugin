928c8a181672f6f56bebdbdc3be93d09821bd641
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-13 14:23:26 +0700

    Maybe its an int

--- src/hooks/sink.cpp
+++ src/hooks/sink.cpp
@@ -8,7 +8,7 @@ namespace Hooks {
 
 void Hook_Sinking::Hook(Trampoline& trampoline) {
 
-    static FunctionHook<float(Actor* a_this, long* unknown, TESObjectCELL* a_cell, float z_pos)> GetSubmergeLevel(
+    static FunctionHook<float(Actor* a_this, std::uintptr_t unknown, TESObjectCELL* a_cell, float z_pos)> GetSubmergeLevel(
       REL::RelocationID(36451, 37447),
       REL::Offset(0x30),
         [](auto* a_this, auto* unknown, auto* cell, float z_pos){
@@ -21,7 +21,7 @@ void Hook_Sinking::Hook(Trampoline& trampoline) {
             log::info("  - In a cell:{}", log_id);
           }
           if (unknown) {
-            log::info("Getting unknown:{}: {}", log_id, *reinterpret_cast<std::uintptr_t *>(unknown));
+            log::info("Getting unknown:{}: {}", log_id, unknown);
           }
           float result = GetSubmergeLevel(a_this, unknown, cell, z_pos);
           log::info("  - Submerged level:{}:: {}", log_id, result);
