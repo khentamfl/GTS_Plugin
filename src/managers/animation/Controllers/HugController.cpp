d2c48ef8fbe6d1315ebb072c81567894691344e8
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-06-13 18:47:42 +0300

    Hug Animations

---
+++ src/managers/animation/Controllers/HugController.cpp
@@ -0,0 +1,193 @@
+#include "managers/animation/AnimationManager.hpp"
+#include "managers/animation/ThighSandwich.hpp"
+#include "managers/Controlles/HugController.hpp"
+#include "managers/animation/Grab.hpp"
+#include "managers/GtsSizeManager.hpp"
+#include "managers/InputManager.hpp"
+#include "managers/CrushManager.hpp"
+#include "managers/explosion.hpp"
+#include "managers/footstep.hpp"
+#include "data/persistent.hpp"
+#include "managers/tremor.hpp"
+#include "managers/Rumble.hpp"
+#include "data/runtime.hpp"
+#include "scale/scale.hpp"
+#include "spring.hpp"
+#include "node.hpp"
+
+
+namespace {
+
+	const float MINIMUM_GRAB_DISTANCE = 95.0;
+	const float MINIMUM_GRAB_SCALE_RATIO = 0.7;
+	const float GRAB_ANGLE = 70;
+	const float PI = 3.14159;
+
+	[[nodiscard]] inline RE::NiPoint3 RotateAngleAxis(const RE::NiPoint3& vec, const float angle, const RE::NiPoint3& axis)
+	{
+		float S = sin(angle);
+		float C = cos(angle);
+
+		const float XX = axis.x * axis.x;
+		const float YY = axis.y * axis.y;
+		const float ZZ = axis.z * axis.z;
+
+		const float XY = axis.x * axis.y;
+		const float YZ = axis.y * axis.z;
+		const float ZX = axis.z * axis.x;
+
+		const float XS = axis.x * S;
+		const float YS = axis.y * S;
+		const float ZS = axis.z * S;
+
+		const float OMC = 1.f - C;
+
+		return RE::NiPoint3(
+			(OMC * XX + C) * vec.x + (OMC * XY - ZS) * vec.y + (OMC * ZX + YS) * vec.z,
+			(OMC * XY + ZS) * vec.x + (OMC * YY + C) * vec.y + (OMC * YZ - XS) * vec.z,
+			(OMC * ZX - YS) * vec.x + (OMC * YZ + XS) * vec.y + (OMC * ZZ + C) * vec.z
+			);
+	}
+}
+
+namespace Gts {
+	HugAnimationController& HugAnimationController::GetSingleton() noexcept {
+		static HugAnimationController instance;
+		return instance;
+	}
+
+	std::string HugAnimationController::DebugName() {
+		return "HugAnimationController";
+	}
+
+	std::vector<Actor*> HugAnimationController::GetGrabTargetsInFront(Actor* pred, std::size_t numberOfPrey) {
+		// Get vore target for actor
+		auto& sizemanager = SizeManager::GetSingleton();
+		if (IsGtsBusy(pred)) {
+			return {};
+		}
+		if (!pred) {
+			return {};
+		}
+		auto charController = pred->GetCharController();
+		if (!charController) {
+			return {};
+		}
+
+		NiPoint3 predPos = pred->GetPosition();
+
+		auto preys = find_actors();
+
+		// Sort prey by distance
+		sort(preys.begin(), preys.end(),
+		     [predPos](const Actor* preyA, const Actor* preyB) -> bool
+		{
+			float distanceToA = (preyA->GetPosition() - predPos).Length();
+			float distanceToB = (preyB->GetPosition() - predPos).Length();
+			return distanceToA < distanceToB;
+		});
+
+		// Filter out invalid targets
+		preys.erase(std::remove_if(preys.begin(), preys.end(),[pred, this](auto prey)
+		{
+			return !this->CanGrab(pred, prey);
+		}), preys.end());
+
+		// Filter out actors not in front
+		auto actorAngle = pred->data.angle.z;
+		RE::NiPoint3 forwardVector{ 0.f, 1.f, 0.f };
+		RE::NiPoint3 actorForward = RotateAngleAxis(forwardVector, -actorAngle, { 0.f, 0.f, 1.f });
+
+		NiPoint3 predDir = actorForward;
+		predDir = predDir / predDir.Length();
+		preys.erase(std::remove_if(preys.begin(), preys.end(),[predPos, predDir](auto prey)
+		{
+			NiPoint3 preyDir = prey->GetPosition() - predPos;
+			if (preyDir.Length() <= 1e-4) {
+				return false;
+			}
+			preyDir = preyDir / preyDir.Length();
+			float cosTheta = predDir.Dot(preyDir);
+			return cosTheta <= 0; // 180 degress
+		}), preys.end());
+
+		// Filter out actors not in a truncated cone
+		// \      x   /
+		//  \  x     /
+		//   \______/  <- Truncated cone
+		//   | pred |  <- Based on width of pred
+		//   |______|
+		float predWidth = 70 * get_visual_scale(pred);
+		float shiftAmount = fabs((predWidth / 2.0) / tan(GRAB_ANGLE/2.0));
+
+		NiPoint3 coneStart = predPos - predDir * shiftAmount;
+		preys.erase(std::remove_if(preys.begin(), preys.end(),[coneStart, predWidth, predDir](auto prey)
+		{
+			NiPoint3 preyDir = prey->GetPosition() - coneStart;
+			if (preyDir.Length() <= predWidth*0.4) {
+				return false;
+			}
+			preyDir = preyDir / preyDir.Length();
+			float cosTheta = predDir.Dot(preyDir);
+			return cosTheta <= cos(GRAB_ANGLE*PI/180.0);
+		}), preys.end());
+
+		// Reduce vector size
+		if (preys.size() > numberOfPrey) {
+			preys.resize(numberOfPrey);
+		}
+
+		return preys;
+	}
+
+	bool HugAnimationController::CanHug(Actor* pred, Actor* prey) {
+		if (pred == prey) {
+			return false;
+		}
+		if (prey->IsDead()) {
+			return false;
+		}
+		if (prey->formID == 0x14 && !Persistent::GetSingleton().vore_allowplayervore) {
+			return false;
+		}
+
+		float pred_scale = get_visual_scale(pred);
+		float prey_scale = get_visual_scale(prey);
+		if (IsDragon(prey)) {
+			prey_scale *= 3.0;
+		}
+
+		float sizedifference = pred_scale/prey_scale;
+
+		float MINIMUM_GRAB_SCALE = MINIMUM_GRAB_SCALE_RATIO;
+		float MINIMUM_DISTANCE = MINIMUM_GRAB_DISTANCE;
+
+		float balancemode = SizeManager::GetSingleton().BalancedMode();
+
+		float prey_distance = (pred->GetPosition() - prey->GetPosition()).Length();
+		if (pred->formID == 0x14 && prey_distance <= MINIMUM_DISTANCE * pred_scale && pred_scale/prey_scale < MINIMUM_GRAB_SCALE) {
+			Notify("{} is too big to be grabbed.", prey->GetDisplayFullName());
+			return false;
+		}
+		if (prey_distance <= (MINIMUM_DISTANCE * pred_scale) && pred_scale/prey_scale > MINIMUM_GRAB_SCALE) {
+			if ((prey->formID != 0x14 && prey->IsEssential() && Runtime::GetBool("ProtectEssentials"))) {
+				return false;
+			} else {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	void HugAnimationController::StartGrab(Actor* pred, Actor* prey) {
+		auto& hugging = HugAnimationController::GetSingleton();
+		if (!hugging.CanHug(pred, prey)) {
+			return;
+		}
+		if (HasSMT(pred)) {
+		}
+		HugShrink::GetSingleton().HugActor(pred, prey);
+		AnimationManager::StartAnim("GrabSomeone", pred);
+	}
+}
---
+++ src/managers/animation/HugShrink.cpp
@@ -0,0 +1,213 @@
+#include "managers/animation/Utils/AnimationUtils.hpp"
+#include "managers/animation/AnimationManager.hpp"
+#include "managers/animation/AttachPoint.hpp"
+#include "managers/Controlles/HugController.hpp"
+#include "managers/emotions/EmotionManager.hpp"
+#include "managers/ShrinkToNothingManager.hpp"
+#include "managers/damage/SizeHitEffects.hpp"
+#include "managers/damage/LaunchActor.hpp"
+#include "managers/animation/HugShrink.hpp"
+#include "managers/GtsSizeManager.hpp"
+#include "managers/ai/aifunctions.hpp"
+#include "managers/CrushManager.hpp"
+#include "managers/InputManager.hpp"
+#include "magic/effects/common.hpp"
+#include "utils/actorUtils.hpp"
+#include "data/persistent.hpp"
+#include "managers/tremor.hpp"
+#include "managers/Rumble.hpp"
+#include "data/transient.hpp"
+#include "managers/vore.hpp"
+#include "data/runtime.hpp"
+#include "scale/scale.hpp"
+#include "data/time.hpp"
+#include "events.hpp"
+#include "timer.hpp"
+#include "node.hpp"
+
+#include <random>
+
+using namespace RE;
+using namespace REL;
+using namespace Gts;
+using namespace std;
+
+
+/*Event used in the behaviours to transition between most behaviour states
+ */
+
+
+namespace {
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////E V E N T S
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void HugAttemptEvent(const InputEventData& data) {
+		auto player = PlayerCharacter::GetSingleton();
+		auto huggedActor = HugShrink::GetHuggiesActor(player);
+		if (!huggedActor) {
+			return;
+		}
+		if (IsGtsBusy(player)) {
+			return;
+		}
+        auto& Hugging = HugAnimationController::GetSingleton();
+		std::size_t numberOfPrey = 1;
+		if (Runtime::HasPerkTeam(player, "MassVorePerk")) {
+			numberOfPrey = 1 + (get_visual_scale(player)/3);
+		}
+		std::vector<Actor*> preys = Hugging.GetHugTargetsInFront(player, numberOfPrey);
+		for (auto prey: preys) {
+			Hugging.StartHug(player, prey);
+		}
+		AnimationManager::StartAnim("Huggies_Try", player);
+	}
+
+	void HugShrinkEvent(const InputEventData& data) {
+		auto player = PlayerCharacter::GetSingleton();
+		auto huggedActor = HugShrink::GetHuggiesActor(player);
+		if (!huggedActor) {
+			return;
+		}
+		AnimationManager::StartAnim("Huggies_Shrink", player);
+	}
+	void HugReleaseEvent(const InputEventData& data) {
+		auto player = PlayerCharacter::GetSingleton();
+		auto huggedActor = HugShrink::GetHuggiesActor(player);
+		if (!huggedActor) {
+			return;
+		}
+		AnimationManager::StartAnim("Huggies_Spare", player);
+	}
+}
+
+namespace Gts {
+	HugShrink& HugShrink::GetSingleton() noexcept {
+		static HugShrink instance;
+		return instance;
+	}
+
+	std::string HugShrink::DebugName() {
+		return "HugShrink";
+	}
+
+	void HugShrink::DetachActorTask(Actor* giant) {
+		std::string name = std::format("Huggies_{}", giant->formID);
+		TaskManager::Cancel(name);
+	}
+
+	void HugShrink::AttachActorTask(Actor* giant, Actor* tiny) {
+		if (!giant) {
+			return;
+		}
+		if (!tiny) {
+			return;
+		}
+		std::string name = std::format("Huggies_{}", giant->formID);
+		ActorHandle gianthandle = giant->CreateRefHandle();
+		ActorHandle tinyhandle = tiny->CreateRefHandle();
+		TaskManager::Run(name, [=](auto& progressData) {
+			if (!gianthandle) {
+				return false;
+			}
+			if (!tinyhandle) {
+				return false;
+			}
+			auto giantref = gianthandle.get().get();
+			auto tinyref = tinyhandle.get().get();
+
+			// Exit on death
+			float sizedifference = get_target_scale(giantref)/get_target_scale(tinyref);
+
+			if (tinyref->IsDead() || sizedifference > 6.0) {
+				HugShrink::Release(giantref);
+				PushActorAway(giantref, tinyref, 0.1);
+				DrainStamina(giant, "GrabAttack", "DestructionBasics", false, 0.30, 1.0);
+				return false;
+			}
+
+            if (!AttachToObjectA(gianthandle, tinyhandle)) {
+                // Unable to attach
+                return false;
+            }
+			
+			// All good try another frame
+			return true;
+		});
+	}
+
+
+	void HugShrink::HugActor_Actor(Actor* giant, TESObjectREFR* tiny, float strength) {
+		HugShrink::GetSingleton().data.try_emplace(giant, tiny, strength);
+	}
+	void HugShrink::HugActor(Actor* giant, TESObjectREFR* tiny) {
+		// Default strength 1.0: normal grab for actor of their size
+		//
+		HugShrink::HugActor(giant, tiny, 1.0);
+	}
+
+	void HugShrink::Reset() {
+		this->data.clear();
+	}
+
+	void HugShrink::ResetActor(Actor* actor) {
+		this->data.erase(actor);
+	}
+
+	void HugShrink::Release(Actor* giant) {
+		HugShrink::GetSingleton().data.erase(giant);
+	}
+
+	TESObjectREFR* HugShrink::GetHuggiesObj(Actor* giant) {
+		try {
+			auto& me = HugShrink::GetSingleton();
+			return me.data.at(giant).tiny;
+		} catch (std::out_of_range e) {
+			return nullptr;
+		}
+
+	}
+	Actor* HugShrink::GetHuggiesActor(Actor* giant) {
+		auto obj = HugShrink::GetHeldObj(giant);
+		Actor* actor = skyrim_cast<Actor*>(obj);
+		if (actor) {
+			return actor;
+		} else {
+			return nullptr;
+		}
+	}
+
+	void HugShrink::RegisterEvents() {
+        InputManager::RegisterInputEvent("HugAttemptEvent", HugAttemptEvent);
+		InputManager::RegisterInputEvent("HugReleaseEvent", HugReleaseEvent);
+		InputManager::RegisterInputEvent("HugShrinkEvent", HugShrinkEvent);
+
+		AnimationManager::RegisterEvent("GTSBEH_GrabExit", "Hugs", GTSBEH_GrabExit);
+		AnimationManager::RegisterEvent("GTSBEH_AbortGrab", "Hugs", GTSBEH_AbortGrab);
+	}
+
+	void HugShrink::RegisterTriggers() {
+		AnimationManager::RegisterTrigger("Huggies_Try", "Hugs", "GTSBEH_HugAbsorbStart_A");
+        AnimationManager::RegisterTrigger("Huggies_Try_Victim", "Hugs", "GTSBEH_HugAbsorbStart_V");
+        AnimationManager::RegisterTrigger("Huggies_Shrink", "Hugs", " GTSBEH_HugAbsorbAtk");
+        AnimationManager::RegisterTrigger("Huggies_Spare", "Hugs", "GTSBEH_HugAbsorbExitLoop");
+        AnimationManager::RegisterTrigger("Huggies_Cancel", "Hugs", " GTSBEH_PairedAbort";)
+	}
+
+	HugShrinkData::HugShrinkData(TESObjectREFR* tiny, float strength) : tiny(tiny), strength(strength) {
+	}
+}
+
+
+//Beh's:
+/*
+        GTSBEH_GrabStart
+        GTSBEH_GrabVore
+        GTSBEH_GrabAttack
+        GTSBEH_GrabThrow
+        GTSBEH_GrabRelease
+
+        GTSBeh_GrabExit
+        GTSBEH_AbortGrab (Similar to GTSBEH_Exit but Grab only)
+ */
