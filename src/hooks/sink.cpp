13a93dd75dbd68f7f6eaa6f41aa53894603d2e25
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-13 09:22:27 +0700

    Without sink hook too

--- src/hooks/sink.cpp
+++ src/hooks/sink.cpp
@@ -8,22 +8,22 @@ namespace Hooks {
 
 void Hook_Sinking::Hook(Trampoline& trampoline) {
 
-    static FunctionHook<float(Actor* a_this, TESObjectCELL* a_cell, float z_pos)> GetSubmergeLevel(
-      REL::RelocationID(38850, 39887),
-      REL::Offset(0x30),
-        [](auto* a_this, auto* cell, float z_pos){
-          float result = GetSubmergeLevel(a_this, cell, z_pos);
-          log::info("GetSubmergeLevel");
-          if (a_this) {
-            log::info("  - Actor: {}", a_this->GetDisplayFullName());
-          }
-          if (cell) {
-            log::info("  - In a cell");
-          }
-          log::info("  - Submerged level: {}", result);
-          return result;
-      }
-    );
+    // static FunctionHook<float(Actor* a_this, TESObjectCELL* a_cell, float z_pos)> GetSubmergeLevel(
+    //   REL::RelocationID(38850, 39887),
+    //   REL::Offset(0x30),
+    //     [](auto* a_this, auto* cell, float z_pos){
+    //       float result = GetSubmergeLevel(a_this, cell, z_pos);
+    //       log::info("GetSubmergeLevel");
+    //       if (a_this) {
+    //         log::info("  - Actor: {}", a_this->GetDisplayFullName());
+    //       }
+    //       if (cell) {
+    //         log::info("  - In a cell");
+    //       }
+    //       log::info("  - Submerged level: {}", result);
+    //       return result;
+    //   }
+    // );
 
 	}
 }
