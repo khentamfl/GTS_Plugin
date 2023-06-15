b93944b3fd64e7b08191876753b639451ee44ae8
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-06-15 19:30:01 +0300

    Debugs

--- src/managers/animation/HugShrink.cpp
+++ src/managers/animation/HugShrink.cpp
@@ -130,9 +130,11 @@ namespace Gts {
 			// Exit on death
 			float sizedifference = get_target_scale(giantref)/get_target_scale(tinyref);
 
-			tiny->data.angle.x = -giant->data.angle.x;
-			tiny->data.angle.y = -giant->data.angle.y;
-			tiny->data.angle.z = -giant->data.angle.z;
+			tiny->data.angle.x = giant->data.angle.x/2;
+			tiny->data.angle.y = giant->data.angle.y/2;
+			tiny->data.angle.z = giant->data.angle.z/2;
+            log::info("Tiny Angle: x: {} y: {} z:{}", tiny->data.angle.x, tiny->data.angle.y, tiny->data.angle.z);
+            log::info("Giant Angle: x: {} y: {} z:{}", giant->data.angle.x, giant->data.angle.y, giant->data.angle.z);
 
 			if (tinyref->IsDead() || sizedifference > 6.0 || !HugShrink::GetHuggiesActor(giantref)) {
 				HugShrink::Release(giantref);
