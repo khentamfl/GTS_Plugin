916992eb851f936f96f5340f70eafcbd1bd0fa1c
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-06-15 18:34:20 +0300

    Try this

--- src/managers/animation/Utils/AttachPoint.hpp
+++ src/managers/animation/Utils/AttachPoint.hpp
@@ -74,24 +74,32 @@ namespace Gts {
 		if (!giant) {
 			return false;
 		}
+		Actor* tiny = GetActorPtr(anyTiny);
+		if (!tiny) {
+			return false;
+		}
 
-    std::vector<std::string_view> bone_names = {
-      "L Breast02",
-      "R Breast02"
-    };
+		std::vector<std::string_view> bone_names = {
+		"L Breast02",
+		"R Breast02"
+		};
 
-    NiPoint3 clevagePos = NiPoint3();
-    std::uint32_t bone_count = bone_names.size();
-    for (auto bone_name: bone_names) {
-      auto bone = find_node(giant, bone_name);
-      if (!bone) {
-        return false;
-      }
-      DebugAPI::DrawSphere(glm::vec3(bone->world.translate.x, bone->world.translate.y, bone->world.translate.z), 2.0, 10, {1.0, 1.0, 1.0 , 1.0});
-      clevagePos += (bone->world * NiPoint3()) * (1.0/bone_count);
-    }
+		NiPoint3 clevagePos = NiPoint3();
+		std::uint32_t bone_count = bone_names.size();
+		for (auto bone_name: bone_names) {
+		auto bone = find_node(giant, bone_name);
+		if (!bone) {
+			return false;
+		}
+		DebugAPI::DrawSphere(glm::vec3(bone->world.translate.x, bone->world.translate.y, bone->world.translate.z), 2.0, 10, {1.0, 1.0, 1.0 , 1.0});
+		clevagePos += (bone->world * NiPoint3()) * (1.0/bone_count);
+		}
 
-    DebugAPI::DrawSphere(glm::vec3(clevagePos.x, clevagePos.y, clevagePos.z), 2.0, 10, {1.0, 0.0, 0.0 , 1.0});
+		tiny->data.angle.x = giant->data.angle.x;
+		tiny->data.angle.y = giant->data.angle.y;
+		tiny->data.angle.z = giant->data.angle.z;
+
+		DebugAPI::DrawSphere(glm::vec3(clevagePos.x, clevagePos.y, clevagePos.z), 2.0, 10, {1.0, 0.0, 0.0 , 1.0});
 
 		return AttachTo(anyGiant, anyTiny, clevagePos);
 	}
