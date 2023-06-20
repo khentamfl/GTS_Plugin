a751fbaeece6b5f7a47f44443b3f5e3c5ce766cb
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-20 15:42:26 +0700

    yeah return false gotcha

--- src/managers/animation/HugShrink.cpp
+++ src/managers/animation/HugShrink.cpp
@@ -73,7 +73,6 @@ namespace {
   void AbortAnimation(Actor* giant, Actor* tiny) {
     AnimationManager::StartAnim("Huggies_Spare", giant);
 		HugShrink::Release(giant);
-		HugShrink::DetachActorTask(giant);
 		if (tiny) {
       EnableCollisions(tiny);
 			SetBeingHeld(tiny, false);
@@ -115,6 +114,7 @@ namespace {
 		auto player = PlayerCharacter::GetSingleton();
 		auto huggedActor = HugShrink::GetHuggiesActor(player);
 		AbortAnimation(player, huggedActor);
+    HugShrink::DetachActorTask(giant);
 	}
 }
 
