6ba08ffb9feb4a804df9673291b77519cc701515
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-20 09:52:13 +0700

    Stage One: Give me the Errors

---
+++ src/colliders/charcontroller.hpp
@@ -0,0 +1,26 @@
+#pragma once
+// Takes a char controller and extracts its collision objects
+#include "colliders/RE.hpp"
+
+using namespace std;
+using namespace SKSE;
+using namespace RE;
+
+namespace Gts {
+
+	class CharContData {
+		public:
+			CharContData(bhkCharacterController* charCont);
+			void ReInit(bhkCharacterController* charCont);
+
+			bhkCharacterController* charCont = nullptr;
+		private:
+      void AddRB(hkpRigidBody* rb);
+			void AddPhantom(hkpPhantom* phantom);
+
+			std::unordered_map<hkpRigidBody*, hkRefPtr<hkpRigidBody>> rbs;
+			std::unordered_map<hkpPhantom*, hkRefPtr<hkpPhantom>> phantoms;
+
+			mutable std::mutex _lock;
+	};
+}
