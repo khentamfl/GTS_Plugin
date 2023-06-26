0aa283d90a9200aaedec4f582478dccf16fd736d
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-06-27 01:16:16 +0300

    Copy Sink offset from feature/sinking

--- src/hooks/sink.cpp
+++ src/hooks/sink.cpp
@@ -8,28 +8,28 @@ namespace Hooks {
 
 	void Hook_Sinking::Hook(Trampoline& trampoline) {
 
-		static FunctionHook<float(Actor* a_this, std::uintptr_t unknown, TESObjectCELL* a_cell, float z_pos)> GetSubmergeLevel(
-		REL::RelocationID(36451, 37447),
-		REL::Offset(0x30),
-		[](auto* a_this, auto unknown, auto* cell, float z_pos){
-		log::info("GetSubmergeLevel");
-		std::uintptr_t log_id =  *reinterpret_cast<std::uintptr_t *>(a_this);
-		float scale = 1.0;
-		if (a_this) {
-			log::info("  - Actor:{}: {}", log_id, a_this->GetDisplayFullName());
-			scale = get_visual_scale(a_this);
-		}
-		if (cell) {
-		 	log::info("  - In a cell:{}", log_id);
-		}
-		if (unknown) {
-			log::info("Getting unknown:{}: {}", log_id, unknown);
-		}
-		
-		float result = GetSubmergeLevel(a_this, unknown, cell, z_pos);
-		log::info("  - Submerged level:{}:: {}", log_id, result);
-		return result/scale;
-		}
-	);
+		static FunctionHook<float(TESObjectREFR* a_this, float z_pos, TESObjectCELL* a_cell)> GetSubmergeLevel(
+			RELOCATION_ID(36452, 37448),
+			[](auto* a_this, float z_pos, auto* a_cell){
+  			log::info("GetSubmergeLevel");
+  			std::uintptr_t log_id =  *reinterpret_cast<std::uintptr_t *>(a_this);
+  			if (a_this) {
+  				log::info("  - Actor:{}: {}", log_id, a_this->GetDisplayFullName());
+  			}
+  			if (a_cell) {
+  				log::info("  - In a cell:{}", log_id);
+  			}
+			float result = GetSubmergeLevel(a_this, z_pos, a_cell);
+			Actor* is_actor = skyrim_cast<Actor*>(a_this);
+			float scale = 1.0;
+			if (is_actor) {
+			 	scale = std::clamp(get_visual_scale(is_actor), 0.10f, 9999999.0f);
+				if (is_actor->formID == 0x14) {
+					log::info("  - Submerged level:{}:: {}", log_id, result);
+					result/=scale;
+				}
+			}
+  			return result;
+		});
 	}
 }
