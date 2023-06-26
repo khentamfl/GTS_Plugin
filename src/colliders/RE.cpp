b6c9267a2633cbfbbcd3fba9f25a7323df8df884
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-26 10:48:52 +0700

    The hkp one

--- src/colliders/RE.cpp
+++ src/colliders/RE.cpp
@@ -240,8 +240,8 @@ namespace RE {
 	}
 
   void SetMotionType(hkpRigidBody* a_this, hkpMotion::MotionType a_newState, hkpEntityActivation a_preferredActivationState, hkpUpdateCollisionFilterOnEntityMode a_collisionFilterUpdateMode) {
-    typedef void (*DefRealSetMotionType)(RE::bhkRigidBody* a_this, hkpMotion::MotionType a_newState, hkpEntityActivation a_preferredActivationState, hkpUpdateCollisionFilterOnEntityMode a_collisionFilterUpdateMode);
-    REL::Relocation<DefRealSetMotionType> RealSetMotionType{ RELOCATION_ID(76247, 78077) };
+    typedef void (*DefRealSetMotionType)(hkpRigidBody* a_this, hkpMotion::MotionType a_newState, hkpEntityActivation a_preferredActivationState, hkpUpdateCollisionFilterOnEntityMode a_collisionFilterUpdateMode);
+    REL::Relocation<DefRealSetMotionType> RealSetMotionType{ RELOCATION_ID(60153, 60908) };
     RealSetMotionType(a_this, a_newState, a_preferredActivationState, a_collisionFilterUpdateMode);
   }
 }
