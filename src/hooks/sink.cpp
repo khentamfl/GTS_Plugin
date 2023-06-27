d155045a0fcac3cd4c925fb9a8bb1f9d9e128753
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-27 10:19:50 +0700

    Scale bound size

--- src/hooks/sink.cpp
+++ src/hooks/sink.cpp
@@ -8,17 +8,32 @@ namespace Hooks {
 
 	void Hook_Sinking::Hook(Trampoline& trampoline) {
 
-		static FunctionHook<float(TESObjectREFR* a_this, float z_pos, TESObjectCELL* a_cell)> GetSubmergeLevel(
-			RELOCATION_ID(36452, 37448),
-			[](auto* a_this, float z_pos, auto* a_cell){
-  			float result = GetSubmergeLevel(a_this, z_pos, a_cell);
-  			Actor* actor = skyrim_cast<Actor*>(a_this);
 
-  			if (actor) {
-          float scale = get_visual_scale(actor);
-  				result/=scale;
-  			}
-  			return result;
-		});
+    static FunctionHook<float(TESObjectREFR* a_this)> GetScaledBoundSize(
+      RELOCATION_ID(36448, 37444),
+      [](auto* a_this) {
+        float result = GetScaledBoundSize(a_this);
+        if (a_this) {
+      		Actor* actor = skyrim_cast<Actor*>(a_this);
+          if (actor) {
+            float scale = get_visual_scale(a_this)
+            result *= scale;
+          }
+        }
+        return result;
+      }
+    );
+		// static FunctionHook<float(TESObjectREFR* a_this, float z_pos, TESObjectCELL* a_cell)> GetSubmergeLevel(
+		// 	RELOCATION_ID(36452, 37448),
+		// 	[](auto* a_this, float z_pos, auto* a_cell){
+  	// 		float result = GetSubmergeLevel(a_this, z_pos, a_cell);
+  	// 		Actor* actor = skyrim_cast<Actor*>(a_this);
+    //
+  	// 		if (actor) {
+    //       float scale = get_visual_scale(actor);
+  	// 			result/=scale;
+  	// 		}
+  	// 		return result;
+		// });
 	}
 }
