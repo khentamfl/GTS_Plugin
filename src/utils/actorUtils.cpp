7d38b4457fbd500031fce03b3be4128b886d8288
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-29 11:48:59 +0700

    Mutable

--- src/utils/actorUtils.cpp
+++ src/utils/actorUtils.cpp
@@ -1015,7 +1015,7 @@ namespace Gts {
     auto growData = std::make_unique<SpringGrowData>(actor, amt, halfLife);
 
     TaskManager::Run(
-      [ growData = std::move(growData) ](const auto& progressData){
+      [ growData = std::move(growData) ](const auto& progressData) mutable {
         float totalScaleToAdd = growData->amount.value;
         float prevScaleAdded = growData->addedSoFar;
         float deltaScale = totalScaleToAdd - prevScaleAdded;
