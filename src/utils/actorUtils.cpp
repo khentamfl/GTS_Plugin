bb8f6d61d2dd067a63fb7ee0a8084ede4b90e8ab
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-06-29 11:36:35 +0700

    This too

--- src/utils/actorUtils.cpp
+++ src/utils/actorUtils.cpp
@@ -1026,7 +1026,7 @@ namespace Gts {
           if (actorData) {
             actorData->target_scale += deltaScale;
             actorData->visual_scale += deltaScale;
-            slowGrowData->addedSoFar = totalScaleToAdd;
+            growData->addedSoFar = totalScaleToAdd;
         	}
 	      }
 
