19e9093496d42be0bdaa66f6de220c08f939a951
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-06-30 20:46:20 +0300

    This one too

--- src/utils/actorUtils.cpp
+++ src/utils/actorUtils.cpp
@@ -1053,7 +1053,7 @@ namespace Gts {
         	}
 	      }
 
-        return fabs(growData->amount.value - growData->amount.target) > 1e-4;
+        return growData->amount.value > 0;//fabs(growData->amount.value - growData->amount.target) > 1e-4;
       }
     );
   }
