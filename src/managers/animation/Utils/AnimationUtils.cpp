5125f98f2dd254fcefd84f035b0294c2743db7e3
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-13 00:17:43 +0300

    giant'

--- src/managers/animation/Utils/AnimationUtils.cpp
+++ src/managers/animation/Utils/AnimationUtils.cpp
@@ -47,7 +47,7 @@ namespace Gts {
 		float bonus = 1.0;
 		if (HasSMT(giant)) {
 			bonus = overr;
-		} if (find_node_any(actor, node)) {
+		} if (find_node_any(giant, node)) {
 			LaunchActor::GetSingleton().ApplyLaunch(giant, radius * bonus, damage, node);
 		}
 	}
