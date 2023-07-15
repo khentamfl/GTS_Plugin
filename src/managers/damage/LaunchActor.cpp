37a4dd8c0bd5353e3b19f89eb4c8e40c9c91ce9e
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-15 19:37:54 +0300

    Const?

--- src/managers/damage/LaunchActor.cpp
+++ src/managers/damage/LaunchActor.cpp
@@ -83,7 +83,8 @@ namespace {
 				PushActorAway(giant, tiny, 1.0);
 				ActorHandle tinyHandle = tiny->CreateRefHandle();
 				std::string name = std::format("PushOther_{}", tiny->formID);
-				TaskManager::RunFor(name, 2.0, [=](auto& progressData){
+				const float DURATION = 1.2;
+				TaskManager::RunFor(name, DURATION, [=](auto& progressData){
 					if (tinyHandle) {
 						ApplyHavokImpulse(tinyHandle.get().get(), 0, 0, 150 * sizeRatio, 150 * sizeRatio);
 					}
