8a84b6aaee588798b48407cc311a53231aed51dd
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-15 17:22:38 +0300

    Fixes

--- src/managers/damage/AccurateDamage.cpp
+++ src/managers/damage/AccurateDamage.cpp
@@ -181,7 +181,7 @@ namespace Gts {
 			return;
 		}
 
-		radius *= 1.0 + GetHighHeelsBonusDamage(actor)/2;
+		radius *= 1.0 + (GetHighHeelsBonusDamage(actor) * 2.5);
 		log::info("Radius: {}", radius);
 
 		float giantScale = get_visual_scale(actor);
@@ -293,7 +293,7 @@ namespace Gts {
 			return;
 		}
 
-		radius *= 1.0 + GetHighHeelsBonusDamage(actor)/2;
+		radius *= 1.0 + (GetHighHeelsBonusDamage(actor) * 2.5);
 		log::info("Radius: {}", radius);
 
 		float giantScale = get_visual_scale(actor);
@@ -465,7 +465,7 @@ namespace Gts {
 		float multiplier = giantsize/tinysize * highheels;
 		float additionaldamage = 1.0 + sizemanager.GetSizeVulnerability(tiny); // Get size damage debuff from enemy
 		float normaldamage = std::clamp(sizemanager.GetSizeAttribute(giant, 0) * 0.25, 0.25, 999999.0);
-		float highheelsdamage = 1.0 + GetHighHeelsBonusDamage(giant);
+		float highheelsdamage = 1.0 + (GetHighHeelsBonusDamage(giant) * 5);
 		log::info("HighheelsDamage: {}", highheelsdamage);
 		float sprintdamage = 1.0; // default Sprint damage of 1.0
 		float falldamage = 1.0; // default Fall damage of 1.0
