33403aa6a2bb5e6c4eac5307fa928937fbf8e9b2
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-18 21:54:46 +0300

    Try this...

--- src/managers/damage/bhktest.cpp
+++ src/managers/damage/bhktest.cpp
@@ -4,28 +4,28 @@ namespace Gts
 {
 	void RigidBody::SetAngularImpulse(const hkVector4& a_impulse)
 	{
-		using func_t = decltype(&bhkRigidBody::SetAngularImpulse);
+		using func_t = decltype(&RigidBody::SetAngularImpulse);
 		REL::Relocation<func_t> func{ RELOCATION_ID(76262, 78092) };
 		return func(this, a_impulse);
 	}
 
 	void RigidBody::SetAngularVelocity(const hkVector4& a_newVel)
 	{
-		using func_t = decltype(&bhkRigidBody::SetAngularVelocity);
+		using func_t = decltype(&RigidBody::SetAngularVelocity);
 		REL::Relocation<func_t> func{ RELOCATION_ID(76260, 78090) };
 		return func(this, a_newVel);
 	}
 
 	void RigidBody::SetLinearImpulse(const hkVector4& a_impulse)
 	{
-		using func_t = decltype(&bhkRigidBody::SetLinearImpulse);
+		using func_t = decltype(&RigidBody::SetLinearImpulse);
 		REL::Relocation<func_t> func{ RELOCATION_ID(76261, 78091) };
 		return func(this, a_impulse);
 	}
 
 	void RigidBody::SetLinearVelocity(const hkVector4& a_newVel)
 	{
-		using func_t = decltype(&bhkRigidBody::SetLinearVelocity);
+		using func_t = decltype(&RigidBody::SetLinearVelocity);
 		REL::Relocation<func_t> func{ RELOCATION_ID(76259, 78089) };
 		return func(this, a_newVel);
 	}
