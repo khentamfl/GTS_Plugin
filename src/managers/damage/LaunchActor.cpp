13dc4411c9f80c173b785f588c8d8e12b2ee837d
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-18 22:45:07 +0300

    *

--- src/managers/damage/LaunchActor.cpp
+++ src/managers/damage/LaunchActor.cpp
@@ -47,7 +47,7 @@ namespace {
 	const float LAUNCH_KNOCKBACK = 0.02f;
 	const float UNDERFOOT_POWER = 0.70;
 
-	void SetLinearImpulse(bhkRigidBody& body, const hkVector4& a_impulse)
+	void SetLinearImpulse(bhkRigidBody* body, const hkVector4& a_impulse)
 	{
 		using func_t = decltype(&SetLinearImpulse);
 		REL::Relocation<func_t> func{ RELOCATION_ID(76261, 78091) };
