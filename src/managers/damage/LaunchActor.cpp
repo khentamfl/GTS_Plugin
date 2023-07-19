752b8091359bce47956afeb5998cb77b634fe74f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-20 01:03:25 +0300

    get.get again

--- src/managers/damage/LaunchActor.cpp
+++ src/managers/damage/LaunchActor.cpp
@@ -199,29 +199,27 @@ namespace {
 			auto child = data.loadedData;
 
 			for (auto result: {child->unk100, child->activatingRefs}) {
-				if (result) {
-					log::info("Child 2 True");
-					auto objectref = result->As<TESObjectREFR>();
-					if (objectref) {
-						Actor* NonRef = skyrim_cast<Actor*>(objectref); 
-						log::info("ref 2 true");
-						if (!NonRef) {
-							NiPoint3 objectlocation = objectref->GetPosition();
-							for (auto point: footPoints) {
-								float distance = (point - objectlocation).Length();
-								if (distance <= maxFootDistance) {
-									float force = 1.0 - distance / maxFootDistance;
-									auto Object1 = objectref->Get3D1(false);
-									if (Object1) {
-										auto collision = Object1->GetCollisionObject();
-										if (collision) {
-											auto rigidbody = collision->GetRigidBody();
-											if (rigidbody) {
-												auto body = rigidbody->AsBhkRigidBody();
-												if (body)
-													log::info("Launching Body 2");
-													SetLinearImpulse(body, hkVector4(0, 0, 1.2 * GetLaunchPower_Object(giantScale) * force * power, 1.2 * GetLaunchPower_Object(giantScale) * force * power));
-												}
+				log::info("Child 2 True");
+				auto objectref = result.get().get();
+				if (objectref) {
+					Actor* NonRef = skyrim_cast<Actor*>(objectref); 
+					log::info("ref 2 true");
+					if (!NonRef) {
+						NiPoint3 objectlocation = objectref->GetPosition();
+						for (auto point: footPoints) {
+							float distance = (point - objectlocation).Length();
+							if (distance <= maxFootDistance) {
+								float force = 1.0 - distance / maxFootDistance;
+								auto Object1 = objectref->Get3D1(false);
+								if (Object1) {
+									auto collision = Object1->GetCollisionObject();
+									if (collision) {
+										auto rigidbody = collision->GetRigidBody();
+										if (rigidbody) {
+											auto body = rigidbody->AsBhkRigidBody();
+											if (body)
+												log::info("Launching Body 2");
+												SetLinearImpulse(body, hkVector4(0, 0, 1.2 * GetLaunchPower_Object(giantScale) * force * power, 1.2 * GetLaunchPower_Object(giantScale) * force * power));
 											}
 										}
 									}
@@ -230,6 +228,7 @@ namespace {
 						}
 					}
 				}
+				
 
 			for (auto getthem: child->unk070) {
 				auto result = getthem.first;
