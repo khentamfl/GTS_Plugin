1e2c132c318ec57970a4b78899367af3573e6709
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-20 02:13:15 +0300

    _last?

--- src/managers/damage/LaunchActor.cpp
+++ src/managers/damage/LaunchActor.cpp
@@ -198,7 +198,7 @@ namespace {
 			auto data = cell->GetRuntimeData();
 			auto WorldSpace = data.worldSpace;
 			
-			for (auto object: WorldSpace->largeRefData.cellFormIDMapFiltered.at(1)) {
+			for (auto object: WorldSpace->largeRefData.cellFormIDMapFiltered._last) {
 				auto objectref = object->As<TESObjectREFR>();  
 				log::info("Getting ObjectRef");
 				if (objectref) {
