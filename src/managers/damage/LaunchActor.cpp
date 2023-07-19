e7ec54a740f45d656eea77909116334f8012a6d8
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-19 19:53:59 +0300

    Like this then?

--- src/managers/damage/LaunchActor.cpp
+++ src/managers/damage/LaunchActor.cpp
@@ -197,8 +197,8 @@ namespace {
 		if (cell) { 
 			//auto data = cell->GetRuntimeData();
 			auto data = TESDataHandler::GetSingleton()->GetFormArray(RE::FormType::Reference);
-			for (auto object: data) {
-				auto objectref = object.get();
+			for (auto objectref: data) {
+				//auto objectref = object.get();
 				if (objectref) {
 					Actor* NonRef = skyrim_cast<Actor*>(objectref); 
 					if (!NonRef) {
