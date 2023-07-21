8962cfdf29ea71f5f13d45dbf06d24b161cc0e4f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-21 15:02:03 +0300

    No Sorting

--- src/utils/findActor.cpp
+++ src/utils/findActor.cpp
@@ -59,7 +59,7 @@ namespace Gts {
 		result.insert(result.end(), low_actors.begin(), low_actors.end());*/
 
 		result.erase( std::unique( result.begin(), result.end() ), result.end() );
-		std::sort( result.begin(), result.end() );
+		//std::sort( result.begin(), result.end() );
 		
 		return result;
 	}
