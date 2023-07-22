5ca5d4c83101812043645d956306dee5c129ef28
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-22 12:18:00 +0300

    ;

--- src/managers/animation/Utils/CrawlUtils.hpp
+++ src/managers/animation/Utils/CrawlUtils.hpp
@@ -12,5 +12,5 @@ namespace Gts {
     void DoCrawlingSounds(Actor* actor, float scale, NiAVObject* node, FootEvent foot_kind);
     void DoCrawlingFunctions(Actor* actor, float scale, float multiplier, CrawlEvent kind, std::string_view tag, float launch_dist, float damage_dist);
     void DoCrawlingDamage(Actor* giant, float radius, float damage, NiAVObject* node, float random, float bbmult);
-    void ApplyAllCrawlingDamage(Actor* giant, float damage, int random, float bonedamage)
+    void ApplyAllCrawlingDamage(Actor* giant, float damage, int random, float bonedamage);
 }
