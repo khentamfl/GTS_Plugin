e7959579ad60a980754bee5e1f73579bbe0e0d6f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-07-26 23:00:23 +0300

    Also print tiny X

--- src/managers/animation/Utils/AttachPoint.hpp
+++ src/managers/animation/Utils/AttachPoint.hpp
@@ -160,7 +160,8 @@ namespace Gts {
 		//clevagePos.z *= bonus;
 
 		tiny->data.angle.x = ((RPosX + LPosX) * 70) / 2;//giant->data.angle.x;
-		tiny->data.angle.y = giant->data.angle.y;
+		log::info("Tiny Angle X: {}", tiny->data.angle.x);
+		//tiny->data.angle.y = giant->data.angle.y;
 		tiny->data.angle.z = giant->data.angle.z;
 		if (Runtime::GetBool("EnableDebugOverlay")) {
 			DebugAPI::DrawSphere(glm::vec3(clevagePos.x, clevagePos.y, clevagePos.z), 2.0, 10, {1.0, 0.0, 0.0, 1.0});
