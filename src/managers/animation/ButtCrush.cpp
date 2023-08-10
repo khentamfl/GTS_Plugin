14ebc16d54144085f3b1b866cdc2a7665c017e23
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-08-10 21:02:04 +0300

    Rumble during Growth

--- src/managers/animation/ButtCrush.cpp
+++ src/managers/animation/ButtCrush.cpp
@@ -25,8 +25,55 @@ using namespace RE;
 using namespace Gts;
 
 namespace {
+
+    const std::vector<std::string_view> ALL_RUMBLE_NODES = { // used for body rumble
+		"NPC COM [COM ]",
+		"NPC L Foot [Lft ]",
+		"NPC R Foot [Rft ]",
+		"NPC L Toe0 [LToe]",
+		"NPC R Toe0 [RToe]",
+		"NPC L Calf [LClf]",
+		"NPC R Calf [RClf]",
+		"NPC L PreRearCalf",
+		"NPC R PreRearCalf",
+		"NPC L FrontThigh",
+		"NPC R FrontThigh",
+		"NPC R RearCalf [RrClf]",
+		"NPC L RearCalf [RrClf]",
+        "NPC L UpperarmTwist1 [LUt1]",
+		"NPC L UpperarmTwist2 [LUt2]",
+		"NPC L Forearm [LLar]",
+		"NPC L ForearmTwist2 [LLt2]",
+		"NPC L ForearmTwist1 [LLt1]",
+		"NPC L Hand [LHnd]",
+        "NPC R UpperarmTwist1 [RUt1]",
+		"NPC R UpperarmTwist2 [RUt2]",
+		"NPC R Forearm [RLar]",
+		"NPC R ForearmTwist2 [RLt2]",
+		"NPC R ForearmTwist1 [RLt1]",
+		"NPC R Hand [RHnd]",
+	};
+
     const std::string_view RNode = "NPC R Foot [Rft ]";
 	const std::string_view LNode = "NPC L Foot [Lft ]";
+
+    void StartRumble(std::string_view tag, Actor& actor, float power, float halflife, bool once) {
+		for (auto& node_name: BODY_RUMBLE_NODES) {
+			std::string rumbleName = std::format("{}{}", tag, node_name);
+			if (!once) {
+				Rumble::Start(rumbleName, &actor, power,  halflife, node_name);
+			} else {
+				Rumble::Once(rumbleName, &actor, power, halflife, node_name);
+			}
+		}
+	}
+
+    void StopRumble(std::string_view tag, Actor& actor) {
+		for (auto& node_name: BODY_RUMBLE_NODES) {
+			std::string rumbleName = std::format("{}{}", tag, node_name);
+			Rumble::Stop(rumbleName, &actor);
+		}
+	}
 
     void CameraFOVTask(Actor* actor, float reduce, float speed) {
 		auto camera = PlayerCamera::GetSingleton();
@@ -162,6 +209,7 @@ namespace {
             }
 
             float stamina = GetAV(giant, ActorValue::kStamina);
+            ForceRagdoll(tinyref, false);
             DamageAV(giant, ActorValue::kStamina, 0.18 * GetButtCrushCost(giant));
             
             if (stamina <= 2.0) {
@@ -210,6 +258,13 @@ namespace {
         //CameraFOVTask(giant, 1.0, 0.003);
         Runtime::PlaySoundAtNode("growthSound", giant, 1.0, 1.0, "NPC Pelvis [Pelv]");
 		Runtime::PlaySoundAtNode("MoanSound", giant, 1.0, 1.0, "NPC Head [Head]");
+
+        StartRumble("BCRumble", data.giant, 1.2, 0.02, false);
+    }
+
+    void GTSButtCrush_GrowthFinish(AnimationEventData& data) {
+        auto giant = &data.giant;
+        StopRumble("BCRumble", data.giant);
     }
 
     void GTSButtCrush_FootstepR(AnimationEventData& data) { 
@@ -349,6 +404,7 @@ namespace Gts
 	void AnimationButtCrush::RegisterEvents() {
         AnimationManager::RegisterEvent("GTSButtCrush_Exit", "ButtCrush", GTSButtCrush_Exit);
         AnimationManager::RegisterEvent("GTSButtCrush_GrowthStart", "ButtCrush", GTSButtCrush_GrowthStart);
+        AnimationManager::RegisterEvent("GTSButtCrush_GrowthFinish", "ButtCrush", GTSButtCrush_GrowthFinish);
         AnimationManager::RegisterEvent("GTSButtCrush_FallDownImpact", "ButtCrush", GTSButtCrush_FallDownImpact);
         AnimationManager::RegisterEvent("GTSButtCrush_HandImpactR", "ButtCrush", GTSButtCrush_HandImpactR);
         AnimationManager::RegisterEvent("GTSButtCrush_FootstepR", "ButtCrush", GTSButtCrush_FootstepR);
