aed7a4b8baf24cac061a4c31ef57ad2d50a51b00
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-08-12 15:50:46 +0300

    Disable camera later

--- src/managers/animation/Crawling.cpp
+++ src/managers/animation/Crawling.cpp
@@ -72,14 +72,23 @@ namespace {
 	void GTSCrawl_Slam_Impact_R(AnimationEventData& data) {
 		auto giant = &data.giant;
 		float scale = get_visual_scale(giant);
-		DoCrawlingFunctions(giant, scale, 1.1, 2.6, CrawlEvent::RightHand, "RightHandRumble", 21, 17);
+		DoCrawlingFunctions(giant, scale, 1.1, 1.9, CrawlEvent::RightHand, "RightHandRumble", 22, 17);
 		EnableHandTracking(&data.giant, CrawlEvent::RightHand, false);
 	}
 
 	void GTSCrawl_Slam_Impact_L(AnimationEventData& data) {
 		auto giant = &data.giant;
 		float scale = get_visual_scale(giant);
-		DoCrawlingFunctions(giant, scale, 1.1, 2.6, CrawlEvent::LeftHand, "LeftHandRumble", 21, 17);
+		DoCrawlingFunctions(giant, scale, 1.1, 1.9, CrawlEvent::LeftHand, "LeftHandRumble", 22, 17);
+		EnableHandTracking(&data.giant, CrawlEvent::LeftHand, false);
+	}
+
+	void GTSCrawl_Slam_Cam_Off_R(AnimationEventData& data) {
+		auto giant = &data.giant;
+		EnableHandTracking(&data.giant, CrawlEvent::RightHand, false);
+	}
+	void GTSCrawl_Slam_Cam_Off_L(AnimationEventData& data) {
+		auto giant = &data.giant;
 		EnableHandTracking(&data.giant, CrawlEvent::LeftHand, false);
 	}
 }
