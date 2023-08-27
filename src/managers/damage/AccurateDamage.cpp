a31136840550c27045a02c74ecdb5ca33772a03d
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-08-27 21:15:08 +0300

    Transfer Cause

--- src/managers/damage/AccurateDamage.cpp
+++ src/managers/damage/AccurateDamage.cpp
@@ -255,7 +255,7 @@ namespace Gts {
 							if (nodeCollisions > 0) {
 								float aveForce = std::clamp(force, 0.00f, 0.70f);///nodeCollisions;
 								//log::info("Actor: {}, Node collisions: {}, force: {}", actor->GetDisplayFullName(), nodeCollisions, force);
-								accuratedamage.ApplySizeEffect(actor, otherActor, aveForce * damage, random, bbmult, crushmult);
+								accuratedamage.ApplySizeEffect(actor, otherActor, aveForce * damage, random, bbmult, crushmult, Cause);
 							}
 						}
 					}
