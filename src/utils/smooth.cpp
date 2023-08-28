5becde397e062a6f429520ab80aa1a43ac1cd58f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-08-28 16:52:57 +0300

    Try like this then

--- src/utils/smooth.cpp
+++ src/utils/smooth.cpp
@@ -39,8 +39,8 @@ namespace Gts {
 	}
 
 	//https://www.desmos.com/calculator/8lqgse3jkr
-	float bezier_curve(const float x,const float x1, const float x2, const float x3, const float x4, const float i) {
-    	return pow((((1-x)*(1-x)*(1-x)*x1) + (3.0*(1-x)*(1-x)*x*x2) + (3.0*(1-x)*x*x*x3) + (x*x*x*x4)),i);
+	float bezier_curve(const float x,const float x1, const float x2, const float x3, const float x4, const float i, const float k) {
+    	return pow((((1-x)*(1-x)*(1-x)*x1) + (3.0*(1-x)*(1-x)*x*x2) + (3.0*(1-x)*x*x*x3) + (x*x*x*x4)),i)*k;
 		//i determines the steepness of the curve - how slow or fast it accelerates and decelerates. Should be between two and three (two might be the best one)
 		//x1 should always be 0, x3 and x4 should always be 1
 		//x2 determines if it accelerates fast but deceleares slow, or vice verca. Should be between 0.8 and 1.
