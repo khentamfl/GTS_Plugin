ababfa5a7a0838ce37ea4f399cd00a2680484e5a
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-08-30 05:06:30 +0300

    Remove this function and don't use it

--- src/utils/actorUtils.hpp
+++ src/utils/actorUtils.hpp
@@ -120,7 +120,6 @@ namespace Gts {
 	void SpawnParticle(Actor* actor, float lifetime, const char* modelName, const NiMatrix3& rotation, const NiPoint3& position, float scale, std::uint32_t flags, NiAVObject* target);
 	void SpawnDustParticle(Actor* giant, Actor* tiny, std::string_view node, float size);
 	void SpawnDustExplosion(Actor* giant, Actor* tiny, std::string_view node, float size);
-	void SpawnExplosion(Actor* giant, std::string_view node, float size);
 	void StaggerOr(Actor* giant, Actor* tiny, float power, float afX, float afY, float afZ, float afMagnitude);
 	void DoDamageEffect(Actor* giant, float damage, float radius, int random, float bonedamage, FootEvent kind, float crushmult, DamageSource Cause);
 
