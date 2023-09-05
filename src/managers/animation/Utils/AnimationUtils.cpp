7a380b209a2a2c5248adb91e71bb004d96e16d81
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-09-06 02:08:07 +0300

    33% less powerful kicks

--- src/managers/animation/Utils/AnimationUtils.cpp
+++ src/managers/animation/Utils/AnimationUtils.cpp
@@ -541,7 +541,7 @@ namespace Gts {
 						bool allow = sizemanager.IsHandDamaging(otherActor);
 						if (!allow) {
 							float aveForce = std::clamp(force, 0.15f, 0.70f);
-							float pushForce = std::clamp(force, 0.04f, 0.15f);
+							float pushForce = std::clamp(force, 0.04f, 0.10f);
 							AccurateDamage::GetSingleton().ApplySizeEffect(giant, otherActor, aveForce * damage, random, bbmult, crushmult, Cause);
 							if (giantScale / tinyScale > 2.5) {
 								PushTowards(giant, otherActor, node, pushForce * pushpower, true);
