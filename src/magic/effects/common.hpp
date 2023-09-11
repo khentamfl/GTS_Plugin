a9d72a260f217017c0691c2e78eb8aceeb0f1183
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-09-11 11:15:14 +0300

    get_giantess_scale

--- src/magic/effects/common.hpp
+++ src/magic/effects/common.hpp
@@ -152,13 +152,13 @@ namespace Gts {
 		float progression_multiplier = Persistent::GetSingleton().progression_multiplier;
 		// y = mx +c
 		// power = scale_factor * scale + bonus
-		return (get_visual_scale(actor) * scale_factor + bonus) * progression_multiplier * MASTER_POWER * TimeScale();
+		return (get_giantess_scale(actor) * scale_factor + bonus) * progression_multiplier * MASTER_POWER * TimeScale();
 	}
 
 	inline float CalcPower_NoMult(Actor* actor, float scale_factor, float bonus) {
 		// y = mx +c
 		// power = scale_factor * scale + bonus
-		return (get_visual_scale(actor) * scale_factor + bonus) * MASTER_POWER * TimeScale();
+		return (get_giantess_scale(actor) * scale_factor + bonus) * MASTER_POWER * TimeScale();
 	}
 
 	inline void Grow(Actor* actor, float scale_factor, float bonus) {
@@ -191,7 +191,7 @@ namespace Gts {
 			return false;
 		} else if (target_scale < natural_scale) { // NOLINT
 			mod_target_scale(actor, amount);
-		} else {
+		} else if (target_scale > natural_scale){
 			mod_target_scale(actor, -amount);
 		}
 		return true;
