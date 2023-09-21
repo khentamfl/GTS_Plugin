e7bdbfa1dd79986cfdff95ea82c00fd570656178
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-09-21 16:50:06 +0300

    Like this?

--- src/managers/InputManager.hpp
+++ src/managers/InputManager.hpp
@@ -40,32 +40,31 @@ namespace Gts
 			//   Not taking into account things like duration
 			bool OnlyKeysPressed(const std::unordered_set<std::uint32_t>& keys);
 
-      // Resets the timer and all appropiate state variables
-      void Reset();
+			// Resets the timer and all appropiate state variables
+			void Reset();
 
-      // Returns the duration required for the event to fire
-      float MinDuration();
+			// Returns the duration required for the event to fire
+			float MinDuration();
 
-      // Returns if the event is a onup event
-      bool IsOnUp();
-	  std::string GetName();
-	}
+			// Returns if the event is a onup event
+			bool IsOnUp();
+			std::string GetName();
 
-      // Check if this is an On key up event
-      bool IsOnUp() {
-			// Checks if this key is the same as another in terms
-			// of mutaally exclusive triggers
-	  bool SameGroup(const InputEventData& other);
-		private:
-			std::string name = "";
-			unordered_set<std::uint32_t> keys = {};
-			float minDuration = 0.0;
-			double startTime = 0.0;
-			// If true this event won't fire unles ONLY the keys are pressed for the entire duration
-			bool exclusive = false;
-			TriggerMode trigger = TriggerMode::Once;
-			InputEventState state = InputEventState::Idle;
-      bool primed = false; // Used for release events. Once primed, when keys are not pressed we fire
+			// Check if this is an On key up event
+			//bool IsOnUp();
+					// Checks if this key is the same as another in terms
+					// of mutaally exclusive triggers
+			bool SameGroup(const InputEventData& other);
+				private:
+					std::string name = "";
+					unordered_set<std::uint32_t> keys = {};
+					float minDuration = 0.0;
+					double startTime = 0.0;
+					// If true this event won't fire unles ONLY the keys are pressed for the entire duration
+					bool exclusive = false;
+					TriggerMode trigger = TriggerMode::Once;
+					InputEventState state = InputEventState::Idle;
+			bool primed = false; // Used for release events. Once primed, when keys are not pressed we fire
 	};
 
 	struct RegisteredInputEvent {
