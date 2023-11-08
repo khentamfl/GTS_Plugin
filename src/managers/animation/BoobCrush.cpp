183c3dacc4b6392ecc9019b298ff23d4de308ad5
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-11-08 14:15:50 +0300

    Forgot include

--- src/managers/animation/BoobCrush.cpp
+++ src/managers/animation/BoobCrush.cpp
@@ -8,6 +8,7 @@
 #include "managers/GtsSizeManager.hpp"
 #include "managers/InputManager.hpp"
 #include "managers/CrushManager.hpp"
+#include "magic/effects/common.hpp"
 #include "managers/explosion.hpp"
 #include "managers/footstep.hpp"
 #include "managers/highheel.hpp"
