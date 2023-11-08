5baf85a8fb63c03ff82a24ea0a8add7585919d97
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-11-08 16:01:00 +0300

    Smort moment

--- src/hooks/havok.cpp
+++ src/hooks/havok.cpp
@@ -88,7 +88,7 @@ namespace {
 		Actor* actor_a = skyrim_cast<Actor*>(actor);
 		Actor* actor_b = skyrim_cast<Actor*>(otherActor);
 		if (actor_a && actor_b) {
-			float sizedifference = get_visual_scale(actor)/get_visual_scale(otherActor);
+			float sizedifference = get_visual_scale(actor_a)/get_visual_scale(actor_b);
 			if (sizedifference >= 3.4) {
 				return true;
 			}
