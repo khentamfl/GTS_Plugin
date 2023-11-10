174d023eed35c0e862e0a84ed20c270a563d8b34
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-11-10 17:34:06 +0300

    string, not NiAvObject*

--- src/managers/impact.cpp
+++ src/managers/impact.cpp
@@ -108,7 +108,7 @@ namespace {
 			std::mt19937 gen(rd());
 			std::uniform_real_distribution<float> dis(-0.2, 0.2);
 
-			auto decal = Runtime::PlayImpactEffect(actor, "GtsBloodSprayImpactSetVoreSmallest", node, NiPoint3{dis(gen), 0, -1}, 512, true, true);
+			auto decal = Runtime::PlayImpactEffect(actor, "GtsBloodSprayImpactSetVoreSmallest", foot, NiPoint3{dis(gen), 0, -1}, 512, true, true);
 			if (decal) {
 				decal->dData.decalMinWidth *= scale;
 				decal->dData.decalMaxWidth *= scale;
