ae0c85577885d09a6ee6eaef1f4f9a6f58b0e24b
Sermitse <108320865+Sermitse@users.noreply.github.com>
2023-11-15 18:36:41 +0300

    GetInventoryChanges

--- src/utils/actorUtils.cpp
+++ src/utils/actorUtils.cpp
@@ -1835,22 +1835,17 @@ namespace Gts {
 					}
 				});
 			}	
-			TESObjectREFR* ref = skyrim_cast<TESObjectREFR*>(actor);
-			if (ref) {
-				for (auto&[a_object, invData]: ref->GetDroppedInventory()) { // transfer drops
-					log::info("Transfering DropMap {} from {}, formID {} to dropbox", a_object->GetName(), actor->GetDisplayFullName(), a_object->formID);
-					actor->RemoveItem(a_object, 1, ITEM_REMOVE_REASON::kRemove, nullptr, dropbox, nullptr, nullptr);
-				}
-			}
 		for (auto &[a_object, invData]: actor->GetInventory()) { // transfer loot
 
 			log::info("Transfering item {} from {}, formID {} to dropbox", a_object->GetName(), actor->GetDisplayFullName(), a_object->formID);
 			
-			if (a_object->GetPlayable() && a_object->GetFormType() != FormType::LeveledItem) { // We don't want to move Leveled Items
+			if (a_object->GetPlayable()) {// && a_object->GetFormType() != FormType::LeveledItem) { // We don't want to move Leveled Items
 				if ((!invData.second->IsQuestObject() || removeQuestItems)) {
 					actor->RemoveItem(a_object, 1, ITEM_REMOVE_REASON::kRemove, nullptr, dropbox, nullptr, nullptr);
 				}
 			}
+			
+			dropbox->GetInventoryChanges(false)->InitLeveledItems();
 		}
 	}
 
