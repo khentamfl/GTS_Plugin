21bf052a72b6c9e6971af1907d80e5794937bd99
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-11-21 08:48:17 +0700

    Header only

--- src/utils/fmt.hpp
+++ src/utils/fmt.hpp
@@ -1,13 +1,28 @@
+#pragma once
 #include <fmt/core.h>
 #include <spdlog/fmt/ostr.h>
 
 using namespace RE;
 
 template <> struct fmt::formatter<BSFixedString> : formatter<string_view> {
-  std::string format(BSFixedString v, format_context& ctx) const;
+  auto format(BSFixedString v, format_context& ctx) const {
+    string_view name = "<empty>";
+    if v.c_str() {
+      name = v.c_str();
+    }
+    return formatter<string_view>::format(name, ctx);
+  }
 };
 
 
 template <> struct fmt::formatter<ACTOR_VALUE_MODIFIER>: formatter<string_view> {
-  std::string format(ACTOR_VALUE_MODIFIER v, format_context& ctx) const;
+  auto format(ACTOR_VALUE_MODIFIER v, format_context& ctx) const {
+    string_view name = "unknown";
+    switch (v) {
+      case ACTOR_VALUE_MODIFIER::kPermanent: name = "kPermanent"; break;
+      case ACTOR_VALUE_MODIFIER::kTemporary: name = "kTemporary"; break;
+      case ACTOR_VALUE_MODIFIER::kDamage: name = "kDamage"; break;
+    }
+    return formatter<string_view>::format(name, ctx);
+  }
 };
