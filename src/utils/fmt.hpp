10d653768a359caa669a888d4580f136653d29e7
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-11-21 08:16:21 +0700

    Remove already formatted one

--- src/utils/fmt.hpp
+++ src/utils/fmt.hpp
@@ -6,10 +6,6 @@ using namespace RE;
 template <> struct fmt::formatter<BSFixedString> : ostream_formatter{};
 
 
-template <> struct fmt::formatter<ActorValue>: formatter<string_view> {
-  auto format(ActorValue c, format_context& ctx) const;
-};
-
 template <> struct fmt::formatter<ACTOR_VALUE_MODIFIER>: formatter<string_view> {
   auto format(ACTOR_VALUE_MODIFIER c, format_context& ctx) const;
 };
