b6b684b7113c00526fc36a2f47f0c3104337cbfc
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-11-21 08:07:40 +0700

    Adding other fmt

--- src/utils/fmt.hpp
+++ src/utils/fmt.hpp
@@ -1,7 +1,15 @@
+#include <fmt/core.h>
 #include <spdlog/fmt/ostr.h>
 
-using RE;
+using namespace RE;
 
-#if FMT_VERSION >= 90000
-    template <> struct fmt::formatter<BSFixedString> : ostream_formatter{};
-#endif
+template <> struct fmt::formatter<BSFixedString> : ostream_formatter{};
+
+
+template <> struct fmt::formatter<ActorValue>: formatter<string_view> {
+  auto format(ActorValue c, format_context& ctx) const;
+};
+
+template <> struct fmt::formatter<ACTOR_VALUE_MODIFIER>: formatter<string_view> {
+  auto format(ACTOR_VALUE_MODIFIER c, format_context& ctx) const;
+};
