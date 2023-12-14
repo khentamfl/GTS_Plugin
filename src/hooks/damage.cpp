59fad2e0998d5990229cbf53661179ef9f75c8b4
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-12-14 14:07:06 +0700

    Reduce damage with the hook

--- src/hooks/damage.cpp
+++ src/hooks/damage.cpp
@@ -14,6 +14,9 @@ namespace Hooks
 		static FunctionHook<void(Actor* a_this, float dmg, uintptr_t maybe_hit_data, Actor* aggressor,TESObjectREFR* damageSrc)> SkyrimTakeDamage(
       RELOCATION_ID(36345, 37335),
       [](auto* a_this, auto dmg, auto maybe_hit_data,auto* aggressor,auto* damageSrc) {
+        log::info("{}: Taking {} damge", a_this->GetDisplayFullName(), dmg);
+        log::info("    - Reducing damage to 1.0");
+        dmg = 1.0;
         SkyrimTakeDamage(a_this, dmg, maybe_hit_data, aggressor, damageSrc);
         return;
 			}
