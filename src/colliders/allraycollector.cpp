f6c935f8889763fbe1e3d28417e50312aa0947ee
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-12-20 12:49:13 +0700

    Dragons vs Dragons

---
+++ src/colliders/allraycollector.cpp
@@ -0,0 +1,22 @@
+// Holds RE and dummy classes
+#include "colliders/allraycollector.hpp"
+
+namespace RE {
+  void hkpAllRayHitCollector::AddRayHit(const hkpCdBody& a_body, const hkpShapeRayCastCollectorOutput& a_hitInfo) {
+    REL::Relocation<std::uintptr_t> vtable{VTABLE_hkpAllRayHitCollector[0]};
+		const auto a_idx = 0x01;
+		const auto addr = vtable.address() + (sizeof(void *) * a_idx);
+		const auto result = *reinterpret_cast<std::uintptr_t *>(addr);
+		REL::Relocation<void(hkpCapsuleShape*)> func(result);
+		func(this);
+  }
+
+  hkpAllRayHitCollector::~hkpAllRayHitCollector() {
+    REL::Relocation<std::uintptr_t> vtable{VTABLE_hkpAllRayHitCollector[0]};
+		const auto a_idx = 0x00;
+		const auto addr = vtable.address() + (sizeof(void *) * a_idx);
+		const auto result = *reinterpret_cast<std::uintptr_t *>(addr);
+		REL::Relocation<void(hkpCapsuleShape*)> func(result);
+		func(this);
+  }
+}
