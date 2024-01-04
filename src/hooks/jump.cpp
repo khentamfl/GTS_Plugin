6ac9bb7126b3a265d2cb8140c9d3a04657769fb1
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-04 20:40:39 +0300

    .

--- src/hooks/jump.cpp
+++ src/hooks/jump.cpp
@@ -54,7 +54,7 @@ namespace Hooks {
 					log::info("IdleForm: found");
 				} if (object) {
 					log::info("Default Object found");
-					log::info("Object Name: {}", object->name);
+					log::info("Object Name: {}", object.name);
 				}
 				SkyrimPlayAnimation(proc, attacker, object, idle, a5, a6, target);
 				return;
