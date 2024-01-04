4b4718835325825dcb8588742cc247ed452fff77
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-04 18:25:00 +0300

    Try again

--- src/hooks/damage.cpp
+++ src/hooks/damage.cpp
@@ -203,8 +203,8 @@ namespace Hooks
 		);
 
     // SE(626400)
-		static FunctionHook<void(RE::AIProcess* proc, RE::Actor* attacker, RE::DEFAULT_OBJECT object, RE::TESIdleForm* idle, bool a5, bool a6, RE::TESObjectREFR* target)> SkyrimAnimEvent( 
-			RELOCATION_ID(38290, 38290),
+		static FunctionHook<void(AIProcess* proc, Actor* attacker, DEFAULT_OBJECT object, TESIdleForm* idle, bool a5, bool a6, TESObjectREFR* target)> SkyrimAnimEvent( 
+			RELOCATION_ID(38290, 38294),
 			[](auto* proc, auto* attacker, auto* object, auto* idle, auto a5, auto a6, auto* target) {
 				if (target) {
 					log::info("Target: {}", target->GetDisplayFullName());
@@ -218,6 +218,7 @@ namespace Hooks
 					log::info("Default Object: {}", object);
 				}
 				SkyrimAnimEvent(proc, attacker, object, idle, a5, a6, target);
+				return;
 			}
 		);
 	}
