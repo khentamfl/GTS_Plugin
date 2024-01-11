08857818fc078547b3a1b853587757678aeecf75
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-11 21:07:34 +0300

    Without -

--- src/hooks/jump.cpp
+++ src/hooks/jump.cpp
@@ -89,8 +89,8 @@ namespace Hooks {
 				//log::info("Found Graph Variable Float: {} - {}", a_variableName.c_str(), a_in);
 				if (a_variableName.c_str() == "VelocityZ") {
 					log::info("Found Velocity");
-					//log::info(" - Original Value {}", a_in);
-					log::info(" - Setting to zero");
+					log::info("Original Value {}", a_in);
+					log::info("Setting to zero");
 					//float Velocity = a_in;
 					//Velocity = 0.0;
 					//return SkyrimSetGraphVarFloat(a_variableName, Velocity);
