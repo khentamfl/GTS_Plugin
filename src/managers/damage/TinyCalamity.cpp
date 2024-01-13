dc64dd4b352b23cbfbbec0373576446f1634060f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-13 10:15:54 +0300

    More fixes

--- src/managers/damage/TinyCalamity.cpp
+++ src/managers/damage/TinyCalamity.cpp
@@ -1,5 +1,6 @@
-#include "managers/damage/TinyCalamity.hpp"
+#include "managers/damage/tinyCalamity.hpp"
 #include "managers/CrushManager.hpp"
+#include "magic/effects/common.hpp"
 #include "managers/Attributes.hpp"
 #include "utils/actorUtils.hpp"
 #include "data/persistent.hpp"
@@ -25,14 +26,14 @@ namespace {
         }
     }
 
-    bool Collision_AllowTinyCalamityCrush(Actor* giant, Actor* tiny) {
-        float GiantHp = GetAV(giant, ActorValue::kHealth);
-		float TinyHp = GetAV(tiny, ActorValue::kHealth);
+    bool Collision_AllowtinyCalamityCrush(Actor* giant, Actor* tiny) {
+        float giantHp = GetAV(giant, ActorValue::kHealth);
+		float tinyHp = GetAV(tiny, ActorValue::kHealth);
 
-        float Multiplier = GetSizeDifference(giant, Tiny);
+        float Multiplier = GetSizeDifference(giant, tiny);
         float reduction = AttributeManager::GetSingleton().GetAttributeBonus(giant, ActorValue::kHealth);
 
-        if (GiantHp >= ((TinyHp / Multiplier) * reduction)) {
+        if (giantHp >= ((tinyHp / Multiplier) * reduction)) {
             return true;
         } else {
             return false;
@@ -41,7 +42,7 @@ namespace {
 }
 
 namespace Gts {
-    void TinyCalamity_SeekActors(Actor* giant) {
+    void tinyCalamity_SeekActors(Actor* giant) {
         if (giant->formID == 0x14 && giant->AsActorState()->IsSprinting() && HasSMT(giant)) {
             auto node = find_node(giant, "NPC Pelvis [Pelv]");
             if (!node) {
@@ -80,7 +81,7 @@ namespace Gts {
                             });
                         }
                         if (nodeCollisions > 0) {
-                            TinyCalamity_CrushCheck(giant, otherActor);
+                            tinyCalamity_CrushCheck(giant, otherActor);
                         }
                     }
                 }
@@ -88,46 +89,46 @@ namespace Gts {
         }
     }
 
-    void TinyCalamity_CrushCheck(Actor* Giant, Actor* Tiny) {
-		auto profiler = Profilers::Profile("TinyCalamity: CrushCheck");
-		if (Giant == Tiny) {
+    void tinyCalamity_CrushCheck(Actor* giant, Actor* tiny) {
+		auto profiler = Profilers::Profile("tinyCalamity: CrushCheck");
+		if (giant == tiny) {
 			return;
 		}
 		auto& persistent = Persistent::GetSingleton();
-		if (persistent.GetData(Giant)) {
-			if (persistent.GetData(Giant)->smt_run_speed >= 1.0) {
-                float GiantHp = GetAV(Giant, ActorValue::kHealth);
+		if (persistent.GetData(giant)) {
+			if (persistent.GetData(giant)->smt_run_speed >= 1.0) {
+                float giantHp = GetAV(giant, ActorValue::kHealth);
 
-                if (CrushManager::AlreadyCrushed(Tiny)) {
+                if (CrushManager::AlreadyCrushed(tiny)) {
                     return;
                 }
 
-				if (GiantHp <= 0) {
+				if (giantHp <= 0) {
 					return; // just in case, to avoid CTD
 				}
 
-				if (Collision_AllowTinyCalamityCrush(Giant, Tiny)) {
-					CrushManager::Crush(Giant, Tiny);
-					CrushBonuses(Giant, Tiny); // common.hpp
+				if (Collision_AllowtinyCalamityCrush(giant, tiny)) {
+					CrushManager::Crush(giant, tiny);
+					CrushBonuses(giant, tiny); // common.hpp
 
-					Runtime::PlaySound("GtsCrushSound", Giant, 1.0, 1.0);
-                    Runtime::PlaySound("GtsCrushSound", Giant, 1.0, 0.0);
+					Runtime::PlaySound("GtsCrushSound", giant, 1.0, 1.0);
+                    Runtime::PlaySound("GtsCrushSound", giant, 1.0, 0.0);
 
-					Cprint("{} was instantly turned into mush by the body of {}", Tiny->GetDisplayFullName(), Giant->GetDisplayFullName());
-                    shake_camera(Giant, 1.75, 0.45);
+					Cprint("{} was instantly turned into mush by the body of {}", tiny->GetDisplayFullName(), giant->GetDisplayFullName());
+                    shake_camera(giant, 1.75, 0.45);
                     RefreshDuration(giant);
 				} else {
-					PushForward(Giant, Tiny, 1000);
-					AddSMTDuration(Giant, 2.5);
-					StaggerActor(Giant);
+					PushForward(giant, tiny, 1000);
+					AddSMTDuration(giant, 2.5);
+					StaggerActor(giant);
 
-					DamageAV(tiny, ActorValue::kHealth, GiantHp * 0.75);
-					DamageAV(tiny, ActorValue::kHealth, GiantHp * 0.25);
+					DamageAV(tiny, ActorValue::kHealth, giantHp * 0.75);
+					DamageAV(tiny, ActorValue::kHealth, giantHp * 0.25);
 
-					shake_camera(Giant, 4.35, 0.5);
-					Runtime::PlaySound("lJumpLand", Giant, 1.0, 1.0);
+					shake_camera(giant, 4.35, 0.5);
+					Runtime::PlaySound("lJumpLand", giant, 1.0, 1.0);
 
-					Notify("{} is too tough to be crushed", Tiny->GetDisplayFullName());
+					Notify("{} is too tough to be crushed", tiny->GetDisplayFullName());
 
                     RefreshDuration(giant);
 				}
