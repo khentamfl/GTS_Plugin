cc326c7ab45905f25ae3b68910571605833e976b
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-20 21:32:23 +0300

    Hugging

--- src/managers/impact.cpp
+++ src/managers/impact.cpp
@@ -31,7 +31,7 @@ namespace {
 
 		bool allow = ((!is_jumping && !in_air) || hugging);
 		if (actor && actor->formID == 0x14 || IsTeammate(actor)) {
-			log::info("{} In Air: {}, Jumping: {}, hugged: {}", actor->GetDisplayFullName(), in_air, is_jumping, huging);
+			log::info("{} In Air: {}, Jumping: {}, hugged: {}", actor->GetDisplayFullName(), in_air, is_jumping, hugging);
 		}
 
 		if (matches(tag, ".*Foot.*Left.*") && allow) {
