72cdc4b3a5df04711a74e98545b3efce1f87242a
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-21 13:09:59 +0300

    Remove it

--- src/utils/actorUtils.cpp
+++ src/utils/actorUtils.cpp
@@ -825,7 +825,6 @@ namespace Gts {
 			auto nif_dim = nif_bb->extents;
 			auto nif_volume = nif_dim.x * nif_dim.y * nif_dim.z;
 			log::info("{} Nif BB: {}, Volume: {}", tiny->GetDisplayFullName(), Vector2Str(nif_dim), nif_volume);
-			log::info("{} Pow BB: {}", );
 		}
 		
 		return sc;
