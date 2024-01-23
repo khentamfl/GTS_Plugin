b995fdc1f3f5eed9b2c69f6e3a3cb05d393f75df
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-23 20:32:54 +0300

    Fine

--- src/utils/actorUtils.cpp
+++ src/utils/actorUtils.cpp
@@ -2624,16 +2624,15 @@ namespace Gts {
 	}
 
 	ThresholdData& Action_GetSizeThreshold() {
-		/*ThresholdData Info = ThresholdData { // create Info
+		ThresholdData Info = ThresholdData { // create Info
 			.Action_Sandwich = 6.0, // used for sandwich only
 			.Action_Crush = 10.0,
 			.Action_Booty = 2.0, // for butt and cleavage crush
 			.Action_Vore = 8.0,
 			.Action_Grab = 8.0,
 			.Action_Hug = 0.92,
-		};*/
-		ThresholdData& Data = new ThresholdData&;
-		return Data;
+		};
+		return Info;
 	}
 
 	// RE Fun
