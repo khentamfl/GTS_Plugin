08c68962670b69d3d27fdff23118d46fa7bde22f
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-28 20:02:57 +0300

    Fine

--- src/hooks/scale.cpp
+++ src/hooks/scale.cpp
@@ -115,10 +115,12 @@ namespace Hooks {
             REL::RelocationID(69271, 70641),
             [](auto* camera) {
 				//log::info("Camera hook is running");
-				NiPoint3* result = Skyrim_Camera(camera);
+				NiPoint3 result = Skyrim_Camera(camera);
 				log::info("Hook Result: {}", Vector2Str(result));
-				result += camera_getplayeroffset();
+				//result += camera_getplayeroffset();
+				NiPoint3 offset = camera_getplayeroffset();
 				log::info("Hook Result After: {}", Vector2Str(result));
+				log::info("Offset: {}", Vector2Str(offset));
                 return result;
             }
         );
