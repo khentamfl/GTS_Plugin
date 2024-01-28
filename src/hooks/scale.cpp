4b42c33dddb93cc7992cc97c11d6f2cd85bc6420
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-28 20:47:06 +0300

    3, not 33.

--- src/hooks/scale.cpp
+++ src/hooks/scale.cpp
@@ -110,7 +110,7 @@ namespace Hooks {
             REL::RelocationID(69271, 70641),
             [](auto* camera) {
 				//log::info("Camera hook is running");
-				NiPoint33 result = Skyrim_Camera(camera);
+				NiPoint3 result = Skyrim_Camera(camera);
 				log::info("Hook Result: {}", Vector2Str(result));
 				result[0] *= camera_getplayersize();
 				log::info("Hook Result After: {}", Vector2Str(result));
