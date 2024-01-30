36ed6010a0ca71043587756ff31813e5109fd52d
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-01-30 20:46:12 +0300

    Hook_Movement::Hook

--- src/hooks/Movement.cpp
+++ src/hooks/Movement.cpp
@@ -21,65 +21,68 @@ namespace {
 
 namespace Hooks
 {
-	static CallHook<float(TESObjectREFR* param_1)>Scale_AlterMovementSpeed(  // Movement speed alteration
-        REL::RelocationID(37013, 37013), REL::Relocate(0x1A, 0x1A),          
-        [](auto* param_1) {
-            // sub_14060EEF0 : 37013
-            // 0x14060ef0a - 0x14060EEF0 = 0x1A
-            float result = 1.0; // force it to 1.0. We DON'T want the SetScale() to affect it.
-            return result;
-        }
-    );
+    void Hook_Movement::Hook(Trampoline& trampoline) {
 
-    static CallHook<float(TESObjectREFR* param_1)>sub_140623F10( // Seems to be called on attacks. 
-        REL::RelocationID(37588, 37588), REL::Relocate(0x6B, 0x6B), // Supposedly moves invisible "Hitbox" zone for weapons more forward or something
-        [](auto* param_1) {                                         // Not sure.
-            // 37588
-            // 0x140623f7b - 0x140623F10 = 0x6B
-            float result = sub_140623F10(param_1) * 10.0;
-            log::info("(18) sub_140623F10 Hooked");
-            return result;
-        }
-    );
-    //^ Hook 18
+        static CallHook<float(TESObjectREFR* param_1)>Scale_AlterMovementSpeed(  // Movement speed alteration
+            REL::RelocationID(37013, 37013), REL::Relocate(0x1A, 0x1A),          
+            [](auto* param_1) {
+                // sub_14060EEF0 : 37013
+                // 0x14060ef0a - 0x14060EEF0 = 0x1A
+                float result = 1.0; // force it to 1.0. We DON'T want the SetScale() to affect it.
+                return result;
+            }
+        );
 
-    static CallHook<float(TESObjectREFR* param_1)>sub_1404E6B30_1(
-        REL::RelocationID(31951, 31951), REL::Relocate(0x1F9, 0x1F9),
-        [](auto* param_1) {
-            // 31951
-            // 0x1404e6d29 - 0x1404E6B30 = 0x1F9
-            float result = sub_1404E6B30_1(param_1);
-            float Alter = affect_by_scale(param_1, result);
-            log::info("(42 - 1) sub_1404E6B30 Hooked");
-            return Alter;
-        }
-    );
-    //^ Hook 42
+        static CallHook<float(TESObjectREFR* param_1)>sub_140623F10( // Seems to be called on attacks. 
+            REL::RelocationID(37588, 37588), REL::Relocate(0x6B, 0x6B), // Supposedly moves invisible "Hitbox" zone for weapons more forward or something
+            [](auto* param_1) {                                         // Not sure.
+                // 37588
+                // 0x140623f7b - 0x140623F10 = 0x6B
+                float result = sub_140623F10(param_1) * 10.0;
+                log::info("(18) sub_140623F10 Hooked");
+                return result;
+            }
+        );
+        //^ Hook 18
 
-    static CallHook<float(TESObjectREFR* param_1)>sub_1404E6B30_2(
-        REL::RelocationID(31951, 31951), REL::Relocate(0xDD, 0xDD),
-        [](auto* param_1) {
-            // 31951
-            // 0x1404e6c0d - 0x1404E6B30 = 0xDD
-            float result = sub_1404E6B30_2(param_1);
-            float Alter = affect_by_scale(param_1, result);
-            log::info("(42 - 2) sub_1404E6B30 Hooked");
-            return Alter;
-        }
-    );
-    //^ Hook 43
+        static CallHook<float(TESObjectREFR* param_1)>sub_1404E6B30_1(
+            REL::RelocationID(31951, 31951), REL::Relocate(0x1F9, 0x1F9),
+            [](auto* param_1) {
+                // 31951
+                // 0x1404e6d29 - 0x1404E6B30 = 0x1F9
+                float result = sub_1404E6B30_1(param_1);
+                float Alter = affect_by_scale(param_1, result);
+                log::info("(42 - 1) sub_1404E6B30 Hooked");
+                return Alter;
+            }
+        );
+        //^ Hook 42
+
+        static CallHook<float(TESObjectREFR* param_1)>sub_1404E6B30_2(
+            REL::RelocationID(31951, 31951), REL::Relocate(0xDD, 0xDD),
+            [](auto* param_1) {
+                // 31951
+                // 0x1404e6c0d - 0x1404E6B30 = 0xDD
+                float result = sub_1404E6B30_2(param_1);
+                float Alter = affect_by_scale(param_1, result);
+                log::info("(42 - 2) sub_1404E6B30 Hooked");
+                return Alter;
+            }
+        );
+        //^ Hook 43
 
 
-    static CallHook<float(TESObjectREFR* param_1)>sub_1404E6B30_3(
-        REL::RelocationID(31951, 31951), REL::Relocate(0x5B, 0x5B),
-        [](auto* param_1) {
-            // 31951
-            // 0x1404e6b8b - 0x1404E6B30 = 0x5B
-            float result = sub_1404E6B30_3(param_1);
-            float Alter = affect_by_scale(param_1, result);
-            log::info("(42 - 3) sub_1404E6B30 Hooked");
-            return Alter;
-        }
-    );
-    //^ Hook 44
+        static CallHook<float(TESObjectREFR* param_1)>sub_1404E6B30_3(
+            REL::RelocationID(31951, 31951), REL::Relocate(0x5B, 0x5B),
+            [](auto* param_1) {
+                // 31951
+                // 0x1404e6b8b - 0x1404E6B30 = 0x5B
+                float result = sub_1404E6B30_3(param_1);
+                float Alter = affect_by_scale(param_1, result);
+                log::info("(42 - 3) sub_1404E6B30 Hooked");
+                return Alter;
+            }
+        );
+        //^ Hook 44
+    }
 }
