6b0447dfb4653affef83b76cc24f53aa4c8ddddb
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-02-02 01:55:23 +0300

    void StopStaminaDrain(Actor* giant)

--- src/managers/animation/Sneak_Slam_FingerGrind.hpp
+++ src/managers/animation/Sneak_Slam_FingerGrind.hpp
@@ -15,4 +15,5 @@ namespace Gts
 
     void Finger_DamageAndShrink(Actor* giant, float radius, float damage, NiAVObject* node, float random, float bbmult, float crushmult, float Shrink, DamageSource Cause);
     void TrackMatchingHand(Actor* giant, CrawlEvent kind, bool enable);
+    void StopStaminaDrain(Actor* giant);
 }
