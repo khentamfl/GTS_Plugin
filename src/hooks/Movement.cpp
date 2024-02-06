c96bf5a19fb977e69bf1e020fea3216d5e4499bf
QuantumEntangledAndy <sheepchaan@gmail.com>
2024-02-06 09:50:01 +0700

    Force get scale speed adjustmnet to 1.0 and use our hook only

--- src/hooks/Movement.cpp
+++ src/hooks/Movement.cpp
@@ -32,7 +32,7 @@ namespace Hooks
                 // ---------------AE:
                 // FUN_140630510 :  37943
                 // 0x140630561 - 0x140630510 = 0x51
-                float result = 0.15; // force it to 1.0. We DON'T want the SetScale() to affect it.
+                float result = 1.0; // force it to 1.0. We DON'T want the SetScale() to affect it.
                 //log::info("(21) - Hooked Alter Movement Speed, value * 0.15");
                 return result;
             }
