d00156c4eb8a96c84edd374f6bac10cd22036b52
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-02-11 19:18:37 +0300

    Try this

--- src/hooks/havok.cpp
+++ src/hooks/havok.cpp
@@ -88,16 +88,28 @@ namespace {
 
 		Actor* actor_a = skyrim_cast<Actor*>(actor);
 		Actor* actor_b = skyrim_cast<Actor*>(otherActor);
+
 		if (actor_a && actor_b) {
+
+			float gts_scale = get_visual_scale(actor_a);
+			float tiny_scale = get_visual_scale(actor_b);
+
 			float Scale_A = get_visual_scale(actor_a) * GetSizeFromBoundingBox(actor_a); // A is usually GTS, but can be TIny as well
 			float Scale_B = get_visual_scale(actor_b) * GetSizeFromBoundingBox(actor_b); // B is usually a tiny but can be GTS as well
-			// Because actors vary, it is a good idea to pass size difference from both POV's
+
 			float sizedifference_gts = Scale_A/Scale_B;
-			float sizedifference_tiny = Scale_B/Scale_A;
+
+			if (gts_scale/tiny_scale < 1.0) { // Switch actor roles
+				log::info("Roles switched");
+				sizedifference_gts = Scale_B/Scale_A; // Rough fix for Tiny being the Gts sometimes
+			}
+
+			float limit = 3.0;
+
+			bool ignore = (sizedifference_gts >= limit);
 			log::info("Both are actors");
 			log::info("GTS size difference: {}, BB: {}", sizedifference_gts, GetSizeFromBoundingBox(actor_a));
-			log::info("TINY size difference: {}, BB: {}", sizedifference_tiny, GetSizeFromBoundingBox(actor_b));
-			if (sizedifference_gts >= 3.0 || sizedifference_tiny <= 0.33) {
+			if (ignore) {
 				return true;
 			}
 		}
