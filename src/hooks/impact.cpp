67fe8ec06ed6afc200084199d085e019c74110f6
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-02-14 02:52:14 +0300

    Sigh

--- src/hooks/impact.cpp
+++ src/hooks/impact.cpp
@@ -18,9 +18,9 @@ namespace Hooks
 		return result;
 	}
 
-	BSEventNotifyControl Hook_BGSImpactManager::ProcessCollisionEvent(BGSImpactManager* a_this, const BGSCollisionSoundEvent* a_event, BSTEventSource<BGSCollisionSoundEvent>* a_eventSource) {
+	BSEventNotifyControl Hook_BGSImpactManager::ProcessEvent(BGSImpactManager* a_this, const BGSCollisionSoundEvent* a_event, BSTEventSource<BGSCollisionSoundEvent>* a_eventSource) {
 		Gts::ImpactManager::GetSingleton().HookCollisionEvent(a_this, a_event, a_eventSource);
-		auto result = _ProcessCollisionEvent(a_this, a_event, a_eventSource);
+		auto result = _ProcessEvent(a_this, a_event, a_eventSource);
 		return result;
 	}
 }
