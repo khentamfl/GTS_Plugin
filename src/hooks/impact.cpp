055363cf6e10667c8ddfe0828b306e4cb528cff8
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-02-14 02:55:10 +0300

    More sigh

--- src/hooks/impact.cpp
+++ src/hooks/impact.cpp
@@ -10,6 +10,9 @@ namespace Hooks
 		log::info("Hooking BGSImpactManager");
 		REL::Relocation<std::uintptr_t> Vtbl{ RE::VTABLE_BGSImpactManager[0] };
 		_ProcessEvent = Vtbl.write_vfunc(0x01, ProcessEvent);
+
+		REL::Relocation<std::uintptr_t> Vtbl_1{ RE::VTABLE_BGSImpactManager[1] };
+		_ProcessEvent_Coll = Vtbl_1.write_vfunc(0x01, ProcessEvent_Coll);
 	}
 
 	BSEventNotifyControl Hook_BGSImpactManager::ProcessEvent(BGSImpactManager* a_this, const BGSFootstepEvent* a_event, BSTEventSource<BGSFootstepEvent>* a_eventSource) {
@@ -18,9 +21,9 @@ namespace Hooks
 		return result;
 	}
 
-	BSEventNotifyControl Hook_BGSImpactManager::ProcessEvent(BGSImpactManager* a_this, const BGSCollisionSoundEvent* a_event, BSTEventSource<BGSCollisionSoundEvent>* a_eventSource) {
+	BSEventNotifyControl Hook_BGSImpactManager::ProcessEvent_Coll(BGSImpactManager* a_this, const BGSCollisionSoundEvent* a_event, BSTEventSource<BGSCollisionSoundEvent>* a_eventSource) {
 		Gts::ImpactManager::GetSingleton().HookCollisionEvent(a_this, a_event, a_eventSource);
-		auto result = _ProcessEvent(a_this, a_event, a_eventSource);
+		auto result = _ProcessEvent_Coll(a_this, a_event, a_eventSource);
 		return result;
 	}
 }
