225046564cd27949c53d8077a4181e5e420b74c8
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-02-15 18:28:29 +0300

    Not public?

--- src/utils/ControlUtils.hpp
+++ src/utils/ControlUtils.hpp
@@ -17,62 +17,61 @@ namespace RE {
     class Control_Map :
 		public BSTSingletonSDM<Control_Map>      // 00
         {
-            public:
-                using UEFlag = UserEvents::USER_EVENT_FLAG;
-                static Control_Map* GetSingleton();
+            using UEFlag = UserEvents::USER_EVENT_FLAG;
+            static Control_Map* GetSingleton();
 
-                struct RUNTIME_DATA
-                {
-                    #define RUNTIME_DATA_CONTENT                                                                        \
-                        stl::enumeration<UEFlag, std::uint32_t>          enabledControls;              /* 118, VR 138*/ \
-                        stl::enumeration<UEFlag, std::uint32_t>          unk11C;                       /* 11C, VR 13C*/ \
-                    RUNTIME_DATA_CONTENT
-                };
-                static_assert(sizeof(RUNTIME_DATA) == 0x40);
-
-                //members
-
-                #if !defined(ENABLE_SKYRIM_VR)                                   //flat
-                
-                #	if !defined(ENABLE_SKYRIM_AE) && defined(ENABLE_SKYRIM_SE)  // SSE
-                        RUNTIME_DATA_CONTENT;                                    // 0E8
-                #	else                                                        // AE
-                        RUNTIME_DATA_CONTENT;  // 0F8
-                #	endif
-                #elif !defined(ENABLE_SKYRIM_AE) && defined(ENABLE_SKYRIM_SE)  // VR
-                        RUNTIME_DATA_CONTENT;  // 108
-                #else                                                          // ALL
-                        // controlMap can be accessed up to kTotal, kAETotal, or kVRTotal based on runtime
-                #endif
-
-                [[nodiscard]] inline RUNTIME_DATA& GetRuntimeData() noexcept
-                {
-                    if SKYRIM_REL_CONSTEXPR (REL::Module::IsAE()) {
-                        if (REL::Module::get().version().compare(SKSE::RUNTIME_SSE_1_6_1130) != std::strong_ordering::less) {
-                            return REL::RelocateMember<RUNTIME_DATA>(this, 0xf0);
-                        }
-                    }
-                    return REL::RelocateMember<RUNTIME_DATA>(this, 0xE8, 0x108);
-                }
-
-                [[nodiscard]] inline const RUNTIME_DATA& GetRuntimeData() const noexcept
-                {
-                    if SKYRIM_REL_CONSTEXPR (REL::Module::IsAE()) {
-                        if (REL::Module::get().version().compare(SKSE::RUNTIME_SSE_1_6_1130) != std::strong_ordering::less) {
-                            return REL::RelocateMember<RUNTIME_DATA>(this, 0xf0);
-                        }
-                    }
-                    return REL::RelocateMember<RUNTIME_DATA>(this, 0xE8, 0x108);
-                }
+            struct RUNTIME_DATA
+            {
+                #define RUNTIME_DATA_CONTENT                                                                        \
+                    stl::enumeration<UEFlag, std::uint32_t>          enabledControls;              /* 118, VR 138*/ \
+                    stl::enumeration<UEFlag, std::uint32_t>          unk11C;                       /* 11C, VR 13C*/ \
+                RUNTIME_DATA_CONTENT
             };
-            #if !defined(ENABLE_SKYRIM_VR)
-            #	if !defined(ENABLE_SKYRIM_AE)
-                static_assert(sizeof(Control_Map) == 0x130);
-            #	elif !defined(ENABLE_SKYRIM_SE)
-                static_assert(sizeof(Control_Map) == 0x128);
+            static_assert(sizeof(RUNTIME_DATA) == 0x40);
+
+            //members
+
+            #if !defined(ENABLE_SKYRIM_VR)                                   //flat
+            
+            #	if !defined(ENABLE_SKYRIM_AE) && defined(ENABLE_SKYRIM_SE)  // SSE
+                    RUNTIME_DATA_CONTENT;                                    // 0E8
+            #	else                                                        // AE
+                    RUNTIME_DATA_CONTENT;  // 0F8
             #	endif
-            #elif !defined(ENABLE_SKYRIM_SE) && !defined(ENABLE_SKYRIM_AE)
-        //static_assert(sizeof(ControlMap) == 0x148);  // VS seems to choke even though this should be right
+            #elif !defined(ENABLE_SKYRIM_AE) && defined(ENABLE_SKYRIM_SE)  // VR
+                    RUNTIME_DATA_CONTENT;  // 108
+            #else                                                          // ALL
+                    // controlMap can be accessed up to kTotal, kAETotal, or kVRTotal based on runtime
+            #endif
+
+            [[nodiscard]] inline RUNTIME_DATA& GetRuntimeData() noexcept
+            {
+                if SKYRIM_REL_CONSTEXPR (REL::Module::IsAE()) {
+                    if (REL::Module::get().version().compare(SKSE::RUNTIME_SSE_1_6_1130) != std::strong_ordering::less) {
+                        return REL::RelocateMember<RUNTIME_DATA>(this, 0xf0);
+                    }
+                }
+                return REL::RelocateMember<RUNTIME_DATA>(this, 0xE8, 0x108);
+            }
+
+            [[nodiscard]] inline const RUNTIME_DATA& GetRuntimeData() const noexcept
+            {
+                if SKYRIM_REL_CONSTEXPR (REL::Module::IsAE()) {
+                    if (REL::Module::get().version().compare(SKSE::RUNTIME_SSE_1_6_1130) != std::strong_ordering::less) {
+                        return REL::RelocateMember<RUNTIME_DATA>(this, 0xf0);
+                    }
+                }
+                return REL::RelocateMember<RUNTIME_DATA>(this, 0xE8, 0x108);
+            }
+        };
+        #if !defined(ENABLE_SKYRIM_VR)
+        #	if !defined(ENABLE_SKYRIM_AE)
+            static_assert(sizeof(Control_Map) == 0x130);
+        #	elif !defined(ENABLE_SKYRIM_SE)
+            static_assert(sizeof(Control_Map) == 0x128);
+        #	endif
+        #elif !defined(ENABLE_SKYRIM_SE) && !defined(ENABLE_SKYRIM_AE)
+    //static_assert(sizeof(ControlMap) == 0x148);  // VS seems to choke even though this should be right
     #endif
     }
 #undef RUNTIME_DATA_CONTENT
